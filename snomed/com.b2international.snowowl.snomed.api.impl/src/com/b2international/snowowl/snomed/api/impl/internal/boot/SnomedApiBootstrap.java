3a0d0790062f1839fd2f4ae2cd151d53917abddc
CMark <mczotter@b2international.com>
2015-03-16 13:01:48 +0100

    [api] register skeleton SnomedIdentifierServiceImpl to ApplicationContext via SnomedApiBootstrap.

---
+++ snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/internal/boot/SnomedApiBootstrap.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011-2015 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.snomed.api.impl.internal.boot;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import com.b2international.snowowl.core.ApplicationContext;
+import com.b2international.snowowl.core.config.SnowOwlConfiguration;
+import com.b2international.snowowl.core.setup.BootstrapFragment;
+import com.b2international.snowowl.core.setup.Environment;
+import com.b2international.snowowl.snomed.api.impl.id.SnomedIdentifierServiceImpl;
+import com.b2international.snowowl.snomed.datastore.id.ISnomedIdentifierService;
+
+/**
+ * {@link BootstrapFragment} which registers various high-level services to the {@link ApplicationContext}.
+ * 
+ * @since 1.0
+ */
+public class SnomedApiBootstrap implements BootstrapFragment {
+
+	@Override
+	public void init(SnowOwlConfiguration configuration, Environment env) throws Exception {
+	}
+
+	@Override
+	public void run(SnowOwlConfiguration configuration, Environment env, IProgressMonitor monitor) throws Exception {
+		env.services().registerService(ISnomedIdentifierService.class, new SnomedIdentifierServiceImpl());		
+	}
+	
+}
