8d1a5ca7a21206bcfc3205a1e7b2b707fdfa4ae5
CMark <mczotter@b2international.com>
2015-03-16 11:58:34 +0100

    [api] initial API and skeleton impl of SCT ID services

---
+++ snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/id/SnomedIdentifierServiceImpl.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2011-2015 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.snomed.api.impl.id;
+
+import com.b2international.snowowl.snomed.datastore.ComponentNature;
+import com.b2international.snowowl.snomed.datastore.id.ISnomedIdentifierService;
+
+/**
+ * @since 1.0
+ */
+public class SnomedIdentifierServiceImpl implements ISnomedIdentifierService {
+
+	@Override
+	public String generateId(ComponentNature component) {
+		throw new UnsupportedOperationException("Not implemented yet");
+	}
+
+	@Override
+	public String generateId(ComponentNature component, String namespace) {
+		throw new UnsupportedOperationException("Not implemented yet");
+	}
+
+}
