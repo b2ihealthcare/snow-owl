25c16136a95f3d2001c637235604e3e957b07f36
CMark <mczotter@b2international.com>
2015-03-17 18:15:10 +0100

    [core] moved ComponentCategory to snowowl.core.terminology

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/terminology/ComponentCategory.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/terminology/ComponentCategory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.snowowl.api.domain;
+package com.b2international.snowowl.core.terminology;
 
 import com.b2international.commons.StringUtils;
 
