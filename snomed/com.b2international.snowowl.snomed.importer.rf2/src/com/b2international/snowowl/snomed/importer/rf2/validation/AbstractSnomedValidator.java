3624170761255e1399e7bf7976b7b6e347e31303
Kai Kewley <kaikewley@gmail.com>
2015-03-26 19:06:21 +0000

    Include concept ID when reporting unique component ID failures.

--- snomed/com.b2international.snowowl.snomed.importer.rf2/src/com/b2international/snowowl/snomed/importer/rf2/validation/AbstractSnomedValidator.java
+++ snomed/com.b2international.snowowl.snomed.importer.rf2/src/com/b2international/snowowl/snomed/importer/rf2/validation/AbstractSnomedValidator.java
@@ -382,13 +382,13 @@ public abstract class AbstractSnomedValidator {
 	public void validateComponentUnique(final List<String> row, final Map<String, List<String>> componentIds, final Set<String> messages, final int lineNumber) {
 		if (componentIds.containsKey(row.get(0))) {
 			// if the id is for the same component as before
-			if (componentIds.get(row.get(0)).get(0).equals(row.get(4))) {
+			String conceptId = row.get(4);
+			if (componentIds.get(row.get(0)).get(0).equals(conceptId)) {
 				// we set the new status
 				componentIds.get(row.get(0)).set(1, row.get(2));
 			} else if (!componentIds.get(row.get(0)).get(1).equals("0")) {
-				messages.add(MessageFormat.format("Line number {0} in the ''{1}'' file", lineNumber, releaseFileName));
-			}
-		} else {
+				messages.add(MessageFormat.format("Line number {0} in the ''{1}'' file part of concept ID {2}", lineNumber, releaseFileName, conceptId));
+			}		} else {
 			componentIds.put(row.get(0), createConceptIdStatusList(row));
 		}
 	}
