269eee16d95f8ab1aed99ab0e00fa4a11d910761
András Péteri <apeteri@b2international.com>
2015-06-25 15:40:47 +0200

    SO-1635: Recreate import configuration and example test cases

---
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/io/SnomedImportApiExamplesTest.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2011-2015 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.snomed.api.rest.io;
+
+import static com.b2international.snowowl.test.commons.rest.RestExtensions.givenAuthenticatedRequest;
+import static com.b2international.snowowl.test.commons.rest.RestExtensions.joinPath;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import com.b2international.commons.platform.PlatformUtil;
+import com.b2international.snowowl.snomed.SnomedConstants.Concepts;
+import com.b2international.snowowl.snomed.api.domain.Rf2ReleaseType;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.jayway.restassured.response.Response;
+
+/**
+ * @since 2.0
+ */
+public class SnomedImportApiExamplesTest extends AbstractSnomedImportApiTest {
+	
+	private static final Set<String> FINISH_STATES = ImmutableSet.of("COMPLETED", "FAILED");
+	private static final long POLL_INTERVAL = TimeUnit.SECONDS.toMillis(1L);
+	private static final long POLL_TIMEOUT = TimeUnit.SECONDS.toMillis(30L);
+	
+	private void assertImportFileCanBeUploaded(final String importId, final String importFile) {
+		givenAuthenticatedRequest(SCT_API)
+		.with()
+			.multiPart(new File(PlatformUtil.toAbsolutePath(getClass(), importFile)))
+		.when()
+			.post("/imports/{id}/archive", importId)
+		.then()
+		.assertThat()
+			.statusCode(204);
+	}
+
+	private void assertImportCompletes(final String importId) {
+		
+		final long endTime = System.currentTimeMillis() + POLL_TIMEOUT;
+
+		Response pollResponse;
+		long currentTime;
+		
+		do {
+			
+			try {
+				Thread.sleep(POLL_INTERVAL);
+			} catch (final InterruptedException e) {
+				fail(e.toString());
+			}
+			
+			pollResponse = givenAuthenticatedRequest(SCT_API)
+			.when()
+				.get("/imports/{id}", importId);
+			
+			pollResponse
+			.then()
+			.assertThat()
+				.statusCode(200);
+			
+			currentTime = System.currentTimeMillis();
+			
+		} while (!FINISH_STATES.contains(pollResponse.getBody().path("status")) && currentTime < endTime);
+		
+		pollResponse
+		.then()
+		.assertThat()
+			.body("status", equalTo("COMPLETED"));
+	}
+
+	private void assertImportFileCanBeImported(final String importFile) {
+		final Map<?, ?> importConfiguration = ImmutableMap.of(
+			"type", Rf2ReleaseType.DELTA.name(),
+			"branchPath", joinPath("MAIN", branchName),
+			"languageRefSetId", Concepts.REFSET_LANGUAGE_TYPE_UK,
+			"createVersions", false
+		);
+		
+		final String importId = assertImportConfigurationCanBeCreated(importConfiguration);
+		assertImportFileCanBeUploaded(importId, importFile);
+		assertImportCompletes(importId);
+	}
+
+	@Test
+	public void importNewConcept() {
+		assertBranchCanBeCreated("MAIN", branchName);
+		assertConceptNotExists("63961392103", "MAIN", branchName);
+		
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150131_new_concept.zip");
+		
+		assertConceptExists("63961392103", "MAIN", branchName);
+	}
+
+	@Test
+	public void importNewDescription() {
+		assertBranchCanBeCreated("MAIN", branchName);
+		assertDescriptionNotExists("11320138110", "MAIN", branchName);
+		
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150131_new_concept.zip");
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150201_new_description.zip");
+		
+		assertDescriptionExists("11320138110", "MAIN", branchName);
+	}
+	
+	@Test
+	public void importNewRelationship() {
+		assertBranchCanBeCreated("MAIN", branchName);
+		assertRelationshipNotExists("24088071128", "MAIN", branchName);
+
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150131_new_concept.zip");
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150202_new_relationship.zip");
+		
+		assertRelationshipExists("24088071128", "MAIN", branchName);
+	}
+
+	private void assertPreferredTermEquals(final String conceptId, final String descriptionId, final String... segments) {
+		givenAuthenticatedRequest(SCT_API)
+		.with()
+			.header("Accept-Language", "en-GB")
+		.when()
+			.get("/{path}/concepts/{conceptId}/pt", joinPath(segments), conceptId)
+		.then()
+		.assertThat()
+			.statusCode(200)
+		.and()
+			.body("id", equalTo(descriptionId));
+	}
+	
+	@Test
+	public void importNewPreferredTerm() {
+		assertBranchCanBeCreated("MAIN", branchName);
+
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150131_new_concept.zip");
+		assertPreferredTermEquals("63961392103", "13809498114", "MAIN", branchName);
+		
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150201_new_description.zip");
+		assertPreferredTermEquals("63961392103", "13809498114", "MAIN", branchName);
+		
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150203_change_pt.zip");
+		assertPreferredTermEquals("63961392103", "11320138110", "MAIN", branchName);
+	}
+	
+	@Test
+	public void importConceptInactivation() {
+		assertBranchCanBeCreated("MAIN", branchName);
+		
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150131_new_concept.zip");
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150201_new_description.zip");
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150202_new_relationship.zip");
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150203_change_pt.zip");
+		assertImportFileCanBeImported("SnomedCT_Release_INT_20150204_inactivate_concept.zip");
+		
+		givenAuthenticatedRequest(SCT_API)
+		.when()
+			.get("/{path}/concepts/{conceptId}", joinPath("MAIN", branchName), "63961392103")
+		.then()
+		.assertThat()
+			.statusCode(200)
+		.and()
+			.body("active", equalTo(false));
+	}
+}
---
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/io/SnomedImportApiTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2011-2015 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.snomed.api.rest.io;
+
+import static com.b2international.snowowl.test.commons.rest.RestExtensions.givenAuthenticatedRequest;
+import static com.b2international.snowowl.test.commons.rest.RestExtensions.joinPath;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.b2international.snowowl.snomed.SnomedConstants.Concepts;
+import com.b2international.snowowl.snomed.api.domain.Rf2ReleaseType;
+import com.google.common.collect.ImmutableMap;
+import com.jayway.restassured.response.ValidatableResponse;
+
+/**
+ * @since 2.0
+ */
+public class SnomedImportApiTest extends AbstractSnomedImportApiTest {
+
+	private void assertImportConfigurationCreationFails(final Map<?, ?> importConfiguration) {
+		whenCreatingImportConfiguration(importConfiguration)
+		.then()
+		.assertThat()
+			.statusCode(400)
+		.and()
+			.body("status", equalTo(400));
+	}
+	
+	private ValidatableResponse assertImportConfigurationStatus(final String importId, final int expectedStatus) {
+		return givenAuthenticatedRequest(SCT_API)
+		.when()
+			.get("/imports/{id}", importId)
+		.then()
+		.assertThat()
+			.statusCode(expectedStatus);
+	}
+
+	private String assertImportConfigurationExistsAfterCreation() {
+		final Map<?, ?> importConfiguration = ImmutableMap.of(
+			"type", Rf2ReleaseType.DELTA.name(),
+			"branchPath", "MAIN",
+			"languageRefSetId", Concepts.REFSET_LANGUAGE_TYPE_UK,
+			"createVersions", false
+		);
+
+		final String importId = assertImportConfigurationCanBeCreated(importConfiguration);
+		
+		assertImportConfigurationStatus(importId, 200)
+		.and()
+			.body("status", equalTo("WAITING_FOR_FILE"));
+		
+		return importId;
+	}
+
+	@Test
+	public void createImportConfiguration() {
+		assertImportConfigurationExistsAfterCreation();
+	}
+
+	@Test
+	public void deleteImportConfiguration() {
+		final String importId = assertImportConfigurationExistsAfterCreation();
+		
+		givenAuthenticatedRequest(SCT_API)
+		.when()
+			.delete("/imports/{id}", importId)
+		.then()
+		.assertThat()
+			.statusCode(204);
+		
+		assertImportConfigurationStatus(importId, 404);
+	}
+	
+	@Test
+	public void noVersionsAllowedOnBranch() {
+		assertBranchCanBeCreated("MAIN", branchName);
+		
+		final Map<?, ?> importConfiguration = ImmutableMap.of(
+			"type", Rf2ReleaseType.DELTA.name(),
+			"branchPath", joinPath("MAIN", branchName),
+			"languageRefSetId", Concepts.REFSET_LANGUAGE_TYPE_UK,
+			"createVersions", true
+		);
+		
+		assertImportConfigurationCreationFails(importConfiguration);
+	}
+}
