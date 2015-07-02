89a1165fd280fec2a92d96b6c245ec70abe9aac0
András Péteri <apeteri@b2international.com>
2015-07-02 15:59:46 +0200

    SO-1640: Add REST controller for terminology review support

--- snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/ServicesConfiguration.java
+++ snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/ServicesConfiguration.java
@@ -45,10 +45,12 @@ import com.b2international.snowowl.core.MetadataHolder;
 import com.b2international.snowowl.core.MetadataHolderMixin;
 import com.b2international.snowowl.core.MetadataMixin;
 import com.b2international.snowowl.datastore.server.branch.Branch;
+import com.b2international.snowowl.datastore.server.review.Review;
 import com.b2international.snowowl.eventbus.IEventBus;
 import com.b2international.snowowl.snomed.api.domain.ISnomedComponent;
 import com.b2international.snowowl.snomed.api.domain.browser.ISnomedBrowserComponent;
 import com.b2international.snowowl.snomed.api.rest.domain.BranchMixin;
+import com.b2international.snowowl.snomed.api.rest.domain.ReviewMixin;
 import com.b2international.snowowl.snomed.api.rest.domain.ISnomedComponentMixin;
 import com.fasterxml.classmate.TypeResolver;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -175,6 +177,7 @@ public class ServicesConfiguration extends WebMvcConfigurerAdapter {
 		objectMapper.addMixInAnnotations(Branch.class, BranchMixin.class);
 		objectMapper.addMixInAnnotations(Metadata.class, MetadataMixin.class);
 		objectMapper.addMixInAnnotations(MetadataHolder.class, MetadataHolderMixin.class);
+		objectMapper.addMixInAnnotations(Review.class, ReviewMixin.class);
 		return objectMapper;
 	}
 	
