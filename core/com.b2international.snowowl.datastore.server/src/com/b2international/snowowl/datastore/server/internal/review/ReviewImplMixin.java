a264520c97f5e6c81945f862ed2345d709bc5012
András Péteri <apeteri@b2international.com>
2015-07-14 11:09:53 +0200

    SO-1640: Remove unused Jackson annotation from ReviewImplMixin

--- core/com.b2international.snowowl.datastore.server/src/com/b2international/snowowl/datastore/server/internal/review/ReviewImplMixin.java
+++ core/com.b2international.snowowl.datastore.server/src/com/b2international/snowowl/datastore/server/internal/review/ReviewImplMixin.java
@@ -18,13 +18,11 @@ package com.b2international.snowowl.datastore.server.internal.review;
 import com.b2international.snowowl.datastore.server.review.BranchState;
 import com.b2international.snowowl.datastore.server.review.ReviewStatus;
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * @since 4.2
  */
-@JsonIgnoreProperties({ "sourcePath", "targetPath" })
 public abstract class ReviewImplMixin {
 
     @JsonCreator
