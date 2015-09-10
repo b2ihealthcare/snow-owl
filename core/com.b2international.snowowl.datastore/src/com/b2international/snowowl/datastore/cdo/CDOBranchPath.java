0941ce2b18440fc93426fbf475d9ba3eeccbc5ca
András Péteri <apeteri@b2international.com>
2015-09-10 16:31:35 +0200

    SO-1677: Add javadoc to CDOBranchPath

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/cdo/CDOBranchPath.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/cdo/CDOBranchPath.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011-2015 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.b2international.snowowl.datastore.cdo;
 
 import static com.google.common.base.Preconditions.checkState;
@@ -11,17 +26,20 @@ import com.b2international.snowowl.core.api.BranchPath;
 import com.b2international.snowowl.datastore.branch.Branch;
 import com.google.common.primitives.Ints;
 
+/**
+ * Wraps a sequence of CDO branch identifiers, forming a path to a physical branch in the repository.
+ */
 public class CDOBranchPath implements BranchPath {
 
 	private static int[] getBranchIds(final CDOBranch branch) {
 		final CDOBranchPoint[] basePath = branch.getBasePath();
 		final int[] branchIds = new int[basePath.length];
-        branchIds[basePath.length - 1] = branch.getID();
-        
-        for (int i = 1; i < basePath.length; i++) {
-        	branchIds[i - 1] = basePath[i].getBranch().getID();
-        }
-        
+		branchIds[basePath.length - 1] = branch.getID();
+
+		for (int i = 1; i < basePath.length; i++) {
+			branchIds[i - 1] = basePath[i].getBranch().getID();
+		}
+
 		return branchIds;
 	}
 
@@ -32,11 +50,11 @@ public class CDOBranchPath implements BranchPath {
 	}
 
 	public CDOBranchPath(final CDOBranch branch) {
-        this(getBranchIds(branch));
+		this(getBranchIds(branch));
 	}
 
 	public CDOBranchPath(final int[] branchIds) {
-        checkState(branchIds[0] == CDOBranch.MAIN_BRANCH_ID, "First branch ID segment does not match the MAIN branch identifier.");
+		checkState(branchIds[0] == CDOBranch.MAIN_BRANCH_ID, "First branch ID segment does not match the MAIN branch identifier.");
 		this.branchIds = branchIds;
 	}
 
@@ -45,14 +63,19 @@ public class CDOBranchPath implements BranchPath {
 		checkState(!isMain(), "Can't return parent for MAIN.");
 		return new CDOBranchPath(Arrays.copyOf(branchIds, branchIds.length - 1));
 	}
-	
+
 	@Override
 	public boolean isMain() {
 		return branchIds.length == 1;
 	}
-	
+
 	@Override
 	public String path() {
 		return Ints.join(Branch.SEPARATOR, branchIds);
+	}
+
+	@Override
+	public String toString() {
+		return path();
 	}
 }
