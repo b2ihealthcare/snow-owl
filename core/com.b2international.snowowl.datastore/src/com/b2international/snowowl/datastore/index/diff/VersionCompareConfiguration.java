53ef66592afdcd94eae9101f25f6f1f18d9138e0
András Péteri <apeteri@b2international.com>
2015-09-29 15:30:25 +0200

    SO-1677: Use builder for creating VersionCompareConfiguration instances

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/index/diff/VersionCompareConfiguration.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/index/diff/VersionCompareConfiguration.java
@@ -15,20 +15,17 @@
  */
 package com.b2international.snowowl.datastore.index.diff;
 
-import static com.b2international.snowowl.datastore.BranchPathUtils.convertIntoBasePath;
-import static com.b2international.snowowl.datastore.BranchPathUtils.createVersionPath;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.io.Serializable;
 
 import com.b2international.snowowl.core.api.IBranchPath;
+import com.b2international.snowowl.datastore.BranchPathUtils;
 import com.b2international.snowowl.datastore.CodeSystemUtils;
 import com.b2international.snowowl.datastore.ICodeSystemVersion;
 
 /**
- * Represents a version compare configuration with a source and a target
- * {@link IBranchPath branch path} and a repository UUID.
- *
+ * Represents a version compare configuration with a source and a target {@link IBranchPath branch path} and a repository UUID.
  */
 public class VersionCompareConfiguration implements Serializable {
 
@@ -36,30 +33,91 @@ public class VersionCompareConfiguration implements Serializable {
 
 	private final String repositoryUuid;
 	private final String toolingName;
-	private final IBranchPath sourcePath;
-	private final IBranchPath targetPath;
 	private final boolean threeWay;
+	
+	private final IBranchPath sourceContextPath;
+	private final IBranchPath sourcePath;
 	private final boolean sourcePatched;
+	
+	private final IBranchPath targetPath;
 	private final boolean targetPatched;
 
-	public VersionCompareConfiguration(final String repositoryUuid, final ICodeSystemVersion sourceVersion, final ICodeSystemVersion targetVersion, final boolean threeWay) {
-		this(
-			checkNotNull(repositoryUuid, "repositoryUuid"), 
-			createVersionPath(checkNotNull(sourceVersion, "sourceVersion").getVersionId()), 
-			createVersionPath(checkNotNull(targetVersion, "targetVersion").getVersionId()),
-			sourceVersion.isPatched(),
-			targetVersion.isPatched(),
-			threeWay);
+	public static Builder builder(final String repositoryUuid, final boolean threeWay) {
+		return new Builder(repositoryUuid, threeWay);
 	}
-	
-	public VersionCompareConfiguration(final String repositoryUuid, final IBranchPath sourcePath, final IBranchPath targetPath, final boolean sourcePatched, final boolean targetPatched, final boolean threeWay) {
-		this.repositoryUuid = checkNotNull(repositoryUuid, "repositoryUuid");
-		this.threeWay = threeWay;
-		this.sourcePath = this.threeWay ? convertIntoBasePath(checkNotNull(sourcePath, "sourcePath")) : checkNotNull(sourcePath, "sourcePath");
-		this.targetPath = checkNotNull(targetPath, "targetPath");
-		this.sourcePatched = sourcePatched;
-		this.targetPatched = targetPatched;
-		toolingName = initToolingName(this.repositoryUuid);
+
+	public static class Builder {
+		private final String repositoryUuid;
+		private final String toolingName;
+		private final boolean threeWay;
+		
+		private IBranchPath sourceContextPath;
+		private IBranchPath sourcePath;
+		private boolean sourcePatched;
+
+		private IBranchPath targetPath;
+		private boolean targetPatched;
+		
+		private Builder(final String repositoryUuid, final boolean threeWay) {
+			this.repositoryUuid = checkNotNull(repositoryUuid, "repositoryUuid");
+			this.toolingName = checkNotNull(CodeSystemUtils.getSnowOwlToolingName(repositoryUuid), "Tooling name was null for repository: " + repositoryUuid);
+			this.threeWay = threeWay;
+		}
+		
+		public Builder source(final ICodeSystemVersion version) {
+			return source(BranchPathUtils.createVersionPath(version.getVersionId()), version.isPatched());
+		}
+
+		public Builder sourcePatched(final boolean patched) {
+			this.sourcePatched = patched;
+			return this;
+		}
+
+		public Builder source(final IBranchPath sourcePath, final boolean sourcePatched) {
+			return source(sourcePath, sourcePath, sourcePatched);
+		}
+		
+		public Builder source(final IBranchPath sourceContextPath, final IBranchPath sourcePath, final boolean sourcePatched) {
+			this.sourceContextPath = sourceContextPath;
+			this.sourcePath = sourcePath;
+			this.sourcePatched = sourcePatched;
+			return this;
+		}
+		
+		public Builder target(final ICodeSystemVersion version) {
+			return target(BranchPathUtils.createVersionPath(version.getVersionId()), version.isPatched());
+		}
+		
+		public Builder targetPatched(final boolean patched) {
+			this.targetPatched = patched;
+			return this;
+		}
+		
+		public Builder target(final IBranchPath targetPath, final boolean targetPatched) {
+			this.targetPath = targetPath;
+			this.targetPatched = targetPatched;
+			return this;
+		}
+		
+		public VersionCompareConfiguration build() {
+			checkNotNull(sourceContextPath, "sourceContextPath");
+			checkNotNull(sourcePath, "sourcePath");
+			checkNotNull(targetPath, "targetPath");
+			return new VersionCompareConfiguration(this);
+		}
+	}
+
+	private VersionCompareConfiguration(final Builder builder) {
+		this.repositoryUuid = builder.repositoryUuid;
+		this.toolingName = builder.toolingName;
+		this.threeWay = builder.threeWay;
+		
+		this.sourceContextPath = builder.sourceContextPath;
+		this.sourcePath = builder.sourcePath;
+		this.sourcePatched = builder.sourcePatched;
+		
+		this.targetPath = builder.targetPath;
+		this.targetPatched = builder.targetPatched;
 	}
 
 	/**Returns with the repository UUID.*/
@@ -67,6 +125,10 @@ public class VersionCompareConfiguration implements Serializable {
 		return repositoryUuid;
 	}
 
+	public IBranchPath getSourceContextPath() {
+		return sourceContextPath;
+	}
+	
 	/**Returns with the source {@link IBranchPath branch path}.*/
 	public IBranchPath getSourcePath() {
 		return sourcePath;
@@ -110,6 +172,7 @@ public class VersionCompareConfiguration implements Serializable {
 		int result = 1;
 		result = prime * result + ((repositoryUuid == null) ? 0 : repositoryUuid.hashCode());
 		result = prime * result + ((sourcePath == null) ? 0 : sourcePath.hashCode());
+		result = prime * result + ((sourceContextPath == null) ? 0 : sourceContextPath.hashCode());
 		result = prime * result + ((targetPath == null) ? 0 : targetPath.hashCode());
 		result = prime * result + (threeWay ? 1231 : 1237);
 		return result;
@@ -134,6 +197,11 @@ public class VersionCompareConfiguration implements Serializable {
 				return false;
 		} else if (!sourcePath.equals(other.sourcePath))
 			return false;
+		if (sourceContextPath == null) {
+			if (other.sourceContextPath != null)
+				return false;
+		} else if (!sourceContextPath.equals(other.sourceContextPath))
+			return false;
 		if (targetPath == null) {
 			if (other.targetPath != null)
 				return false;
@@ -151,14 +219,11 @@ public class VersionCompareConfiguration implements Serializable {
 		sb.append(": ");
 		sb.append(sourcePath.getPath());
 		sb.append((sourcePatched ? "*" : ""));
+		sb.append(" on ");
+		sb.append(sourceContextPath.getPath());
 		sb.append(" - ");
 		sb.append(targetPath.getPath());
 		sb.append((targetPatched ? "*" : ""));
 		return sb.toString();  
 	}
-
-	private String initToolingName(final String repositoryUuid) {
-		return checkNotNull(CodeSystemUtils.getSnowOwlToolingName(repositoryUuid), "Tooling name was null for repository: " + repositoryUuid);
-	}
-	
 }
