a5aa19653444046ceb65b9dab8705ce2f2cb28cd
CMark <mczotter@b2international.com>
2015-10-07 11:18:34 +0200

    SO-1762: moved MRCM importer exporter interfaces to mrcm.core.io

--- snomed/com.b2international.snowowl.snomed.mrcm.core/src/com/b2international/snowowl/snomed/mrcm/core/io/MrcmExporter.java
+++ snomed/com.b2international.snowowl.snomed.mrcm.core/src/com/b2international/snowowl/snomed/mrcm/core/io/MrcmExporter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.snowowl.snomed.mrcm.core.server;
+package com.b2international.snowowl.snomed.mrcm.core.io;
 
 import java.io.OutputStream;
 
