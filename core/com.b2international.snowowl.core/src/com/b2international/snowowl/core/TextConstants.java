1e624c756a6f7ddd0a687f14a0d42f1ccaff4509
András Péteri <apeteri@b2international.com>
2015-10-12 11:07:18 +0200

    SO-1718: Remove list of stop words and references

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/TextConstants.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/TextConstants.java
@@ -15,26 +15,9 @@
  */
 package com.b2international.snowowl.core;
 
-import java.util.Set;
-
 import com.google.common.base.CharMatcher;
-import com.google.common.collect.ImmutableSet;
 
 public abstract class TextConstants {
-
-	/**
-	 * Defines a set of words that are not added to the index during the analyzing process; they must also be removed
-	 * from query strings as well when matching for exact terms.
-	 * <p>
-	 * The original set of stop words are defined in <i>zres_ExcludedWords_en-US_INT_20070731.txt</i> in the International
-	 * RF1 SNOMED CT distribution.
-	 */
-	public static final Set<String> STOPWORDS = ImmutableSet.of("about", "alongside", "an", "and", "anything",
-			"around", "as", "at", "because", "before", "being", "both", "by", "cannot", "chronically", "consists",
-			"covered", "does", "during", "every", "finds", "for", "from", "in", "instead", "into", "more", "must",
-			"no", "not", "of", "on", "only", "or", "properly", "side", "sided", "some", "something", "specific",
-			"than", "that", "the", "things", "this", "throughout", "to", "up", "using", "usually", "when", "while",
-			"without");
 	
 	/**
 	 * Holds a set of characters that should be considered as a token separator (non-token character) when splitting
