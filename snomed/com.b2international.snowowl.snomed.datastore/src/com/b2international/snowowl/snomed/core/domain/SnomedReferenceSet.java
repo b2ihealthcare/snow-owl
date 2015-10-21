ed4cce06bc427f3e9c6b404cd5ca03df16a2c864
CMark <mczotter@b2international.com>
2015-10-21 14:15:06 +0200

    SO-1710: move SnomedReferenceSet representation to domain package

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/domain/SnomedReferenceSet.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/domain/SnomedReferenceSet.java
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.snowowl.snomed.core.refset;
+package com.b2international.snowowl.snomed.core.domain;
 
-import com.b2international.snowowl.snomed.core.domain.ISnomedComponent;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetType;
 
 /**
