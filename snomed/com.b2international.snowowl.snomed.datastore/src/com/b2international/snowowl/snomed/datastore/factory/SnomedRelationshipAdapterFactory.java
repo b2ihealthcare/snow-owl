c9a20f43faec00e9aa20d543aaa047c0949156ff
Gabor Nagy <gnagy@b2international.com>
2015-10-29 15:45:51 +0100

    SO-1722: Add simplified constructor to relationship index entry
    - this is necessary for creating fake index entries
    
    https://snowowl.atlassian.net/browse/SO-1722

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/factory/SnomedRelationshipAdapterFactory.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/factory/SnomedRelationshipAdapterFactory.java
@@ -19,10 +19,7 @@ import org.eclipse.core.runtime.IAdapterFactory;
 
 import com.b2international.snowowl.core.ApplicationContext;
 import com.b2international.snowowl.core.api.IComponent;
-import com.b2international.snowowl.core.date.EffectiveTimes;
-import com.b2international.snowowl.datastore.cdo.CDOIDUtils;
 import com.b2international.snowowl.snomed.Relationship;
-import com.b2international.snowowl.snomed.SnomedConstants;
 import com.b2international.snowowl.snomed.datastore.SnomedClientStatementBrowser;
 import com.b2international.snowowl.snomed.datastore.SnomedRelationshipIndexEntry;
 
@@ -41,29 +38,12 @@ public class SnomedRelationshipAdapterFactory implements IAdapterFactory {
 			if (adaptableObject instanceof SnomedRelationshipIndexEntry) {
 				return adaptableObject;
 			} else if (adaptableObject instanceof Relationship) {
-				SnomedRelationshipIndexEntry statement = ApplicationContext.getInstance().getService(SnomedClientStatementBrowser.class).getStatement(((Relationship) adaptableObject).getId());
-				if (null == statement) {
-					final Relationship relationship = (Relationship) adaptableObject;
-					statement = new SnomedRelationshipIndexEntry(
-									relationship.getId(), 
-									relationship.getSource().getId(), 
-									relationship.getType().getId(), 
-									relationship.getDestination().getId(), 
-									relationship.getCharacteristicType().getId(),
-									CDOIDUtils.asLong(relationship.cdoID()),
-									relationship.getModule().getId(),
-									(byte)relationship.getGroup(),
-									(byte)relationship.getUnionGroup(),
-									SnomedRelationshipIndexEntry.generateFlags(
-											relationship.isReleased(),
-											relationship.isActive(),
-											SnomedConstants.Concepts.INFERRED_RELATIONSHIP.equals(relationship.getCharacteristicType().getId()), 
-											SnomedConstants.Concepts.UNIVERSAL_RESTRICTION_MODIFIER.equals(relationship.getModifier().getId()),
-											relationship.isDestinationNegated()),
-									relationship.getEffectiveTime() == null ? EffectiveTimes.UNSET_EFFECTIVE_TIME : relationship.getEffectiveTime().getTime());
-					
+				Relationship relationship = (Relationship) adaptableObject;
+				SnomedRelationshipIndexEntry relationshipIndexEntry = ApplicationContext.getInstance().getService(SnomedClientStatementBrowser.class).getStatement(relationship.getId());
+				if (null == relationshipIndexEntry) {
+					relationshipIndexEntry = new SnomedRelationshipIndexEntry(relationship);
 				}
-				return statement;
+				return relationshipIndexEntry;
 			}
 		}
 		return null;
