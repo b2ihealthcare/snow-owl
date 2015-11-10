b5f472e4809eae1ba6dde01e1678f3dcbaa3a712
CMark <mczotter@b2international.com>
2015-11-10 11:53:19 +0100

    SO-1710: use referencedComponentId as label instead of member UUID

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/index/refset/SnomedRefSetMemberIndexEntry.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/index/refset/SnomedRefSetMemberIndexEntry.java
@@ -203,7 +203,7 @@ public class SnomedRefSetMemberIndexEntry extends SnomedIndexEntry implements IC
 		
 		return new SnomedRefSetMemberIndexEntry(
 				uuid, 
-				uuid, 
+				referencedComponentId, 
 				iconId, 
 				moduleId, 
 				0.0F, 
