a85354bb2233374456d7983753bb8aa59f3eb6f1
CMark <mczotter@b2international.com>
2015-11-24 14:36:52 +0100

    SO-1710: support referencedComponent expansion in member converter

--- snomed/com.b2international.snowowl.snomed.datastore.server/src/com/b2international/snowowl/snomed/datastore/server/converter/SnomedReferenceSetMemberConverter.java
+++ snomed/com.b2international.snowowl.snomed.datastore.server/src/com/b2international/snowowl/snomed/datastore/server/converter/SnomedReferenceSetMemberConverter.java
@@ -23,6 +23,7 @@ import com.b2international.snowowl.core.CoreTerminologyBroker;
 import com.b2international.snowowl.core.date.EffectiveTimes;
 import com.b2international.snowowl.core.domain.BranchContext;
 import com.b2international.snowowl.core.domain.CollectionResource;
+import com.b2international.snowowl.core.terminology.ComponentCategory;
 import com.b2international.snowowl.snomed.common.SnomedRf2Headers;
 import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants;
 import com.b2international.snowowl.snomed.core.domain.SnomedConcept;
@@ -32,7 +33,10 @@ import com.b2international.snowowl.snomed.core.domain.SnomedRelationship;
 import com.b2international.snowowl.snomed.core.domain.refset.SnomedReferenceSetMember;
 import com.b2international.snowowl.snomed.core.domain.refset.SnomedReferenceSetMemberImpl;
 import com.b2international.snowowl.snomed.core.domain.refset.SnomedReferenceSetMembers;
+import com.b2international.snowowl.snomed.datastore.id.SnomedIdentifiers;
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRefSetMemberIndexEntry;
+import com.b2international.snowowl.snomed.datastore.server.request.SearchRequestBuilder;
+import com.b2international.snowowl.snomed.datastore.server.request.SnomedRequests;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedQueryRefSetMember;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetMember;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetType;
@@ -40,6 +44,7 @@ import com.google.common.base.Function;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.Multimap;
 
 /**
  * @since 4.5
@@ -51,7 +56,7 @@ public class SnomedReferenceSetMemberConverter implements ResourceConverter<Snom
 
 	SnomedReferenceSetMemberConverter(BranchContext context, List<String> expand) {
 		this.context = context;
-		this.expand = expand;
+		this.expand = expand == null ? Collections.<String>emptyList() : expand;
 	}
 
 	@Override
@@ -79,17 +84,57 @@ public class SnomedReferenceSetMemberConverter implements ResourceConverter<Snom
 	
 	@Override
 	public SnomedReferenceSetMembers convert(Collection<SnomedRefSetMemberIndexEntry> components) {
-		return new SnomedReferenceSetMembers(
-				FluentIterable
-				.from(components)
-				.transform(new Function<SnomedRefSetMemberIndexEntry, SnomedReferenceSetMember>() {
-					@Override
-					public SnomedReferenceSetMember apply(SnomedRefSetMemberIndexEntry input) {
-						return toResource(input);
+		final List<SnomedReferenceSetMember> members = FluentIterable
+			.from(components)
+			.transform(new Function<SnomedRefSetMemberIndexEntry, SnomedReferenceSetMember>() {
+				@Override
+				public SnomedReferenceSetMember apply(SnomedRefSetMemberIndexEntry input) {
+					return toResource(input);
+				}
+			}).toList();
+		expand(members);
+		return new SnomedReferenceSetMembers(members);
+	}
+
+	private void expand(List<SnomedReferenceSetMember> members) {
+		if (expand.contains("referencedComponent")) {
+			final Multimap<String, SnomedReferenceSetMember> refCompToMembers = FluentIterable.from(members).index(new Function<SnomedReferenceSetMember, String>() {
+				@Override
+				public String apply(SnomedReferenceSetMember input) {
+					return input.getReferencedComponent().getId();
+				}
+			});
+			final Multimap<ComponentCategory, String> typeToIds = FluentIterable.from(refCompToMembers.keySet()).index(new Function<String, ComponentCategory>() {
+				@Override
+				public ComponentCategory apply(String input) {
+					return SnomedIdentifiers.getComponentCategory(input);
+				}
+			});
+			// query components
+			for (ComponentCategory category : typeToIds.keySet()) {
+				final Collection<String> componentIds = typeToIds.get(category);
+				final SearchRequestBuilder<?, ? extends CollectionResource<? extends SnomedCoreComponent>> search;
+				switch (category) {
+				case CONCEPT:
+					search = SnomedRequests.prepareConceptSearch();
+					break;
+				case DESCRIPTION:
+					search = SnomedRequests.prepareDescriptionSearch();
+					break;
+//				case RELATIONSHIP:
+//					search = SnomedRequests.prepareRelationshipSearch();
+//					break;
+				default: throw new UnsupportedOperationException("Category is not supported in referenced component expansion");
+				}
+				// TODO paging in expansion
+				// TODO async execution with Promise.all()
+				for (SnomedCoreComponent component : search.setComponentIds(componentIds).setLimit(componentIds.size()).build().execute(context)) {
+					for (SnomedReferenceSetMember member : refCompToMembers.get(component.getId())) {
+						((SnomedReferenceSetMemberImpl) member).setReferencedComponent(component);
 					}
-				})
-				// TODO add expansion
-				.toList());
+				}
+			}
+		}
 	}
 
 	private SnomedReferenceSetMember toResource(SnomedRefSetMemberIndexEntry entry) {
