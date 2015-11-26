09ab803e0460c2c3c6f5d0d1fe394b5164c684cb
CMark <mczotter@b2international.com>
2015-11-26 11:45:16 +0100

    [request] updated Request class javadoc

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/events/Request.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/events/Request.java
@@ -23,14 +23,24 @@ import com.b2international.snowowl.eventbus.IEventBus;
  * A {@link Request} represents an executable form of user intent. They can be executed in a specific context usually within a {@link ServiceProvider}
  * . Executing a {@link Request} will result in either a success or failure. Success usually returns the requested object or success message while
  * failure usually delivers an error object or {@link Throwable} to the caller.
+ * <p>
+ * If you have a reference to the context where an instance of {@link Request} can be executed, then you can immediately execute it via the
+ * {@link #execute(ServiceProvider)} method, this is basically the same as invoking a function. If you don't have the required context, then you can
+ * dispatch it via the following methods using a dispatcher (currently {@link IEventBus} is supported), which will deliver the message to the owner of
+ * the context and execute your {@link Request}:
+ * <ul>
+ * <li>{@link #execute(IEventBus)} - async execution, will return with a {@link Promise}</li>
+ * <li>{@link #executeSync(IEventBus)} - sync execution with unbounded timeout value</li>
+ * <li>{@link #executeSync(IEventBus, long)} - sync execution with given timeout value</li>
+ * </p>
  *
  * @since 4.5
- * @param <T>
+ * @param <C>
  *            - the type of context where this {@link Request} can be executed
- * @param <B>
- *            - the type of the result
+ * @param <R>
+ *            - the type of the resource aka the response
  */
-public interface Request<T extends ServiceProvider, B> extends Event {
+public interface Request<C extends ServiceProvider, R> extends Event {
 
 	/**
 	 * Sends and executes this {@link Request} asynchronously via the given dispatcher. The returned {@link Promise} will be either resolved or
@@ -40,7 +50,7 @@ public interface Request<T extends ServiceProvider, B> extends Event {
 	 *            - the dispatcher of the request
 	 * @return a {@link Promise} representing the execution of the {@link Request}
 	 */
-	Promise<B> execute(IEventBus bus);
+	Promise<R> execute(IEventBus bus);
 
 	/**
 	 * Sends this {@link Request} to existing handlers and waits for the response synchronously until it arrives. Basically converts this async
@@ -50,7 +60,7 @@ public interface Request<T extends ServiceProvider, B> extends Event {
 	 *            - the dispatcher of the request
 	 * @return
 	 */
-	B executeSync(IEventBus bus);
+	R executeSync(IEventBus bus);
 
 	/**
 	 * Sends this {@link Request} to existing handlers and waits for the response synchronously until it arrives or the timeout (defined in
@@ -62,7 +72,7 @@ public interface Request<T extends ServiceProvider, B> extends Event {
 	 *            - timeout value defined in milliseconds
 	 * @return
 	 */
-	B executeSync(IEventBus bus, long timeout);
+	R executeSync(IEventBus bus, long timeout);
 
 	/**
 	 * Executes this action on the given {@link ExecutionContext} directly without dispatching it.
@@ -71,6 +81,6 @@ public interface Request<T extends ServiceProvider, B> extends Event {
 	 *            - the context within this {@link Request} can be executed
 	 * @return - the result of the {@link Request}, never <code>null</code>.
 	 */
-	B execute(T context);
+	R execute(C context);
 
 }
