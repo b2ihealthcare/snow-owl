d47262171555e357e70b18d3669bdad47311806a
Gabor Nagy <gnagy@b2international.com>
2016-03-03 17:14:37 +0100

    SO-1858: Add logging and monitor handling to GlobalValidationJob
    https://snowowl.atlassian.net/browse/SO-1858

--- snomed/com.b2international.snowowl.snomed.validation/src/com/b2international/snowowl/snomed/validation/GlobalValidationJob.java
+++ snomed/com.b2international.snowowl.snomed.validation/src/com/b2international/snowowl/snomed/validation/GlobalValidationJob.java
@@ -18,6 +18,7 @@ package com.b2international.snowowl.snomed.validation;
 import static com.b2international.snowowl.core.ApplicationContext.getServiceForClass;
 import static com.google.common.collect.Iterables.isEmpty;
 import static com.google.common.collect.Sets.newHashSet;
+import static java.util.Collections.emptySet;
 
 import java.util.Collection;
 import java.util.List;
@@ -28,9 +29,12 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.b2international.commons.TimedProgressMonitorWrapper;
 import com.b2international.commons.http.ExtendedLocale;
+import com.b2international.commons.time.TimeUtil;
 import com.b2international.snowowl.core.ApplicationContext;
 import com.b2international.snowowl.core.ValuedJob;
 import com.b2international.snowowl.core.markers.IDiagnostic.DiagnosticSeverity;
@@ -45,6 +49,7 @@ import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptInd
 import com.b2international.snowowl.snomed.datastore.request.SnomedRequests;
 import com.b2international.snowowl.snomed.datastore.validation.IClientSnomedComponentValidationService;
 import com.google.common.base.Function;
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
@@ -57,6 +62,7 @@ import com.google.common.collect.Maps;
 public class GlobalValidationJob extends ValuedJob<Integer> {
 	
 	private static final String[] EMPTY_ARRAY = {};
+	private static final Logger LOG = LoggerFactory.getLogger(GlobalValidationJob.class);
 	
 	private final IClientSnomedComponentValidationService validationService;
 	private final Collection<String> globalValidationConstraintIds;
@@ -74,35 +80,44 @@ public class GlobalValidationJob extends ValuedJob<Integer> {
 	@Override
 	protected IStatus run(final IProgressMonitor monitor) {
 		
+		LOG.info("Validation started...");
+		final Stopwatch stopwatch = Stopwatch.createStarted();
+		
 		final SubMonitor delegateMonitor = SubMonitor.convert(new TimedProgressMonitorWrapper(monitor), 100);
 		
 		final Collection<ComponentValidationDiagnostic> validationResults;
 		
 		if (isEmpty(globalValidationConstraintIds)) {
-			validationResults = validationService.validateAll(delegateMonitor.newChild(90));
+			validationResults = validationService.validateAll(delegateMonitor.newChild(80));
 		} else {
-			validationResults = validationService.validateGlobalConstraints(globalValidationConstraintIds, delegateMonitor.newChild(90));
+			validationResults = validationService.validateGlobalConstraints(globalValidationConstraintIds, delegateMonitor.newChild(80));
 		}
 		
 		if (validationResults.size() == 1 && DiagnosticSeverity.CANCEL.equals(Iterables.getFirst(validationResults, null).getProblemMarkerSeverity())) {
 			return Status.CANCEL_STATUS;
 		}
 
-		final Collection<String> violatingComponentIds = collectViolatingComponentIds(validationResults, delegateMonitor.newChild(10));
+		final Collection<String> violatingComponentIds = collectViolatingComponentIds(validationResults, delegateMonitor.newChild(20));
+		
+		if (delegateMonitor.isCanceled()) {
+			return Status.CANCEL_STATUS;
+		}
 		
 		setValue(violatingComponentIds.size());
+		
+		LOG.info("Validation finished in {}.", TimeUtil.toString(stopwatch));
 		
 		return Status.OK_STATUS;
 	}
 
 	private Collection<String> collectViolatingComponentIds(final Collection<ComponentValidationDiagnostic> validationResults, final IProgressMonitor monitor) {
 		
-		final SubMonitor subMonitor = SubMonitor.convert(monitor, 10);
+		final SubMonitor subMonitor = SubMonitor.convert(monitor, 2);
 		final MarkerManager markerManager = ApplicationContext.getInstance().getService(MarkerManager.class);
 		
-		final Map<String, SnomedConceptIndexEntry> idToIndexEntryMap = getIdToIndexEntryMap(validationResults, subMonitor.newChild(5));
+		final Map<String, SnomedConceptIndexEntry> idToIndexEntryMap = getIdToIndexEntryMap(validationResults, subMonitor.newChild(1));
 		
-		final SubMonitor markerMonitor = subMonitor.newChild(5);
+		final SubMonitor markerMonitor = subMonitor.newChild(1);
 		markerMonitor.setWorkRemaining(validationResults.size());
 		
 		final Collection<String> violatingComponentIds = newHashSet(); 
@@ -114,6 +129,11 @@ public class GlobalValidationJob extends ValuedJob<Integer> {
 			}
 			
 			markerManager.createValidationMarkerOnComponent(idToIndexEntryMap.get(diagnostic.getId()), diagnostic);
+			
+			if (markerMonitor.isCanceled()) {
+				return emptySet();
+			}
+			
 			markerMonitor.worked(1);
 		}
 		
@@ -145,7 +165,7 @@ public class GlobalValidationJob extends ValuedJob<Integer> {
 			}
 		});
 		
-		monitor.worked(5);
+		monitor.worked(1);
 		
 		return idToEntryMap;
 	}
