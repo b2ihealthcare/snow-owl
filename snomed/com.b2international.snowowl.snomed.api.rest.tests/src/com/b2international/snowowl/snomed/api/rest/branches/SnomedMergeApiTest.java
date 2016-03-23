b49e93d8e966164f31a5daa32bcd0c752d7f1c45
András Péteri <apeteri@b2international.com>
2016-03-23 09:34:06 +0100

    SO-1923 Add test case for active IS A relationship on inactive concept

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/branches/SnomedMergeApiTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/branches/SnomedMergeApiTest.java
@@ -453,6 +453,29 @@ public class SnomedMergeApiTest extends AbstractSnomedApiTest {
 
 		assertMergeJobFails(testBranchPath.getParent(), testBranchPath, "Rebase conflicting concept deletion");
 	}
+	
+	@Test
+	public void noRebaseInactivatedConceptOnBranchNewRelationshipOnParent() {
+		mergeNewConceptForward();
+		
+		final Map<?, ?> changeOnBranch = ImmutableMap.builder()
+				.put("active", false)
+				.put("commitComment", "Inactivated concept on branch")
+				.build();
+		
+		assertConceptCanBeUpdated(testBranchPath, "C1", changeOnBranch);
+
+		final Map<?, ?> changeOnParent = ImmutableMap.builder()
+				.put("sourceId", symbolicNameMap.get("C1"))
+				.put("moduleId", Concepts.MODULE_SCT_CORE)
+				.put("typeId", Concepts.IS_A)
+				.put("destinationId", "49755003") // Morphologic abnormality
+				.put("commitComment", "New relationship")
+				.build();
+
+		assertComponentCreated(testBranchPath.getParent(), "R1", SnomedComponentType.RELATIONSHIP, changeOnParent);
+		assertMergeJobFails(testBranchPath.getParent(), testBranchPath, "Rebase conflicting concept deletion");
+	}
 
 	@Test
 	public void rebaseChangedConceptOnParentDeletedOnBranch() {
