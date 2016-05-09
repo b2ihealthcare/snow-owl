ab46cf5e38a1ad825724c99fcf79f0bb6477a187
Mark Czotter <mczotter@b2international.com>
2016-05-09 09:14:49 +0200

    SO-1957: move index tests to index package from index.lucene

--- commons/com.b2international.index.lucene.tests/src/com/b2international/index/tests/FSIndexAdminTest.java
+++ commons/com.b2international.index.lucene.tests/src/com/b2international/index/tests/FSIndexAdminTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.index.lucene.tests;
+package com.b2international.index.tests;
 
 import static org.junit.Assert.*;
 
