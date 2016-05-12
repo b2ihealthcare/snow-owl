fc7c81352421f13d29d4bf11881148a01e089a70
Mark Czotter <mczotter@b2international.com>
2016-05-12 10:54:52 +0200

    SO-1957: move datastore.index classes to compatibility bundle

---
+++ core/com.b2international.snowowl.datastore.comp46/src/com/b2international/snowowl/datastore/index/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @deprecated As of release 4.7 nested index implementation is no longer support, if possible switch to new index API
+ */
+package com.b2international.snowowl.datastore.index;
