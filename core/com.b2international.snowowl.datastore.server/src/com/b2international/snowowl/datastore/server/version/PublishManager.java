cd4edfd7e3770c6ce1e34693faf1147957f77854
Gabor Nagy <gnagy@b2international.com>
2016-05-13 09:23:29 +0200

    Revert "SDD-681: Log version name after publication"
    This reverts commit eb2128ddd7adb66d821c0c0b7481acebbfbd8152.

--- core/com.b2international.snowowl.datastore.server/src/com/b2international/snowowl/datastore/server/version/PublishManager.java
+++ core/com.b2international.snowowl.datastore.server/src/com/b2international/snowowl/datastore/server/version/PublishManager.java
@@ -105,14 +105,15 @@ public abstract class PublishManager implements IPublishManager {
 			logWork(monitor);
 			publishTerminologyMetadateChanges(configuration);
 			logWork(monitor);
-			
+
 		} catch (final SnowowlServiceException e) {
 			handleError(e);
+		} finally {
 			ToolingIdThreadLocal.reset();
 		}
-		
-		LOGGER.info(format(NEW_VERSION_CREATED_TEMPLATE, configuration.getVersionId(), getToolingName()));
-		ToolingIdThreadLocal.reset();
+
+		format(NEW_VERSION_CREATED_TEMPLATE, configuration.getVersionId(), getToolingName());
+
 	}
 	
 	/**
@@ -315,7 +316,6 @@ public abstract class PublishManager implements IPublishManager {
 	@Nullable
 	private ICodeSystemVersion getVersion(final IBranchPath branchPath, final String versionId) {
 		final ICodeSystemVersion version = uniqueIndex(getAllVersions(branchPath), new Function<ICodeSystemVersion, String>() {
-			@Override
 			public String apply(final ICodeSystemVersion version) {
 				return checkNotNull(version, "version").getVersionId();
 			}
