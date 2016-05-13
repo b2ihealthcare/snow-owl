a5bb5c42d5e0ca0ee7dd6706c6aa3af47513eac6
Mark Czotter <mczotter@b2international.com>
2016-05-13 13:25:40 +0200

    SO-1957: fix compile errors in SearchRequest

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/request/SearchRequest.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/request/SearchRequest.java
@@ -15,8 +15,6 @@
  */
 package com.b2international.snowowl.datastore.request;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
@@ -24,22 +22,12 @@ import java.util.List;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
-import org.apache.lucene.queries.BooleanFilter;
-import org.apache.lucene.queries.ChainedFilter;
-import org.apache.lucene.search.BooleanClause.Occur;
-import org.apache.lucene.search.ConstantScoreQuery;
-import org.apache.lucene.search.Filter;
-import org.apache.lucene.search.FilteredQuery;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.TotalHitCountCollector;
-
 import com.b2international.commons.options.Options;
+import com.b2international.index.mapping.DocumentMapping;
+import com.b2international.index.query.Expression;
+import com.b2international.index.query.Expressions;
 import com.b2international.snowowl.core.api.SnowowlRuntimeException;
 import com.b2international.snowowl.core.domain.BranchContext;
-import com.b2international.snowowl.datastore.index.mapping.Mappings;
-import com.google.common.collect.Iterables;
-import com.google.common.primitives.Ints;
 
 /**
  * @since 4.5
@@ -128,51 +116,51 @@ public abstract class SearchRequest<B> extends BaseResourceRequest<BranchContext
 		return componentIds;
 	}
 	
-	protected Filter createComponentIdFilter() {
-		return Mappings.id().createTermsFilter(componentIds);
+	protected Expression createComponentIdFilter() {
+		return Expressions.matchAny(getIdField(), componentIds);
 	}
 	
-	protected Query createConstantScoreQuery(Query query) {
-		return (query instanceof ConstantScoreQuery) ? query : new ConstantScoreQuery(query);
+	protected String getIdField() {
+		return DocumentMapping._ID;
 	}
 
-	protected void addFilterClause(final BooleanFilter target, final Filter filter, final Occur occur) {
-		target.add(checkNotNull(filter, "Filter clause to add was null"), occur);
-	}
+//	protected void addFilterClause(final BooleanFilter target, final Filter filter, final Occur occur) {
+//		target.add(checkNotNull(filter, "Filter clause to add was null"), occur);
+//	}
+//	
+//	protected void addFilterClause(final List<Filter> target, final Filter filter) {
+//		target.add(checkNotNull(filter, "Filter clause to add was null"));
+//	}
+//	
+//	protected Query createFilteredQuery(final Query query, final BooleanFilter filter) {
+//		if (!filter.clauses().isEmpty()) {
+//			return new FilteredQuery(query, filter);
+//		} else {
+//			return query;
+//		}
+//	}
 	
-	protected void addFilterClause(final List<Filter> target, final Filter filter) {
-		target.add(checkNotNull(filter, "Filter clause to add was null"));
-	}
+//	protected Query createFilteredQuery(final Query query, final List<Filter> filters, final List<Integer> ops) {
+//		if (!filters.isEmpty()) {
+//			return new FilteredQuery(query, new ChainedFilter(Iterables.toArray(filters, Filter.class), Ints.toArray(ops)));
+//		} else {
+//			return query;
+//		}
+//	}
 	
-	protected Query createFilteredQuery(final Query query, final BooleanFilter filter) {
-		if (!filter.clauses().isEmpty()) {
-			return new FilteredQuery(query, filter);
-		} else {
-			return query;
-		}
-	}
+//	protected int getTotalHits(final IndexSearcher searcher, final Query query) throws IOException {
+//		final TotalHitCountCollector totalCollector = new TotalHitCountCollector();
+//		searcher.search(createConstantScoreQuery(query), totalCollector);
+//		return totalCollector.getTotalHits();
+//	}
 	
-	protected Query createFilteredQuery(final Query query, final List<Filter> filters, final List<Integer> ops) {
-		if (!filters.isEmpty()) {
-			return new FilteredQuery(query, new ChainedFilter(Iterables.toArray(filters, Filter.class), Ints.toArray(ops)));
-		} else {
-			return query;
-		}
-	}
+//	protected int numDocsToRetrieve(final IndexSearcher searcher, final int totalHits) {
+//		return numDocsToRetrieve(searcher, offset(), limit(), totalHits);
+//	}
 	
-	protected int getTotalHits(final IndexSearcher searcher, final Query query) throws IOException {
-		final TotalHitCountCollector totalCollector = new TotalHitCountCollector();
-		searcher.search(createConstantScoreQuery(query), totalCollector);
-		return totalCollector.getTotalHits();
-	}
-	
-	protected int numDocsToRetrieve(final IndexSearcher searcher, final int totalHits) {
-		return numDocsToRetrieve(searcher, offset(), limit(), totalHits);
-	}
-	
-	protected int numDocsToRetrieve(final IndexSearcher searcher, final int offset, final int limit, final int totalHits) {
-		return Ints.min(offset + limit, searcher.getIndexReader().maxDoc(), totalHits);
-	}
+//	protected int numDocsToRetrieve(final IndexSearcher searcher, final int offset, final int limit, final int totalHits) {
+//		return Ints.min(offset + limit, searcher.getIndexReader().maxDoc(), totalHits);
+//	}
 
 	@Override
 	public final B execute(BranchContext context) {
@@ -196,10 +184,10 @@ public abstract class SearchRequest<B> extends BaseResourceRequest<BranchContext
 				options);
 	}
 
-	protected void addComponentIdFilter(BooleanFilter filter) {
-		if (!componentIds().isEmpty()) {
-			addFilterClause(filter, createComponentIdFilter(), Occur.MUST);
-		}		
-	}
+//	protected void addComponentIdFilter(BooleanFilter filter) {
+//		if (!componentIds().isEmpty()) {
+//			addFilterClause(filter, createComponentIdFilter(), Occur.MUST);
+//		}		
+//	}
 
 }
