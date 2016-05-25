a1c121a53397cf3ec3c01562ffc64db426b4b804
Gabor Nagy <gnagy@b2international.com>
2016-05-25 11:01:59 +0200

    SO-1960: Implement Snomed Version builder
    - fix licenses
    - fix repository UUID in default INT Snomed Release
    
    https://snowowl.atlassian.net/browse/SO-1960

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/store/SnomedReleases.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/store/SnomedReleases.java
@@ -1,6 +1,18 @@
-/*******************************************************************************
- * Copyright (c) 2016 B2i Healthcare. All rights reserved.
- *******************************************************************************/
+/*
+ * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.b2international.snowowl.snomed.core.store;
 
 import static com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants.CONCEPT;
@@ -11,10 +23,9 @@ import static com.b2international.snowowl.snomed.common.SnomedTerminologyCompone
 import static com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants.SNOMED_INT_NAME;
 import static com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants.SNOMED_INT_OID;
 import static com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants.SNOMED_INT_SHORT_NAME;
-import static com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants.TERMINOLOGY_ID;
 
-import com.b2international.snowowl.datastore.CodeSystemUtils;
 import com.b2international.snowowl.snomed.SnomedReleaseType;
+import com.b2international.snowowl.snomed.datastore.SnomedDatastoreActivator;
 
 /**
  * @since 4.7
@@ -37,7 +48,10 @@ public class SnomedReleases {
 				.withCitation(SNOMED_INT_CITATION)
 				.withType(SnomedReleaseType.INTERNATIONAL)
 				.withTerminologyComponentId(CONCEPT)
-				.withRepositoryUUID(CodeSystemUtils.getRepositoryUuid(TERMINOLOGY_ID));
+				.withRepositoryUUID(SnomedDatastoreActivator.REPOSITORY_UUID);
 	}
 	
+	public static SnomedVersionBuilder newSnomedVersion() {
+		return new SnomedVersionBuilder();
+	}
 }
