13aa2f469a5676b969722fa3b4400eaba98d11cc
rporcio <rporcio@b2international.com>
2016-06-03 12:32:32 +0200

    New tests for versioning.

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/versioning/SnomedVersioningApiTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/versioning/SnomedVersioningApiTest.java
@@ -15,8 +15,15 @@
  */
 package com.b2international.snowowl.snomed.api.rest.versioning;
 
+import static com.b2international.snowowl.datastore.BranchPathUtils.createMainPath;
+import static com.b2international.snowowl.snomed.SnomedConstants.Concepts.MODULE_SCT_CORE;
+import static com.b2international.snowowl.snomed.SnomedConstants.Concepts.ROOT_CONCEPT;
+import static com.b2international.snowowl.snomed.api.rest.SnomedApiTestConstants.PREFERRED_ACCEPTABILITY_MAP;
 import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.givenBranchWithPath;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentCreated;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.givenConceptRequestBody;
 import static com.b2international.snowowl.test.commons.rest.RestExtensions.givenAuthenticatedRequest;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 import java.util.Map;
 
@@ -24,6 +31,8 @@ import org.junit.Test;
 
 import com.b2international.snowowl.core.api.IBranchPath;
 import com.b2international.snowowl.snomed.api.rest.AbstractSnomedApiTest;
+import com.b2international.snowowl.snomed.api.rest.SnomedApiTestConstants;
+import com.b2international.snowowl.snomed.api.rest.SnomedComponentType;
 import com.google.common.collect.ImmutableMap;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
@@ -98,15 +107,71 @@ public class SnomedVersioningApiTest extends AbstractSnomedApiTest {
 	}
 	
 	@Test
-	public void createExtensionVersion() {
+	public void createExtensionVersion01() {
+		final Map<?, ?> requestBody = givenConceptRequestBody(null, ROOT_CONCEPT, MODULE_SCT_CORE, PREFERRED_ACCEPTABILITY_MAP, false);
+		final String conceptId = assertComponentCreated(createMainPath(), SnomedComponentType.CONCEPT, requestBody);
+		
+		givenAuthenticatedRequest(SnomedApiTestConstants.SCT_API)
+			.when().get("{path}/concepts/{conceptId}", "MAIN", conceptId)
+			.then().body("released", equalTo(false));
+		
+		final String shortName = "versionTest";
 		final IBranchPath branchPath = createRandomBranchPath();
 		givenBranchWithPath(branchPath);
-		createCodeSystem(branchPath.getPath(), "versionTest");
+		createCodeSystem(branchPath.getPath(), shortName);
 		
-		whenCreatingVersion("v1", "20150130", "versionTest")
+		givenAuthenticatedRequest(SnomedApiTestConstants.SCT_API)
+			.when().get("{path}/concepts/{conceptId}", branchPath.getPath(), conceptId)
+			.then().body("released", equalTo(false));
+		
+		whenCreatingVersion("v1", "20150130", shortName)
 			.then().assertThat().statusCode(201);
 		
-		assertVersionGetStatus("v1", 200, "versionTest");
+		assertVersionGetStatus("v1", 200, shortName);
+		
+		givenAuthenticatedRequest(SnomedApiTestConstants.SCT_API)
+			.when().get("{path}/concepts/{conceptId}", "MAIN", conceptId)
+			.then().body("released", equalTo(false))
+			.and().body("effectiveTime", equalTo(null));
+		
+		givenAuthenticatedRequest(SnomedApiTestConstants.SCT_API)
+			.when().get("{path}/concepts/{conceptId}", branchPath.getPath(), conceptId)
+			.then().body("released", equalTo(true))
+			.and().body("effectiveTime", equalTo("20150130"));
+	}
+	
+	@Test
+	public void createExtensionVersion02() {
+		final IBranchPath branchPath = createRandomBranchPath();
+		givenBranchWithPath(branchPath);
+		
+		final Map<?, ?> requestBody = givenConceptRequestBody(null, ROOT_CONCEPT, MODULE_SCT_CORE, PREFERRED_ACCEPTABILITY_MAP, false);
+		final String conceptId = assertComponentCreated(branchPath, SnomedComponentType.CONCEPT, requestBody);
+		
+		givenAuthenticatedRequest(SnomedApiTestConstants.SCT_API)
+			.when().get("{path}/concepts/{conceptId}", "MAIN", conceptId)
+			.then().statusCode(404);
+		
+		final String shortName = "versionTest2";
+		createCodeSystem(branchPath.getPath(), shortName);
+		
+		givenAuthenticatedRequest(SnomedApiTestConstants.SCT_API)
+			.when().get("{path}/concepts/{conceptId}", branchPath.getPath(), conceptId)
+			.then().body("released", equalTo(false));
+		
+		whenCreatingVersion("v1", "20150130", shortName)
+			.then().assertThat().statusCode(201);
+		
+		assertVersionGetStatus("v1", 200, shortName);
+		
+		givenAuthenticatedRequest(SnomedApiTestConstants.SCT_API)
+			.when().get("{path}/concepts/{conceptId}", "MAIN", conceptId)
+			.then().statusCode(404);
+		
+		givenAuthenticatedRequest(SnomedApiTestConstants.SCT_API)
+			.when().get("{path}/concepts/{conceptId}", branchPath.getPath(), conceptId)
+			.then().body("released", equalTo(true))
+			.and().body("effectiveTime", equalTo("20150130"));
 	}
 	
 }
