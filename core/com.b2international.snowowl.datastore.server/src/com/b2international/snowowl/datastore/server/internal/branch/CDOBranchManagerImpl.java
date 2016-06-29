4cd7fc35182f60456b1dcced249615acd4faaff9
Mark Czotter <mczotter@b2international.com>
2016-06-29 11:14:44 +0200

    SO-1957: fix parent re-registration during branch creation

--- core/com.b2international.snowowl.datastore.server/src/com/b2international/snowowl/datastore/server/internal/branch/CDOBranchManagerImpl.java
+++ core/com.b2international.snowowl.datastore.server/src/com/b2international/snowowl/datastore/server/internal/branch/CDOBranchManagerImpl.java
@@ -92,8 +92,7 @@ public class CDOBranchManagerImpl extends BranchManagerImpl implements BranchRep
 				segments.add(segmentId);
 				segments.addAll(parent.segments());
 				registerBranch(new CDOBranchImpl(branch.getName(), branch.getBase().getBranch().getPathName(), baseTimestamp, headTimestamp, branch.getID(), segmentId, segments));
-				parent.withSegmentId(nextSegmentId());
-				registerBranch(parent);
+				registerBranch(parent.withSegmentId(nextSegmentId()));
 			}
 		}
     }
