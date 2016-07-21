9212ee8eac68542680fa09d02a83c237f5535d5f
Mark Czotter <mczotter@b2international.com>
2016-07-21 15:51:50 +0200

    SO-2003: move remaining classes from comp46 bundles to so-ext

--- core/com.b2international.snowowl.datastore.comp46/src/com/b2international/snowowl/datastore/index/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * @deprecated As of release 4.7 nested index implementation is no longer support, if possible switch to new index API
- */
-package com.b2international.snowowl.datastore.index;
