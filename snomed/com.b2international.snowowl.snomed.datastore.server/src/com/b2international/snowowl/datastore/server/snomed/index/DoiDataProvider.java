7e3923fe54cd33adadb1dbc7f55fc2259947b3e6
Mark Czotter <mczotter@b2international.com>
2016-10-10 15:59:48 +0200

    SO-2069: fix license key of DoiDataProvider class

--- snomed/com.b2international.snowowl.snomed.datastore.server/src/com/b2international/snowowl/datastore/server/snomed/index/DoiDataProvider.java
+++ snomed/com.b2international.snowowl.snomed.datastore.server/src/com/b2international/snowowl/datastore/server/snomed/index/DoiDataProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2015 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,6 @@ import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptDoc
 
 /**
  * Class for initializing the DOI data on concept documents.
- *
  */
 enum DoiDataProvider implements DoiData {
 
