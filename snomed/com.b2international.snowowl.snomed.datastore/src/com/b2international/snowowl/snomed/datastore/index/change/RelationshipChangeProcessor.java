d177f5865c8a82258ee00f74eb28b8ae49e8f772
rporcio <rporcio@b2international.com>
2016-10-20 16:45:53 +0200

    SO-2096 Handle referring refsets for changed relationships

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/index/change/RelationshipChangeProcessor.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/index/change/RelationshipChangeProcessor.java
@@ -16,26 +16,47 @@
 package com.b2international.snowowl.snomed.datastore.index.change;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import com.b2international.index.Hits;
+import com.b2international.index.query.Query;
 import com.b2international.index.revision.RevisionSearcher;
+import com.b2international.snowowl.core.api.ComponentUtils;
+import com.b2international.snowowl.core.date.EffectiveTimes;
 import com.b2international.snowowl.datastore.ICDOCommitChangeSet;
 import com.b2international.snowowl.datastore.index.ChangeSetProcessorBase;
 import com.b2international.snowowl.snomed.Relationship;
 import com.b2international.snowowl.snomed.SnomedPackage;
+import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants;
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRelationshipIndexEntry;
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRelationshipIndexEntry.Builder;
+import com.b2international.snowowl.snomed.datastore.index.refset.RefSetMemberChange;
+import com.b2international.snowowl.snomed.datastore.index.update.ReferenceSetMembershipUpdater;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
 
 /**
  * @since 4.3
  */
 public class RelationshipChangeProcessor extends ChangeSetProcessorBase {
+	
+	private final ReferringMemberChangeProcessor memberChangeProcessor;
 
 	public RelationshipChangeProcessor() {
 		super("relationship changes");
+		this.memberChangeProcessor = new ReferringMemberChangeProcessor(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER);
 	}
 
 	@Override
 	public void process(ICDOCommitChangeSet commitChangeSet, RevisionSearcher searcher) throws IOException {
+		final Multimap<String, RefSetMemberChange> referringRefSets = memberChangeProcessor.process(commitChangeSet, searcher);
+		
 		deleteRevisions(SnomedRelationshipIndexEntry.class, commitChangeSet.getDetachedComponents(SnomedPackage.Literals.RELATIONSHIP));
 		
 		for (Relationship relationship : commitChangeSet.getNewComponents(Relationship.class)) {
@@ -43,9 +64,46 @@ public class RelationshipChangeProcessor extends ChangeSetProcessorBase {
 			indexNewRevision(relationship.cdoID(), doc.build());
 		}
 		
-		for (Relationship relationship : commitChangeSet.getDirtyComponents(Relationship.class)) {
-			final Builder doc = SnomedRelationshipIndexEntry.builder(relationship);
-			indexChangedRevision(relationship.cdoID(), doc.build());
+		final Map<String, Relationship> changedRelationshipsById = StreamSupport
+				.stream(commitChangeSet.getDirtyComponents(Relationship.class).spliterator(), false)
+				.collect(Collectors.toMap(relationship -> relationship.getId(), relationship -> relationship));
+		
+		final Set<String> changedRelationshipIds = ImmutableSet.<String> builder()
+				.addAll(referringRefSets.keySet())
+				.addAll(changedRelationshipsById.keySet())
+				.build();
+		
+		final Query<SnomedRelationshipIndexEntry> query = Query.select(SnomedRelationshipIndexEntry.class)
+				.where(SnomedRelationshipIndexEntry.Expressions.ids(changedRelationshipIds))
+				.limit(changedRelationshipIds.size())
+				.build();
+		
+		final Hits<SnomedRelationshipIndexEntry> changedRelationshipHits = searcher.search(query);
+		final ImmutableMap<String, SnomedRelationshipIndexEntry> changedRelationshipRevisionsById = Maps
+				.uniqueIndex(changedRelationshipHits, ComponentUtils.<String> getIdFunction());
+		
+		for (final String id : changedRelationshipIds) {
+			final SnomedRelationshipIndexEntry currentDoc = changedRelationshipRevisionsById.get(id);
+			if (currentDoc == null) {
+				throw new IllegalStateException(String.format("Current relationship revision should not be null for %s", id));
+			}
+			
+			final Relationship relationship = changedRelationshipsById.get(id);
+			final Builder doc = SnomedRelationshipIndexEntry.builder(currentDoc);
+
+			if (relationship != null) {
+				doc.active(relationship.isActive())
+					.released(relationship.isReleased())
+					.moduleId(relationship.getModule().getId())
+					.effectiveTime(relationship.isSetEffectiveTime() ? relationship.getEffectiveTime().getTime() : EffectiveTimes.UNSET_EFFECTIVE_TIME);
+			}
+			
+			final Collection<String> currentReferringRefSets = currentDoc.getReferringRefSets();
+			final Collection<String> currentReferringMappingRefSets = currentDoc.getReferringMappingRefSets();
+			new ReferenceSetMembershipUpdater(referringRefSets.removeAll(id), currentReferringRefSets, currentReferringMappingRefSets)
+					.update(doc);
+			
+			indexChangedRevision(currentDoc.getStorageKey(), doc.build());
 		}
 	}
 	
