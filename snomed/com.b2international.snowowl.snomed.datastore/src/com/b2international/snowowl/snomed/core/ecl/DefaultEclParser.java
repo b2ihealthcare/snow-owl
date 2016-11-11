01dd33c0d697937ee95f89e3e0273ca452623eb1
Mark Czotter <mczotter@b2international.com>
2016-11-11 10:01:17 +0100

    SO-1782: use specific SyntaxException with position information...
    ...instead of one concatenated string with only messages

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/ecl/DefaultEclParser.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/ecl/DefaultEclParser.java
@@ -15,16 +15,21 @@
  */
 package com.b2international.snowowl.snomed.core.ecl;
 
-import java.io.StringReader;
+import static com.google.common.collect.Maps.newHashMap;
 
+import java.io.StringReader;
+import java.util.Map;
+
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.SyntaxErrorMessage;
 import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.parser.IParser;
 
+import com.b2international.commons.Pair;
 import com.b2international.snowowl.core.exceptions.BadRequestException;
+import com.b2international.snowowl.core.exceptions.SyntaxException;
 import com.b2international.snowowl.snomed.ecl.ecl.ExpressionConstraint;
-import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
-import com.google.common.collect.Iterables;
 
 /**
  * @since 5.4
@@ -45,8 +50,12 @@ public class DefaultEclParser implements EclParser {
 			try (final StringReader reader = new StringReader(expression)) {
 				final IParseResult parseResult = eclParser.parse(reader);
 				if (parseResult.hasSyntaxErrors()) {
-					final String message = Joiner.on("\n").join(Iterables.transform(parseResult.getSyntaxErrors(), (node) -> node.getSyntaxErrorMessage().getMessage()));
-					throw new BadRequestException(message);
+					final Map<Pair<Integer, Integer>, String> errors = newHashMap();
+					for (INode node : parseResult.getSyntaxErrors()) {
+						final SyntaxErrorMessage syntaxError = node.getSyntaxErrorMessage();
+						errors.put(Pair.of(node.getTotalStartLine(), node.getTotalOffset()), syntaxError.getMessage());
+					}
+					throw new SyntaxException("ECL", errors);
 				} else {
 					return (ExpressionConstraint) parseResult.getRootASTElement();
 				}
