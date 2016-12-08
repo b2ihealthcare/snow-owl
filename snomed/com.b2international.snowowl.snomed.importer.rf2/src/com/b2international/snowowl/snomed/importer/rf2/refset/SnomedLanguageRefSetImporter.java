59e33b56099a9d915840836af0b060c4f38be87d
Mark Czotter <mczotter@b2international.com>
2016-12-08 11:47:23 +0100

    SO-2127: remove temporary resource form lang. refset importer
    Fetch Description objects by storageKey and attach new members to lang
    members list directly instead of hacking with low-level revision
    objects.

--- snomed/com.b2international.snowowl.snomed.importer.rf2/src/com/b2international/snowowl/snomed/importer/rf2/refset/SnomedLanguageRefSetImporter.java
+++ snomed/com.b2international.snowowl.snomed.importer.rf2/src/com/b2international/snowowl/snomed/importer/rf2/refset/SnomedLanguageRefSetImporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2015 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,72 +17,35 @@ package com.b2international.snowowl.snomed.importer.rf2.refset;
 
 import java.io.InputStream;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.emf.cdo.CDOState;
-import org.eclipse.emf.cdo.common.id.CDOID;
-import org.eclipse.emf.cdo.common.revision.CDOList;
-import org.eclipse.emf.cdo.common.revision.CDORevision;
-import org.eclipse.emf.cdo.common.revision.CDORevisionUtil;
-import org.eclipse.emf.cdo.common.revision.delta.CDOAddFeatureDelta;
-import org.eclipse.emf.cdo.eresource.CDOResource;
-import org.eclipse.emf.cdo.internal.common.revision.delta.CDOAddFeatureDeltaImpl;
-import org.eclipse.emf.cdo.spi.common.revision.BaseCDORevision;
-import org.eclipse.emf.cdo.spi.common.revision.InternalCDORevision;
-import org.eclipse.emf.cdo.spi.common.revision.InternalCDORevisionDelta;
-import org.eclipse.emf.cdo.transaction.CDOTransaction;
-import org.eclipse.emf.ecore.InternalEObject;
-import org.eclipse.emf.internal.cdo.view.AbstractCDOView;
-import org.eclipse.emf.spi.cdo.InternalCDOSavepoint;
-import org.eclipse.emf.spi.cdo.InternalCDOTransaction;
 import org.supercsv.cellprocessor.NullObjectPattern;
 import org.supercsv.cellprocessor.ParseBool;
 import org.supercsv.cellprocessor.ift.CellProcessor;
 
-import com.b2international.collections.PrimitiveMaps;
-import com.b2international.collections.longs.LongKeyMap;
-import com.b2international.collections.longs.LongSet;
-import com.b2international.commons.CompareUtils;
-import com.b2international.commons.StringUtils;
-import com.b2international.commons.collect.LongSets;
-import com.b2international.commons.collect.LongSets.LongFunction;
-import com.b2international.snowowl.core.api.SnowowlServiceException;
-import com.b2international.snowowl.datastore.BranchPointUtils;
-import com.b2international.snowowl.datastore.cdo.CDOIDUtils;
-import com.b2international.snowowl.datastore.cdo.CDOUtils;
-import com.b2international.snowowl.datastore.server.CDOServerUtils;
 import com.b2international.snowowl.snomed.Component;
+import com.b2international.snowowl.snomed.Description;
 import com.b2international.snowowl.snomed.SnomedConstants.Concepts;
-import com.b2international.snowowl.snomed.SnomedPackage;
 import com.b2international.snowowl.snomed.common.SnomedRf2Headers;
-import com.b2international.snowowl.snomed.datastore.SnomedEditingContext;
 import com.b2international.snowowl.snomed.importer.rf2.csv.AssociatingRefSetRow;
 import com.b2international.snowowl.snomed.importer.rf2.csv.cellprocessor.ParseUuid;
 import com.b2international.snowowl.snomed.importer.rf2.model.ComponentImportType;
 import com.b2international.snowowl.snomed.importer.rf2.model.IndexConfiguration;
 import com.b2international.snowowl.snomed.importer.rf2.model.SnomedImportConfiguration;
 import com.b2international.snowowl.snomed.importer.rf2.model.SnomedImportContext;
-import com.b2international.snowowl.snomed.importer.rf2.terminology.ComponentLookup;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedLanguageRefSetMember;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSet;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetFactory;
-import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetPackage;
+import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetMember;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetType;
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 
-@SuppressWarnings("restriction")
-public class SnomedLanguageRefSetImporter extends AbstractSnomedRefSetImporter<AssociatingRefSetRow, SnomedLanguageRefSetMember> {
+public final class SnomedLanguageRefSetImporter extends AbstractSnomedRefSetImporter<AssociatingRefSetRow, SnomedLanguageRefSetMember> {
 
-	/**
-	 * CDO root resource name for storing SNOMED&nbsp;CT language type reference set members before committing them.
-	 */
-	private static final String TEMPORARY_LANGUAGE_MEMBER_ROOT_RESOURCE_NAME = "temporary_language_member_root";
-	
 	private static final Map<String, CellProcessor> CELL_PROCESSOR_MAPPING = ImmutableMap.<String, CellProcessor>builder()
 			.put(AssociatingRefSetRow.PROP_UUID, new ParseUuid())
 			.put(AssociatingRefSetRow.PROP_EFFECTIVE_TIME, createEffectiveTimeCellProcessor())
@@ -108,11 +71,8 @@ public class SnomedLanguageRefSetImporter extends AbstractSnomedRefSetImporter<A
 			SnomedRf2Headers.LANGUAGE_TYPE_HEADER,
 			INDEXES);
 
-	private final List<SnomedLanguageRefSetMember> newMembers;
-
 	public SnomedLanguageRefSetImporter(final SnomedImportContext importContext, final InputStream releaseFileStream, final String releaseFileIdentifier) {
 		super(IMPORT_CONFIGURATION, importContext, releaseFileStream, releaseFileIdentifier);
-		newMembers = Lists.newArrayList();
 	}
 
 	@Override
@@ -146,26 +106,14 @@ public class SnomedLanguageRefSetImporter extends AbstractSnomedRefSetImporter<A
 		return SnomedRefSetFactory.eINSTANCE.createSnomedLanguageRefSetMember();
 	}
 	
-	/**
-	 * Member will be added to a list instead. We will deal with the proper container at {@link #preCommit(InternalCDOTransaction)}.
-	 * <p>
-	 * {@inheritDoc}
-	 */
 	@Override
 	protected void attach(Collection<SnomedLanguageRefSetMember> componentsToAttach) {
-		final SnomedEditingContext editingContext = getImportContext().getEditingContext();
-		final CDOTransaction transaction = editingContext.getTransaction();
-		final CDOResource resource = transaction.getOrCreateResource(TEMPORARY_LANGUAGE_MEMBER_ROOT_RESOURCE_NAME);
-		
-		for (SnomedLanguageRefSetMember currentMember : componentsToAttach) {
-			//object with dirty state already contained in their proper container, we do not have to set the
-			//XXX right here we have to consider the followings: referenced component ID has been changed
-			//for the language member, but we will deal with it in the #preCommit
-			//there we have to check whether a dirty member has CDOSetFeature delta for the 'referencedComponentId' feature
-			//if so we will update its container.
-			if (CDOState.TRANSIENT.equals(currentMember.cdoState())) {
-				resource.getContents().add(currentMember);
-				newMembers.add(currentMember);
+		final Multimap<String, SnomedLanguageRefSetMember> membersByReferencedComponent = Multimaps.index(componentsToAttach, SnomedRefSetMember::getReferencedComponentId);
+		final Collection<Component> components = getComponents(membersByReferencedComponent.keySet());
+		for (Component component : components) {
+			if (component instanceof Description) {
+				final Collection<SnomedLanguageRefSetMember> newDescriptionMembers = membersByReferencedComponent.get(component.getId());
+				((Description) component).getLanguageRefSetMembers().addAll(newDescriptionMembers);
 			}
 		}
 	}
@@ -173,121 +121,6 @@ public class SnomedLanguageRefSetImporter extends AbstractSnomedRefSetImporter<A
 	@Override
 	protected SnomedRefSet createUninitializedRefSet(final String identifierConceptId) {
 		return SnomedRefSetFactory.eINSTANCE.createSnomedStructuralRefSet();
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.b2international.snowowl.snomed.importer.rf2.model.AbstractSnomedImporter#preCommit(org.eclipse.emf.spi.cdo.InternalCDOTransaction)
-	 */
-	@Override
-	protected void preCommit(final InternalCDOTransaction transaction) throws SnowowlServiceException {
-		
-		if (CompareUtils.isEmpty(newMembers)) {
-			return; //nothing to do. could happen when transaction has dirty objects but new ones
-		}
-		
-		final ComponentLookup<Component> componentLookup = getImportContext().getComponentLookup();
-		
-		//remove all unrelated description IDs
-		final LongSet referencedDescriptionIds = LongSets.newLongSet(LongSets.transform(newMembers, new LongFunction<SnomedLanguageRefSetMember>() {
-			@Override public long apply(final SnomedLanguageRefSetMember member) {
-				return Long.parseLong(getReferencedComponentId(member));
-			}
-		}));
-		
-		
-		final LongSet descriptionStorageKeys = componentLookup.getComponentStorageKeys(LongSets.toStringSet(referencedDescriptionIds));
-		
-		final List<CDOID> ids = CDOIDUtils.getIds(descriptionStorageKeys);
-		final List<CDORevision> revisions = CDOServerUtils.getRevisions(
-				BranchPointUtils.convert(BranchPointUtils.create(transaction)), 
-				ids);
-		
-		
-		if (null == revisions) {
-			String message = "Cannot load revision for descriptions from store.";
-			log("SNOMED CT import failed. {}", message);
-			throw new SnowowlServiceException(message);
-		}
-		
-		//mapping between SNOMED CT description storage key and the CDO revision
-		final LongKeyMap<InternalCDORevision> storageKeyToRevisionMap = PrimitiveMaps.newLongKeyOpenHashMap();
-		for (final Iterator<CDORevision> itr = revisions.iterator(); itr.hasNext(); /**/) {
-			
-			final InternalCDORevision revision = (InternalCDORevision) itr.next();
-			
-			storageKeyToRevisionMap.put(CDOIDUtils.asLong(revision.getID()), revision);
-			
-		}
-		
-		//process all new members
-		for (final SnomedLanguageRefSetMember newMember : newMembers) {
-			
-			final long descriptionStorageKey = componentLookup.getComponentStorageKey(getReferencedComponentId(newMember));
-			final Object object = storageKeyToRevisionMap.get(descriptionStorageKey);
-			
-			final BaseCDORevision memberRevision = (BaseCDORevision) newMember.cdoRevision();
-			final InternalCDORevision descriptionRevision = 
-					(InternalCDORevision) Preconditions.checkNotNull(object, "Cannot find description revision. CDO ID: %s", descriptionStorageKey);
-			
-			memberRevision.setContainerID(descriptionRevision.getID());
-			memberRevision.setContainingFeatureID(InternalEObject.EOPPOSITE_FEATURE_BASE - SnomedPackage.DESCRIPTION__LANGUAGE_REF_SET_MEMBERS);
-			memberRevision.setResourceID(CDOID.NULL);
-			
-			final InternalCDOSavepoint lastSavepoint = transaction.getLastSavepoint();
-			
-			//get revision delta for description
-			InternalCDORevisionDelta revisionDelta = (InternalCDORevisionDelta) lastSavepoint.getRevisionDeltas().get(descriptionRevision.getID());
-			
-			if (null == revisionDelta) {
-		    	//create empty revision delta
-		        revisionDelta = (InternalCDORevisionDelta) CDORevisionUtil.createDelta(descriptionRevision);
-		        //register it
-		        lastSavepoint.getRevisionDeltas().put(descriptionRevision.getID(), revisionDelta);
-			}
-
-		    //get current language reference set members for description
-		    final CDOList cdoList = descriptionRevision.getList(SnomedPackage.eINSTANCE.getDescription_LanguageRefSetMembers());
-	    
-		    //create add feature delta with the new member
-		    revisionDelta.addFeatureDelta(createAddRevisionDelta(newMember, cdoList.size()));
-		}
-		
-		final CDOResource resource = transaction.getOrCreateResource(TEMPORARY_LANGUAGE_MEMBER_ROOT_RESOURCE_NAME);
-		
-		//remove temporary resource from new objects
-		transaction.getLastSavepoint().getNewObjects().remove(resource.cdoID());
-		//remove object by its CDO ID from the CDO view as well to avoid conflicting temp CDO IDs
-		((AbstractCDOView) transaction).removeObject(resource.cdoID());
-		
-		//un-register dirty root-root resource
-		transaction.getLastSavepoint().getDirtyObjects().remove(transaction.getRootResource().cdoID());
-		
-		//un-register root-root CDO resource changes
-		transaction.getLastSavepoint().getRevisionDeltas().remove(transaction.getRootResource().cdoID()); 
-		
-		//clear members
-		newMembers.clear();
-		
-	}
-
-	//creates a new ADD CDO feature delta with the given reference set member and index
-	private CDOAddFeatureDelta createAddRevisionDelta(final SnomedLanguageRefSetMember newMember, final int index) {
-		return new CDOAddFeatureDeltaImpl(SnomedPackage.eINSTANCE.getDescription_LanguageRefSetMembers(), index, newMember);
-	}
-
-	/*returns with the referenced component ID of the member either from the object or from the CDO settings*/
-	private String getReferencedComponentId(final SnomedLanguageRefSetMember member) {
-		
-		String id = member.getReferencedComponentId();
-		
-		if (StringUtils.isEmpty(id)) {
-			
-			id = CDOUtils.getAttribute(member, SnomedRefSetPackage.eINSTANCE.getSnomedRefSetMember_ReferencedComponentId(), String.class);
-			
-		}
-		
-		return Preconditions.checkNotNull(id, "Referenced component ID was null for reference set member.  Member: %s", member);
-		
 	}
 	
 }
