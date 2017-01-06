110d5fd27df196d414f48304e219ddbd7ffcc454
András Péteri <apeteri@b2international.com>
2017-01-06 16:53:56 +0100

    SO-2138 Add extra properties to Fixtures.Data, convert references

--- commons/com.b2international.index.api.tests/src/com/b2international/index/SingleDocumentIndexTest.java
+++ commons/com.b2international.index.api.tests/src/com/b2international/index/SingleDocumentIndexTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2017 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,33 +44,41 @@ public class SingleDocumentIndexTest extends BaseIndexTest {
 	
 	@Test
 	public void searchEmptyIndexShouldReturnNullDocument() throws Exception {
-		assertNull(getDocument(Data.class, KEY));
+		assertNull(getDocument(Data.class, KEY1));
 	}
 	
 	@Test
 	public void indexDocument() throws Exception {
 		final Data doc = new Data();
-		indexDocument(KEY, doc);
-		assertEquals(doc, getDocument(Data.class, KEY));
+		indexDocument(KEY1, doc);
+		assertEquals(doc, getDocument(Data.class, KEY1));
 	}
 	
 	@Test
 	public void indexTwoDocumentWithDifferentTypeButWithSameId() throws Exception {
 		final Data doc = new Data();
 		final DataWithMap doc2 = new DataWithMap(ImmutableMap.<String, Object>of("prop1", "value1"));
-		indexDocument(KEY, doc);
-		indexDocument(KEY, doc2);
-		assertEquals(doc, getDocument(Data.class, KEY));
-		assertEquals(doc2, getDocument(DataWithMap.class, KEY));
+		indexDocument(KEY1, doc);
+		indexDocument(KEY1, doc2);
+		assertEquals(doc, getDocument(Data.class, KEY1));
+		assertEquals(doc2, getDocument(DataWithMap.class, KEY1));
 	}
 	
 	@Test
 	public void updateDocument() throws Exception {
 		indexDocument();
-		final Data updatedDoc = new Data("field1Updated", "field2Updated");
-		indexDocument(KEY, updatedDoc);
+		
+		final Data updatedDoc = new Data();
+		updatedDoc.setField1("field1_updated");
+		updatedDoc.setField2("field2_updated");
+		indexDocument(KEY1, updatedDoc);
+		
+		Query<Data> query = Query.select(Data.class)
+				.where(DocumentMapping.matchId(KEY1))
+				.build();
+		
 		// execute search so we can see that it really updated the doc and did not create a duplicate
-		final Iterable<Data> matches = search(Query.select(Data.class).where(DocumentMapping.matchId(KEY)).build());
+		final Iterable<Data> matches = search(query);
 		assertThat(matches).hasSize(1);
 		assertThat(matches).containsOnly(updatedDoc);
 	}
@@ -81,8 +89,8 @@ public class SingleDocumentIndexTest extends BaseIndexTest {
 		index().write(new IndexWrite<Void>() {
 			@Override
 			public Void execute(Writer index) throws IOException {
-				index.put(KEY, data);
-				assertNull(index.searcher().get(Data.class, KEY));
+				index.put(KEY1, data);
+				assertNull(index.searcher().get(Data.class, KEY1));
 				index.commit();
 				return null;
 			}
@@ -92,32 +100,42 @@ public class SingleDocumentIndexTest extends BaseIndexTest {
 	@Test
 	public void deleteDocument() throws Exception {
 		indexDocument();
-		deleteDocument(Data.class, KEY);
-		assertNull(getDocument(Data.class, KEY));
+		deleteDocument(Data.class, KEY1);
+		assertNull(getDocument(Data.class, KEY1));
 	}
 	
 	@Test
 	public void searchDocuments() throws Exception {
-		final Data data = new Data("field1", "field2");
-		final Data data2 = new Data("field1Changed", "field2");
-		indexDocument(KEY, data);
+		final Data data1 = new Data();
+		data1.setField1("field1_1");
+		data1.setField2("field2_1");
+		indexDocument(KEY1, data1);
+		
+		final Data data2 = new Data();
+		data2.setField1("field1_2");
+		data2.setField2("field2_2");
 		indexDocument(KEY2, data2);
 		
-		// search for field1Changed value, it should return a single doc
-		final Query<Data> query = Query.select(Data.class).where(Expressions.exactMatch("field1", "field1")).build();
+		// search for field1_1 value, it should return a single doc
+		final Query<Data> query = Query.select(Data.class)
+				.where(Expressions.exactMatch("field1", "field1_1"))
+				.build();
+		
 		final Iterable<Data> matches = search(query);
 		assertThat(matches).hasSize(1);
-		assertThat(matches).containsOnly(data);
+		assertThat(matches).containsOnly(data1);
 	}
 	
 	@Test
 	public void indexDocumentWithMapType() throws Exception {
 		final DataWithMap data = new DataWithMap(ImmutableMap.<String, Object>of("field1", "field1Value", "field2", "field2Value"));
-		indexDocument(KEY, data);
-		assertEquals(data, getDocument(DataWithMap.class, KEY));
+		indexDocument(KEY1, data);
+		assertEquals(data, getDocument(DataWithMap.class, KEY1));
 		
+		final Query<DataWithMap> query = Query.select(DataWithMap.class)
+				.where(Expressions.exactMatch("field1", "field1Value"))
+				.build();
 		
-		final Query<DataWithMap> query = Query.select(DataWithMap.class).where(Expressions.exactMatch("field1", "field1Value")).build();
 		final Iterable<DataWithMap> matches = search(query);
 		assertThat(matches).hasSize(1);
 		assertThat(matches).containsOnly(data);
--- commons/com.b2international.index.api.tests/src/com/b2international/index/revision/NestedDocumentRevisionIndexTest.java
+++ commons/com.b2international.index.api.tests/src/com/b2international/index/revision/NestedDocumentRevisionIndexTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2017 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,9 +40,13 @@ public class NestedDocumentRevisionIndexTest extends BaseRevisionIndexTest {
 	
 	@Test
 	public void indexNestedDocument() throws Exception {
-		final NestedData data = new NestedData("field1", new Data("field1", "field2"));
-		indexRevision(MAIN, STORAGE_KEY1, data);
-		assertEquals(data, getRevision(MAIN, NestedData.class, STORAGE_KEY1));
+		final Data child = new Data();
+		child.setField1("field1");
+		child.setField2("field2");
+		
+		final NestedData parent = new NestedData("parent1", child);
+		indexRevision(MAIN, STORAGE_KEY1, parent);
+		assertEquals(parent, getRevision(MAIN, NestedData.class, STORAGE_KEY1));
 	}
 
 	@Test
@@ -51,42 +55,66 @@ public class NestedDocumentRevisionIndexTest extends BaseRevisionIndexTest {
 		deleteRevision(MAIN, NestedData.class, STORAGE_KEY1);
 		
 		// query to get parent document, should be none
-		final Query<NestedData> parentDocQuery = Query.select(NestedData.class).where(Expressions.matchAll()).build();
+		final Query<NestedData> parentDocQuery = Query.select(NestedData.class)
+				.where(Expressions.matchAll())
+				.build();
+		
 		final Iterable<NestedData> parentDocs = search(MAIN, parentDocQuery);
 		assertThat(parentDocs).isEmpty();
 		
 		// query to get nested child document, should be none
-		final Query<Data> nestedDataQuery = Query.select(Data.class, NestedData.class).where(Expressions.matchAll()).build();
+		final Query<Data> nestedDataQuery = Query.select(Data.class, NestedData.class)
+				.where(Expressions.matchAll())
+				.build();
+		
 		final Iterable<Data> nestedDocs = search(MAIN, nestedDataQuery);
 		assertThat(nestedDocs).hasSize(0);
 	}
 	
 	@Test
 	public void searchParentDocumentWithNestedQuery() throws Exception {
-		final NestedData data = new NestedData("field1", new Data("field1", "field2"));
-		final Data nestedData2 = new Data("field1Changed", "field2");
-		final NestedData data2 = new NestedData("field1", nestedData2);
-		indexRevision(MAIN, STORAGE_KEY1, data);
-		indexRevision(MAIN, STORAGE_KEY2, data2);
+		final Data child1 = new Data();
+		child1.setField1("field1_1");
+		child1.setField2("field2_1");
+		final NestedData parent1 = new NestedData("parent1", child1);
 		
-		final Query<NestedData> query = Query.select(NestedData.class).where(Expressions.nestedMatch("data", Expressions.exactMatch("field1", "field1"))).build();
+		final Data child2 = new Data();
+		child2.setField1("field1_2");
+		child2.setField2("field2_2");
+		final NestedData parent2 = new NestedData("parent2", child2);
+		
+		indexRevision(MAIN, STORAGE_KEY1, parent1);
+		indexRevision(MAIN, STORAGE_KEY2, parent2);
+		
+		final Query<NestedData> query = Query.select(NestedData.class)
+				.where(Expressions.nestedMatch("data", Expressions.exactMatch("field1", "field1_1")))
+				.build();
+		
 		final Iterable<NestedData> matches = search(MAIN, query);
 		assertThat(matches).hasSize(1);
-		assertThat(matches).containsOnly(data);
+		assertThat(matches).containsOnly(parent1);
 	}
 	
 	@Test
 	public void searchNestedDocumentWithParentQuery() throws Exception {
 		indexNestedDocument();
 		deleteRevision(MAIN, NestedData.class, STORAGE_KEY1);
-		final Data nestedData2 = new Data("field1", "field2");
-		final NestedData data2 = new NestedData("field1", nestedData2);
+		
+		// properties in storage key 2's nested child are same as storage key 1's nested child 
+		final Data child = new Data();
+		child.setField1("field1");
+		child.setField2("field2");
+		
+		final NestedData data2 = new NestedData("parent2", child);
 		indexRevision(MAIN, STORAGE_KEY2, data2);
 		
-		final Query<Data> nestedQuery = Query.select(Data.class, NestedData.class).where(Expressions.exactMatch("field1", "field1")).build();
+		final Query<Data> nestedQuery = Query.select(Data.class, NestedData.class)
+				.where(Expressions.exactMatch("field1", "field1"))
+				.build();
+		
 		final Iterable<Data> nestedMatches = search(MAIN, nestedQuery);
 		assertThat(nestedMatches).hasSize(1);
-		assertThat(nestedMatches).containsOnly(nestedData2);
+		assertThat(nestedMatches).containsOnly(child);
 	}
 	
 }
