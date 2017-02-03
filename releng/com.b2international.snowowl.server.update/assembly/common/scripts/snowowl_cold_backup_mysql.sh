3697600a00241cfc0e76c124f977c7a4c13fd355
András Péteri <apeteri@b2international.com>
2017-02-03 14:37:50 +0100

    [releng] Update copyright revision years according to last commit date

--- releng/com.b2international.snowowl.server.update/assembly/common/scripts/snowowl_cold_backup_mysql.sh
+++ releng/com.b2international.snowowl.server.update/assembly/common/scripts/snowowl_cold_backup_mysql.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+#
+# Copyright 2016-2017 B2i Healthcare Pte Ltd, http://b2i.sg
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # This sample backup script for Snow Owl server creates a .zip file in the 
 # current directory with the saved contents of the MySQL databases and semantic
 # indexes for each terminology, as well as supporting index content.
