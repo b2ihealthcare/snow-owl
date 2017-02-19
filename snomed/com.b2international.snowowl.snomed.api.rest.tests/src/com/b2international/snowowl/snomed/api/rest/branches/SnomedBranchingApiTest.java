7ae47260ef36f042668f5c9eb8a5d29652ff3bde
András Péteri <apeteri@b2international.com>
2017-02-19 20:54:56 +0100

    SO-2154 Update branching test cases

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/branches/SnomedBranchingApiTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/branches/SnomedBranchingApiTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2017 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,25 +16,13 @@
 package com.b2international.snowowl.snomed.api.rest.branches;
 
 import static com.b2international.snowowl.datastore.BranchPathUtils.createPath;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.assertBranchChildrenContainsName;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.assertBranchCreationConflicts;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.assertBranchExists;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.assertBranchNotCreated;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.assertBranchNotExists;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.assertBranchReportedAsDeleted;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.assertBranchUpdated;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.assertBranchesContainsName;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.givenBranchWithPath;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.givenBranchWithPathAndMetadata;
-import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingApiAssert.whenDeletingBranchWithPath;
+import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingRestRequests.*;
 import static org.hamcrest.CoreMatchers.equalTo;
-
-import java.util.Map;
+import static org.hamcrest.CoreMatchers.hasItem;
 
 import org.junit.Test;
 
 import com.b2international.snowowl.core.api.IBranchPath;
-import com.b2international.snowowl.core.branch.Branch;
 import com.b2international.snowowl.datastore.BranchPathUtils;
 import com.b2international.snowowl.snomed.api.rest.AbstractSnomedApiTest;
 import com.google.common.collect.ImmutableMap;
@@ -46,80 +34,95 @@ public class SnomedBranchingApiTest extends AbstractSnomedApiTest {
 
 	@Test
 	public void readNonExistentBranch() {
-		assertBranchNotExists(createPath("MAIN/nonexistent"));
+		getBranch(createPath("MAIN/x/y/z")).statusCode(404);
 	}
 
 	@Test
 	public void createBranchWithNonexistentParent() {
-		assertBranchNotCreated(createPath(testBranchPath, "a"));
+		createBranch(createPath("MAIN/x/y/z")).statusCode(400);
+	}
+
+	@Test
+	public void updateMetadata() throws Exception {
+		updateBranch(branchPath, ImmutableMap.of("key", "value"));
+		getBranch(branchPath).body("metadata.key", equalTo("value"));
 	}
 
 	@Test
 	public void updateMainMetadata() throws Exception {
-		assertBranchUpdated(Branch.MAIN_PATH, ImmutableMap.of("test", "metadataUpdate"));
-		assertBranchExists(BranchPathUtils.createMainPath())
-			.and().body("metadata.test", equalTo("metadataUpdate"));
-	}
-	
-	@Test
-	public void createBranch() {
-		givenBranchWithPath(testBranchPath);
-		assertBranchExists(testBranchPath);
+		// XXX: modifies MAIN branch, may affect other tests
+		updateBranch(BranchPathUtils.createMainPath(), ImmutableMap.of("key", "value"));
+		getBranch(BranchPathUtils.createMainPath()).body("metadata.key", equalTo("value"));
 	}
 
 	@Test
-	public void createBranchWithMetadata() {
-		final String description = "Description of branch";
-		final Map<?, ?> metadata = ImmutableMap.of("description", description);
+	public void createChildBranch() {
+		IBranchPath a = BranchPathUtils.createPath(branchPath, "a");
+		createBranch(a).statusCode(201);
+		getBranch(a).statusCode(200);
+	}
 
-		givenBranchWithPathAndMetadata(testBranchPath, metadata);
-		assertBranchExists(testBranchPath)
-		.and().body("metadata.description", equalTo(description));
+	@Test
+	public void createChildBranchWithMetadata() {
+		IBranchPath a = BranchPathUtils.createPath(branchPath, "a");
+		createBranch(a, ImmutableMap.of("key", "value")).statusCode(201);
+		getBranch(a).statusCode(200).body("metadata.key", equalTo("value"));
 	}
 
 	@Test
 	public void createBranchTwice() {
-		givenBranchWithPath(testBranchPath);
-		assertBranchCreationConflicts(testBranchPath);
+		IBranchPath a = BranchPathUtils.createPath(branchPath, "a");
+		createBranch(a).statusCode(201);
+		createBranch(a).statusCode(409);
 	}
 
 	@Test
-	public void deleteBranch() {
-		givenBranchWithPath(testBranchPath);
-		whenDeletingBranchWithPath(testBranchPath);
-		assertBranchReportedAsDeleted(testBranchPath);
+	public void deleteChildBranch() {
+		IBranchPath a = BranchPathUtils.createPath(branchPath, "a");
+		createBranch(a).statusCode(201);
+		deleteBranch(a).statusCode(204);
+		getBranch(a).statusCode(200).body("deleted", equalTo(true));
 	}
 
 	@Test
 	public void deleteBranchRecursively() {
-		final IBranchPath secondBranchPath = createPath(testBranchPath, "child");
+		IBranchPath a = createPath(branchPath, "a");
+		IBranchPath b = createPath(a, "b");
 
-		givenBranchWithPath(testBranchPath);
-		givenBranchWithPath(secondBranchPath);
+		createBranchRecursively(b);
+		getBranch(b).statusCode(200);
+		getBranch(a).statusCode(200);
 
-		assertBranchExists(testBranchPath);
-		assertBranchExists(secondBranchPath);
-
-		whenDeletingBranchWithPath(testBranchPath);
-		assertBranchReportedAsDeleted(testBranchPath);
-		assertBranchReportedAsDeleted(secondBranchPath);
+		deleteBranch(a).statusCode(204);
+		getBranch(a).statusCode(200).body("deleted", equalTo(true));
+		getBranch(b).statusCode(200).body("deleted", equalTo(true));
 	}
 
 	@Test
 	public void createBranchOnDeletedBranch() {
-		final IBranchPath secondBranchPath = createPath(testBranchPath, "childOfDeletedBranch");
+		IBranchPath a = createPath(branchPath, "a");
+		IBranchPath b = createPath(a, "b");
 
-		givenBranchWithPath(testBranchPath);
-		whenDeletingBranchWithPath(testBranchPath);
+		createBranch(a).statusCode(201);
+		deleteBranch(a).statusCode(204);
 
-		assertBranchNotCreated(secondBranchPath);
+		createBranch(b).statusCode(400);
 	}
 
 	@Test
 	public void getChildren() {
-		givenBranchWithPath(testBranchPath);
+		IBranchPath a = createPath(branchPath, "a");
+		IBranchPath b = createPath(a, "b");
 
-		assertBranchesContainsName(testBranchPath);
-		assertBranchChildrenContainsName(testBranchPath);
+		createBranchRecursively(b);
+
+		getAllBranches().statusCode(200)
+				.body("items.name", hasItem(a.lastSegment()))
+				.body("items.name", hasItem(b.lastSegment()));
+
+		getBranchChildren(branchPath).statusCode(200)
+				.body("items.name", hasItem(a.lastSegment()))
+				.body("items.name", hasItem(b.lastSegment()));
 	}
+
 }
