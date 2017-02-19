34a925fa2eb4eb71349d4d0e031bd5ff4dfc7939
András Péteri <apeteri@b2international.com>
2017-02-19 20:55:45 +0100

    SO-2154 Update identifier API test cases

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/id/SnomedIdentifierApiTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/id/SnomedIdentifierApiTest.java
@@ -15,8 +15,8 @@
  */
 package com.b2international.snowowl.snomed.api.rest.id;
 
-import static com.b2international.snowowl.snomed.api.rest.SnomedComponentApiAssert.assertComponentNotExists;
-import static com.b2international.snowowl.test.commons.rest.RestExtensions.givenAuthenticatedRequest;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentRestRequests.getComponent;
+import static com.b2international.snowowl.snomed.api.rest.SnomedIdentifierRestRequests.generateSctId;
 import static org.hamcrest.CoreMatchers.allOf;
 
 import java.util.Arrays;
@@ -28,12 +28,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-import com.b2international.snowowl.datastore.BranchPathUtils;
 import com.b2international.snowowl.snomed.api.rest.AbstractSnomedApiTest;
-import com.b2international.snowowl.snomed.api.rest.SnomedApiTestConstants;
 import com.b2international.snowowl.snomed.api.rest.SnomedComponentType;
-import com.google.common.collect.ImmutableMap;
-import com.jayway.restassured.http.ContentType;
 
 /**
  * @since 2.0
@@ -46,7 +42,7 @@ public class SnomedIdentifierApiTest extends AbstractSnomedApiTest {
 		private final int indexFromEnd;
 		private final char expectedChar;
 
-		public CharAtMatcher(final int index, final char expectedChar) {
+		public CharAtMatcher(int index, char expectedChar) {
 			super(String.format("character at index %d counted from the end is %c", index, expectedChar));
 
 			this.indexFromEnd = index;
@@ -54,12 +50,12 @@ public class SnomedIdentifierApiTest extends AbstractSnomedApiTest {
 		}
 
 		@Override
-		public boolean matches(final Object item) {
+		public boolean matches(Object item) {
 			if (!(item instanceof String)) {
 				return false;
 			}
 
-			final String str = ((String) item);
+			String str = ((String) item);
 			return str.charAt(str.length() - indexFromEnd - 1) == expectedChar;
 		}
 	}
@@ -70,7 +66,7 @@ public class SnomedIdentifierApiTest extends AbstractSnomedApiTest {
 		private final int end;
 		private final String expectedSegment;
 
-		public SegmentMatches(final int start, final int end, final String expectedSegment) {
+		public SegmentMatches(int start, int end, String expectedSegment) {
 			super(String.format("string segment in range (%d,%d) counted from the end matches %s", start, end, expectedSegment));
 
 			this.start = start;
@@ -79,7 +75,7 @@ public class SnomedIdentifierApiTest extends AbstractSnomedApiTest {
 		}
 
 		@Override
-		public boolean matches(final Object item) {
+		public boolean matches(Object item) {
 			if (!(item instanceof String)) {
 				return false;
 			}
@@ -88,21 +84,21 @@ public class SnomedIdentifierApiTest extends AbstractSnomedApiTest {
 				return true;
 			}
 
-			final String str = ((String) item);
+			String str = ((String) item);
 			return str.substring(str.length() - start, str.length() - end).equals(expectedSegment);
 		}
 	}
 
-	@Parameters(name = "{index}: {0} identifier with namespace {1}")
+	@Parameters(name = "{index}_{0}_{1}")
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {
-				//	type								namespaceId	partitionDigit	componentDigit	
-				{ 	SnomedComponentType.CONCEPT,		null, 		'0', 			'0' }, 
-				{ 	SnomedComponentType.DESCRIPTION,	null, 		'0', 			'1' }, 
-				{ 	SnomedComponentType.RELATIONSHIP,	null, 		'0', 			'2' }, 
-				{ 	SnomedComponentType.CONCEPT,		"1000154",	'1', 			'0' }, 
-				{ 	SnomedComponentType.DESCRIPTION,	"1000154",	'1', 			'1' }, 
-				{ 	SnomedComponentType.RELATIONSHIP,	"1000154",	'1', 			'2' }, 
+			//	type								namespaceId	partitionDigit	componentDigit	
+			{ 	SnomedComponentType.CONCEPT,		null, 		'0', 			'0' }, 
+			{ 	SnomedComponentType.DESCRIPTION,	null, 		'0', 			'1' }, 
+			{ 	SnomedComponentType.RELATIONSHIP,	null, 		'0', 			'2' }, 
+			{ 	SnomedComponentType.CONCEPT,		"1000154",	'1', 			'0' }, 
+			{ 	SnomedComponentType.DESCRIPTION,	"1000154",	'1', 			'1' }, 
+			{ 	SnomedComponentType.RELATIONSHIP,	"1000154",	'1', 			'2' }, 
 		});
 	}
 
@@ -111,34 +107,21 @@ public class SnomedIdentifierApiTest extends AbstractSnomedApiTest {
 	private final char partitionDigit;
 	private final char componentDigit;
 
-	public SnomedIdentifierApiTest(final SnomedComponentType type, final String namespaceId, final char partitionDigit, final char componentDigit) {
+	public SnomedIdentifierApiTest(SnomedComponentType type, String namespaceId, char partitionDigit, char componentDigit) {
 		this.type = type;
 		this.namespaceId = namespaceId;
 		this.partitionDigit = partitionDigit;
 		this.componentDigit = componentDigit;
 	}
 
-	private String assertGeneratedIdMatchesRequest() {
-
-		final ImmutableMap.Builder<String, String> requestBuilder = ImmutableMap.builder();
-		requestBuilder.put("type", type.toString());
-
-		if (namespaceId != null) {
-			requestBuilder.put("namespace", namespaceId);
-		}
-
-		return givenAuthenticatedRequest(SnomedApiTestConstants.SCT_API)
-				.with().contentType(ContentType.JSON)
-				.and().body(requestBuilder.build())
-				.when().post("/ids")
-				.then().assertThat().statusCode(201)
-				.and().body("id", allOf(new CharAtMatcher(1, componentDigit), new CharAtMatcher(2, partitionDigit), new SegmentMatches(10, 3, namespaceId)))
-				.and().extract().body().path("id");
-	}
-
 	@Test
 	public void generateComponentId() {
-		final String componentId = assertGeneratedIdMatchesRequest();
-		assertComponentNotExists(BranchPathUtils.createMainPath(), type, componentId);
+		String componentId = generateSctId(type, namespaceId)
+				.statusCode(201)
+				.body("id", allOf(new CharAtMatcher(1, componentDigit), new CharAtMatcher(2, partitionDigit), new SegmentMatches(10, 3, namespaceId)))
+				.extract().body().path("id");
+
+		// A generated SCTID that was just returned should not be in use
+		getComponent(branchPath, type, componentId).statusCode(404);
 	}
 }
