4607de674482ea3fcb1b1d4f84028a009fe1f717
András Péteri <apeteri@b2international.com>
2017-02-24 16:53:35 +0100

    SO-2154 Update copyright date on modified files

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/exceptions/ApiError.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/exceptions/ApiError.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2015 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2017 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
