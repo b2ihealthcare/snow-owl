1c3e186bd28d79d98d0b4a82aa8508b01e171ea6
Mark Czotter <mczotter@b2international.com>
2017-03-29 20:34:16 +0200

    [api] add /repositories and /repositories/:id endpoints

--- core/com.b2international.snowowl.api.rest/src/com/b2international/snowowl/api/rest/admin/RepositoryRestService.java
+++ core/com.b2international.snowowl.api.rest/src/com/b2international/snowowl/api/rest/admin/RepositoryRestService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2017 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,12 +28,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
+import com.b2international.commons.collections.Collections3;
 import com.b2international.snowowl.api.admin.IRepositoryService;
-import com.b2international.snowowl.api.admin.exception.LockConflictException;
 import com.b2international.snowowl.api.admin.exception.LockException;
-import com.mangofactory.swagger.annotations.ApiIgnore;
+import com.b2international.snowowl.api.rest.domain.RestApiError;
+import com.b2international.snowowl.api.rest.util.DeferredResults;
+import com.b2international.snowowl.core.Repositories;
+import com.b2international.snowowl.core.RepositoryInfo;
+import com.b2international.snowowl.core.exceptions.ApiError;
+import com.b2international.snowowl.datastore.request.RepositoryRequests;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
@@ -40,41 +46,53 @@ import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
 
 /**
- * Spring controller for exposing {@link IRepositoryService} functionality.
- * 
+ * Spring controller for exposing repository related administration functionalities.
  */
-@RestController
-@RequestMapping(value={"/repositories"}, produces={ MediaType.TEXT_PLAIN_VALUE })
-@Api("Administration")
-@ApiIgnore
+@Api("Repositories")
+@Controller
+@RequestMapping(value="/repositories", produces={ MediaType.APPLICATION_JSON_VALUE })
 public class RepositoryRestService extends AbstractAdminRestService {
 
 	@Autowired
 	protected IRepositoryService delegate;
 
-	@ExceptionHandler(LockConflictException.class)
-	@ResponseStatus(HttpStatus.CONFLICT)
-	public @ResponseBody String handleLockConflictException(final LockConflictException e) {
-		return handleException(e);
-	}
-
 	@ExceptionHandler(LockException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public @ResponseBody String handleLockException(final LockException e) {
-		return handleException(e);
+	public @ResponseBody RestApiError handleLockException(final LockException e) {
+		return RestApiError.of(ApiError.Builder.of(e.getMessage()).build()).build(HttpStatus.BAD_REQUEST.value());
 	}
 
-	@RequestMapping(method=RequestMethod.GET)
 	@ApiOperation(
-			value="Retrieve all repository identifiers",
-			notes="Retrieves the unique identifier of each running repository that stores terminology content.")
-	public String getRepositoryUuids() {
-		final List<String> repositoryUuids = delegate.getRepositoryUuids();
-		return joinStrings(repositoryUuids);
+			value="Retrieve all repositories",
+			notes="Retrieves all repositories that store terminology content.")
+	@RequestMapping(method=RequestMethod.GET)
+	public @ResponseBody DeferredResult<Repositories> getRepositories(
+			@ApiParam
+			@RequestParam(value="id", required=false)
+			List<String> idFilter) {
+		return DeferredResults.wrap(RepositoryRequests.prepareSearch()
+				.all()
+				.filterByIds(Collections3.toImmutableSet(idFilter))
+				.buildAsync()
+				.execute(bus));
+	}
+	
+	@ApiOperation(
+		value="Retrieve a repository",
+		notes="Retrieves a single repository by its identifier"
+	)
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "OK", response = Void.class),
+		@ApiResponse(code = 404, message = "Not found", response = RestApiError.class)
+	})
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public @ResponseBody DeferredResult<RepositoryInfo> getRepository(
+			@ApiParam("The repository identifier")
+			@PathVariable("id")
+			String id) {
+		return DeferredResults.wrap(RepositoryRequests.prepareGet(id).buildAsync().execute(bus));
 	}
 
-	@RequestMapping(value="lock", method=RequestMethod.POST)
-	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@ApiOperation(
 			value="Lock all repositories",
 			notes="Places a global lock, which prevents other users from making changes to any of the repositories "
@@ -85,6 +103,8 @@ public class RepositoryRestService extends AbstractAdminRestService {
 		@ApiResponse(code=409, message="Conflicting lock already taken"),
 		@ApiResponse(code=400, message="Illegal timeout value, or locking-related issue")
 	})
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@RequestMapping(value="lock", method=RequestMethod.POST)
 	public void lockGlobal(
 			@RequestParam(value="timeoutMillis", defaultValue="5000", required=false) 
 			@ApiParam(value="lock timeout in milliseconds")
@@ -93,8 +113,6 @@ public class RepositoryRestService extends AbstractAdminRestService {
 		delegate.lockGlobal(timeoutMillis);
 	}
 
-	@RequestMapping(value="unlock", method=RequestMethod.POST)
-	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@ApiOperation(
 			value="Unlock all repositories",
 			notes="Releases a previously acquired global lock.")
@@ -102,12 +120,12 @@ public class RepositoryRestService extends AbstractAdminRestService {
 		@ApiResponse(code=204, message="Unlock successful"),
 		@ApiResponse(code=400, message="Unspecified unlock-related issue")
 	})
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@RequestMapping(value="unlock", method=RequestMethod.POST)
 	public void unlockGlobal() {
 		delegate.unlockGlobal();
 	}
 
-	@RequestMapping(value="{repositoryUuid}/lock", method=RequestMethod.POST)
-	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@ApiOperation(
 			value="Lock single repository",
 			notes="Places a repository-level lock, which prevents other users from making changes to the specified repository. "
@@ -119,20 +137,20 @@ public class RepositoryRestService extends AbstractAdminRestService {
 		@ApiResponse(code=404, message="Repository not found"),
 		@ApiResponse(code=400, message="Illegal timeout value, or locking-related issue")
 	})
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@RequestMapping(value="{id}/lock", method=RequestMethod.POST)
 	public void lockRepository(
-			@PathVariable(value="repositoryUuid") 
-			@ApiParam(value="a unique identifier pointing to a particular repository")
-			final String repositoryUuid, 
+			@PathVariable(value="id") 
+			@ApiParam(value="The repository id")
+			final String id, 
 
 			@RequestParam(value="timeoutMillis", defaultValue="5000", required=false)
 			@ApiParam(value="lock timeout in milliseconds")
 			final int timeoutMillis) {
 
-		delegate.lockRepository(repositoryUuid, timeoutMillis);
+		delegate.lockRepository(id, timeoutMillis);
 	}
 
-	@RequestMapping(value="{repositoryUuid}/unlock", method=RequestMethod.POST)
-	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@ApiOperation(
 			value="Unlock single repository",
 			notes="Releases a previously acquired repository-level lock on the specified repository.")
@@ -141,28 +159,14 @@ public class RepositoryRestService extends AbstractAdminRestService {
 		@ApiResponse(code=404, message="Repository not found"),
 		@ApiResponse(code=400, message="Unspecified unlock-related issue")
 	})
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@RequestMapping(value="{id}/unlock", method=RequestMethod.POST)
 	public void unlockRepository(
-			@PathVariable(value="repositoryUuid") 
-			@ApiParam(value="a unique identifier pointing to a particular repository")
+			@ApiParam(value="The repository id")
+			@PathVariable(value="id") 
 			final String repositoryUuid) {
 
 		delegate.unlockRepository(repositoryUuid);
-	}
-
-	@RequestMapping(value="{repositoryUuid}/versions", method=RequestMethod.GET)
-	@ApiOperation(
-			value="Retrieve all version identifiers for a repository",
-			notes="Retrieves all version identifiers for the specified repository.")
-	@ApiResponses({
-		@ApiResponse(code=404, message="Repository not found")
-	})
-	public String getRepositoryVersionIds(
-			@PathVariable(value="repositoryUuid") 
-			@ApiParam(value="a unique identifier pointing to a particular repository")
-			final String repositoryUuid) {
-
-		final List<String> versions = delegate.getRepositoryVersionIds(repositoryUuid);
-		return joinStrings(versions);
 	}
 
 }
