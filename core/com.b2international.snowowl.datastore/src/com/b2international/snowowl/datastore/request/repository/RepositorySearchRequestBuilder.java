e9f322571f987820aca3196f8e240d43ecb1601a
Mark Czotter <mczotter@b2international.com>
2017-03-29 15:36:04 +0200

    [core] add repository get/search Java API

---
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/request/repository/RepositorySearchRequestBuilder.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.datastore.request.repository;
+
+import com.b2international.snowowl.core.Repositories;
+import com.b2international.snowowl.core.ServiceProvider;
+import com.b2international.snowowl.datastore.request.SearchResourceRequest;
+import com.b2international.snowowl.datastore.request.SearchResourceRequestBuilder;
+import com.b2international.snowowl.datastore.request.SystemRequestBuilder;
+
+/**
+ * @since 5.8
+ */
+public final class RepositorySearchRequestBuilder extends SearchResourceRequestBuilder<RepositorySearchRequestBuilder, ServiceProvider, Repositories> 
+		implements SystemRequestBuilder<Repositories>  {
+
+	@Override
+	protected SearchResourceRequest<ServiceProvider, Repositories> createSearch() {
+		return new RepositorySearchRequest();
+	}
+
+}
