fccc779782be71edb0aa349f6321bad89cfe208d
Balazs Banfai <bbanfai@b2international.com>
2017-07-09 13:46:26 +0200

    Added javadoc for the limit directive.

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/domain/refset/SnomedReferenceSet.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/domain/refset/SnomedReferenceSet.java
@@ -34,6 +34,9 @@ import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetType;
  * <li>{@code members()} - returns the members of this reference set as part of the reference set</li>
  * </ul>
  * 
+ * The number of expanded fields can be controlled with the {@code limit:} directive. For example:
+ * <p>members(limit:Integer.MAX_VALUE)<p>
+ *
  * Expand parameters can be nested to further expand or filter the details returned. 
  * 
  * @see SnomedConcept
