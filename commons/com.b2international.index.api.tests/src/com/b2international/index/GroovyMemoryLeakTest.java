6255f9f9a7f9be7f20c2df637a1728602093d703
Mark Czotter <mczotter@b2international.com>
2017-08-18 11:19:02 +0200

    [index.lucene] document groovy memory leak test

--- commons/com.b2international.index.api.tests/src/com/b2international/index/GroovyMemoryLeakTest.java
+++ commons/com.b2international.index.api.tests/src/com/b2international/index/GroovyMemoryLeakTest.java
@@ -27,6 +27,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.RandomStringUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.b2international.index.Fixtures.Data;
@@ -36,12 +37,27 @@ import com.b2international.index.query.SortBy;
 import com.google.common.collect.ImmutableList;
 
 /**
+ * Test to verify that Groovy script execution in the Lucene implementation of the Index API does not produce huge amount of compiled script classes and constant GC allocation failures (even Metaspace) and eventually unresponsiveness of the entire server.
+ * 
+ * To detect the memory leak:
+ * 1. Use 5.10.10 version of Snow Owl
+ * 2. Run this test via the provided launch configuration
+ * 3. Use the GC log output to detect the mem leak (or use an external JVM monitoring app, like jvisualvm)
+ * 4. After the first/second GC in the Metaspace area the JVM will enter into a state, where it constantly tries to GC the Metaspace area, but it can't free up space at all.  
+ * 
+ * To verify the memory leak:
+ * 1. Use 5.10.11 version of Snow Owl (or apply the fix from the corresponding commit)
+ * 2. Run this test via the provided launch configuration
+ * 3. Use the GC log output to detect that there is no Metaspace GC at all (or use an external JVM monitoring app, like jvisualvm)
+ * 4. The fix reduces the amount of runtime generated Script classes, so Metaspace won't fill up and the JVM won't enter into the above observed state
+ * 
  * @since 5.10.11
  */
 public class GroovyMemoryLeakTest extends BaseIndexTest {
 
 	private static final int NUM_DOCS = 10_000;
 	
+	@Ignore
 	@Test
 	public void tryToGenerateMemoryLeak() throws Exception {
 		final List<String> orderedItems = newArrayList(); 
@@ -75,12 +91,15 @@ public class GroovyMemoryLeakTest extends BaseIndexTest {
 			}
 		};
 
+		// run 4 threads to simulate a bit higher load on the index
 		executor.submit(theQuery, null);
 		executor.submit(theQuery, null);
 		executor.submit(theQuery, null);
 		executor.submit(theQuery, null);
-		
+
 		executor.shutdown();
+		// this won't pass at all, even if the fix is applied
+		// the purpose of this test to detect and verify the GC via external monitoring thus it cannot be automated properly
 		assertTrue(executor.awaitTermination(5, TimeUnit.MINUTES));
 	}
 
