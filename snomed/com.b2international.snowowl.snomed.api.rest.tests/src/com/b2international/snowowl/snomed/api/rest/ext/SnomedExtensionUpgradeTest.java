ec849ec6cdc7dbc7adb5949455b7c0ec7e89a97a
Gabor Nagy <gnagy@b2international.com>
2017-10-19 14:31:42 +0200

    APDS-131: Add preliminary test case for donated concept
    https://jira.ihtsdotools.org/browse/APDS-131

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/ext/SnomedExtensionUpgradeTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/ext/SnomedExtensionUpgradeTest.java
@@ -22,9 +22,12 @@ import static com.b2international.snowowl.snomed.api.rest.CodeSystemVersionRestR
 import static com.b2international.snowowl.snomed.api.rest.SnomedBranchingRestRequests.createBranch;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentRestRequests.createComponent;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentRestRequests.getComponent;
+import static com.b2international.snowowl.snomed.api.rest.SnomedRestFixtures.DEFAULT_LANGUAGE_CODE;
 import static com.b2international.snowowl.snomed.api.rest.SnomedRestFixtures.createNewConcept;
 import static com.b2international.snowowl.snomed.api.rest.SnomedRestFixtures.merge;
+import static com.b2international.snowowl.test.commons.rest.RestExtensions.lastPathSegment;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Map;
 
@@ -42,8 +45,11 @@ import com.b2international.snowowl.snomed.api.rest.SnomedApiTestConstants;
 import com.b2international.snowowl.snomed.api.rest.SnomedComponentType;
 import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants;
 import com.b2international.snowowl.snomed.core.domain.CaseSignificance;
+import com.b2international.snowowl.snomed.core.domain.CharacteristicType;
 import com.b2international.snowowl.snomed.datastore.SnomedDatastoreActivator;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.jayway.restassured.response.ValidatableResponse;
 
 /**
  * @since 4.7
@@ -167,6 +173,132 @@ public class SnomedExtensionUpgradeTest extends AbstractSnomedApiTest {
 		merge(branchPath, targetPath, "Upgraded B2i extension to v4.").body("status", equalTo(Merge.Status.CONFLICTS.name()));
 	}
 
+	@Test
+	public void upgradeWithDonatedContent() {
+		
+		Map<?, ?> statedRelationshipRequestBody = ImmutableMap.builder()
+				.put("moduleId", Concepts.MODULE_B2I_EXTENSION)
+				.put("namespaceId", Concepts.B2I_NAMESPACE)
+				.put("typeId", Concepts.IS_A)
+				.put("destinationId", Concepts.ROOT_CONCEPT)
+				.put("characteristicType", CharacteristicType.STATED_RELATIONSHIP)
+				.build();
+		
+		Map<?, ?> inferredRelationshipRequestBody = ImmutableMap.builder()
+				.put("moduleId", Concepts.MODULE_B2I_EXTENSION)
+				.put("namespaceId", Concepts.B2I_NAMESPACE)
+				.put("typeId", Concepts.IS_A)
+				.put("destinationId", Concepts.ROOT_CONCEPT)
+				.put("characteristicType", CharacteristicType.INFERRED_RELATIONSHIP)
+				.build();
+
+		String fsnTerm = "FSN of concept";
+		
+		Map<?, ?> fsnRequestBody = ImmutableMap.builder()
+				.put("moduleId", Concepts.MODULE_B2I_EXTENSION)
+				.put("namespaceId", Concepts.B2I_NAMESPACE)
+				.put("typeId", Concepts.FULLY_SPECIFIED_NAME)
+				.put("term", fsnTerm)
+				.put("languageCode", DEFAULT_LANGUAGE_CODE)
+				.put("acceptability", SnomedApiTestConstants.UK_PREFERRED_MAP)
+				.build();
+		
+		String ptTerm = "PT of donated concept";
+		
+		Map<?, ?> ptRequestBody = ImmutableMap.builder()
+				.put("moduleId", Concepts.MODULE_B2I_EXTENSION)
+				.put("namespaceId", Concepts.B2I_NAMESPACE)
+				.put("typeId", Concepts.SYNONYM)
+				.put("term", ptTerm)
+				.put("languageCode", DEFAULT_LANGUAGE_CODE)
+				.put("acceptability", SnomedApiTestConstants.UK_PREFERRED_MAP)
+				.build();
+		
+		String synonymTerm = "Synonym of donated concept";
+		
+		Map<?, ?> synonymRequestBody = ImmutableMap.builder()
+				.put("moduleId", Concepts.MODULE_B2I_EXTENSION)
+				.put("namespaceId", Concepts.B2I_NAMESPACE)
+				.put("typeId", Concepts.SYNONYM)
+				.put("term", synonymTerm)
+				.put("languageCode", DEFAULT_LANGUAGE_CODE)
+				.put("acceptability", SnomedApiTestConstants.UK_ACCEPTABLE_MAP)
+				.build();
+
+		Map<String, Object> conceptRequestBody = ImmutableMap.<String, Object>builder()
+				.put("moduleId", Concepts.MODULE_B2I_EXTENSION)
+				.put("namespaceId", Concepts.B2I_NAMESPACE)
+				.put("descriptions", ImmutableList.of(fsnRequestBody, ptRequestBody, synonymRequestBody))
+				.put("relationships", ImmutableList.of(statedRelationshipRequestBody, inferredRelationshipRequestBody))
+				.put("commitComment", "Added donated concept")
+				.build();
+		
+		String donatedConceptId = lastPathSegment(createComponent(branchPath, SnomedComponentType.CONCEPT, conceptRequestBody)
+				.statusCode(201)
+				.extract().header("Location"));
+		
+		Map<?, ?> intStatedRelationshipRequestBody = ImmutableMap.builder()
+				.put("moduleId", Concepts.MODULE_SCT_CORE)
+				.put("typeId", Concepts.IS_A)
+				.put("destinationId", Concepts.ROOT_CONCEPT)
+				.put("characteristicType", CharacteristicType.STATED_RELATIONSHIP)
+				.build();
+		
+		Map<?, ?> intInferredRelationshipRequestBody = ImmutableMap.builder()
+				.put("moduleId", Concepts.MODULE_SCT_CORE)
+				.put("typeId", Concepts.IS_A)
+				.put("destinationId", Concepts.ROOT_CONCEPT)
+				.put("characteristicType", CharacteristicType.INFERRED_RELATIONSHIP)
+				.build();
+
+		Map<?, ?> intFsnRequestBody = ImmutableMap.builder()
+				.put("moduleId", Concepts.MODULE_SCT_CORE)
+				.put("typeId", Concepts.FULLY_SPECIFIED_NAME)
+				.put("term", fsnTerm)
+				.put("languageCode", DEFAULT_LANGUAGE_CODE)
+				.put("acceptability", SnomedApiTestConstants.UK_PREFERRED_MAP)
+				.build();
+		
+		Map<?, ?> intPtRequestBody = ImmutableMap.builder()
+				.put("moduleId", Concepts.MODULE_SCT_CORE)
+				.put("typeId", Concepts.SYNONYM)
+				.put("term", ptTerm)
+				.put("languageCode", DEFAULT_LANGUAGE_CODE)
+				.put("acceptability", SnomedApiTestConstants.UK_PREFERRED_MAP)
+				.build();
+
+		Map<String, Object> intConceptRequestBody = ImmutableMap.<String, Object>builder()
+				.put("id", donatedConceptId)
+				.put("moduleId", Concepts.MODULE_SCT_CORE)
+				.put("descriptions", ImmutableList.of(intFsnRequestBody, intPtRequestBody))
+				.put("relationships", ImmutableList.of(intStatedRelationshipRequestBody, intInferredRelationshipRequestBody))
+				.put("commitComment", "Added donated concept to INT")
+				.build();
+		
+		String intConceptId = lastPathSegment(createComponent(BranchPathUtils.createMainPath(), SnomedComponentType.CONCEPT, intConceptRequestBody)
+				.statusCode(201)
+				.body(equalTo(""))
+				.extract().header("Location"));
+		
+		assertEquals(donatedConceptId, intConceptId);
+		
+		String effectiveDate = getNextAvailableEffectiveDateAsString(SnomedTerminologyComponentConstants.SNOMED_SHORT_NAME);
+		String versionId = "v5";
+		createVersion(SnomedTerminologyComponentConstants.SNOMED_SHORT_NAME, versionId, effectiveDate).statusCode(201);
+		
+		IBranchPath targetPath = BranchPathUtils.createPath(SnomedApiTestConstants.PATH_JOINER.join(
+				Branch.MAIN_PATH, 
+				versionId, 
+				SnomedTerminologyComponentConstants.SNOMED_B2I_SHORT_NAME));
+
+		createBranch(targetPath).statusCode(201);
+		
+		ValidatableResponse response = merge(branchPath, targetPath, "Upgraded B2i extension to v5");
+		response.body("status", equalTo(Merge.Status.CONFLICTS.name()));
+		
+	}
+	
+	
 	@AfterClass
 	public static void restoreB2iCodeSystem() {
 		Map<?, ?> updateRequest = ImmutableMap.builder()
