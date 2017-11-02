433826bda490365e92ffb9898fd884f70c8bbbf3
Alina Al-Aswadi <aalaswadi@b2international.com>
2017-11-02 14:26:05 +0100

    Merge 4.x utility methods into 5.x ClassUtils

--- commons/com.b2international.commons.base/src/com/b2international/commons/ClassUtils.java
+++ commons/com.b2international.commons.base/src/com/b2international/commons/ClassUtils.java
@@ -18,6 +18,10 @@ package com.b2international.commons;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+
 /**
  * Collection of utility functions related to classes and interfaces.
  *  
@@ -50,6 +54,54 @@ public class ClassUtils {
 		return match;
 	}
 	
+	  /**
+     * Finds the type parameter for the given class.
+     *
+     * @param klass    a parameterized class
+     * @return the class's type parameter
+     */
+    public static Class<?> getTypeParameter(Class<?> klass) {
+        return getTypeParameter(klass, Object.class);
+    }
+
+    /**
+     * Finds the type parameter for the given class which is assignable to the bound class.
+     *
+     * @param klass    a parameterized class
+     * @param bound    the type bound
+     * @param <T>      the type bound
+     * @return the class's type parameter
+     */
+    public static <T> Class<T> getTypeParameter(Class<?> klass, Class<? super T> bound) {
+        Type t = checkNotNull(klass);
+        while (t instanceof Class<?>) {
+            t = ((Class<?>) t).getGenericSuperclass();
+        }
+        /* This is not guaranteed to work for all cases with convoluted piping
+         * of type parameters: but it can at least resolve straight-forward
+         * extension with single type parameter.
+         * And when it fails to do that, will indicate with specific exception.
+         */
+        if (t instanceof ParameterizedType) {
+            // should typically have one of type parameters (first one) that matches:
+            for (Type param : ((ParameterizedType) t).getActualTypeArguments()) {
+                if (param instanceof Class<?>) {
+                    final Class<T> cls = determineClass(bound, param);
+                    if (cls != null) { return cls; }
+                }
+                else if (param instanceof TypeVariable) {
+                    for (Type paramBound : ((TypeVariable<?>) param).getBounds()) {
+                        if (paramBound instanceof Class<?>) {
+                            final Class<T> cls = determineClass(bound, paramBound);
+                            if (cls != null) { return cls; }
+                        }
+                    }
+                }
+            }
+        }
+        throw new IllegalStateException("Cannot figure out type parameterization for " + klass.getName());
+    }
+	
 	public static boolean isInstanceOrNull(Class<?> clazz, Object object) {
 		return object == null || clazz.isInstance(object);
 	}
@@ -60,5 +112,16 @@ public class ClassUtils {
 		checkArgument(targetClass.isInstance(object), "Object must be an instance of %s but was a %s.", targetClass.getSimpleName(), object.getClass().getSimpleName());
 		return targetClass.cast(object);
 	}
+	
+	 @SuppressWarnings("unchecked")
+	    private static <T> Class<T> determineClass(Class<? super T> bound, Type candidate) {
+	        if (candidate instanceof Class<?>) {
+	            final Class<?> cls = (Class<?>) candidate;
+	            if (bound.isAssignableFrom(cls)) {
+	                return (Class<T>) cls;
+	            }
+	        }
+	        return null;
+	    }
 	
 }
