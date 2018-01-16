f4e9cd8690ccc66a810ebcbf4e13973bc4b0c6fe
Zoltan Molnar <zmolnar@b2international.com>
2018-01-16 11:51:16 +0100

    [fix] Removed module dependency logic from the precommit state

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/SnomedRefSetEditingContext.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/SnomedRefSetEditingContext.java
@@ -62,7 +62,6 @@ import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRefSetMemb
 import com.b2international.snowowl.snomed.datastore.request.SnomedRefSetMemberSearchRequestBuilder;
 import com.b2international.snowowl.snomed.datastore.request.SnomedRequests;
 import com.b2international.snowowl.snomed.datastore.services.ISnomedConceptNameProvider;
-import com.b2international.snowowl.snomed.datastore.services.SnomedModuleDependencyRefSetService;
 import com.b2international.snowowl.snomed.snomedrefset.DataType;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedAssociationRefSetMember;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedAttributeValueRefSetMember;
@@ -717,13 +716,6 @@ public class SnomedRefSetEditingContext extends BaseSnomedEditingContext {
 	
 	@Override
 	public void preCommit() {
-		/*
-		 * Updates the module dependency refset members based on the changes. Source or target
-		 * effective time is set to null if the changed component module id has dependency in
-		 * the refset.
-		 */
-		SnomedModuleDependencyRefSetService dependencyRefSetService = new SnomedModuleDependencyRefSetService();
-		dependencyRefSetService.updateModuleDependenciesDuringPreCommit(getTransaction());
 	}
 	
 	@Override
