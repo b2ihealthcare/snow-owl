c47ba8848b0c14e1050863ca431420c22e582d5b
Mark Czotter <mczotter@b2international.com>
2018-01-26 15:02:00 +0100

    [core] fix class loading issues...
    ...when executing script based validation rules

--- core/com.b2international.snowowl.datastore.server/src/com/b2international/snowowl/datastore/server/RepositoryClassLoaderProviderRegistry.java
+++ core/com.b2international.snowowl.datastore.server/src/com/b2international/snowowl/datastore/server/RepositoryClassLoaderProviderRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2015 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2018 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,14 +15,10 @@
  */
 package com.b2international.snowowl.datastore.server;
 
-import com.b2international.snowowl.core.ClassLoaderProvider;
-
 /**
  * @since 4.5
  */
 public interface RepositoryClassLoaderProviderRegistry {
-	
-	ClassLoaderProvider get(String repositoryId);
 	
 	ClassLoader getClassLoader();
 
