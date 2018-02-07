607c94efbccf76e759f5fbc6d0545bfd9ccbaf6d
Gabor Nagy <gnagy@b2international.com>
2018-02-07 13:46:43 +0100

    [snomed.tests] Add test case for updating existing module dependency...
    ...members

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/components/SnomedModuleDependencyRefsetTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/components/SnomedModuleDependencyRefsetTest.java
@@ -5,7 +5,11 @@ import static com.b2international.snowowl.snomed.api.rest.CodeSystemVersionRestR
 import static com.b2international.snowowl.snomed.api.rest.CodeSystemVersionRestRequests.getNextAvailableEffectiveDateAsString;
 import static com.b2international.snowowl.snomed.api.rest.CodeSystemVersionRestRequests.getVersion;
 import static com.b2international.snowowl.snomed.api.rest.SnomedComponentRestRequests.createComponent;
+import static com.b2international.snowowl.snomed.api.rest.SnomedComponentRestRequests.getComponent;
 import static com.b2international.snowowl.snomed.api.rest.SnomedRestFixtures.createConceptRequestBody;
+import static com.b2international.snowowl.test.commons.rest.RestExtensions.lastPathSegment;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
@@ -15,8 +19,10 @@ import org.eclipse.xtext.util.Pair;
 import org.eclipse.xtext.util.Tuples;
 import org.junit.Test;
 
+import com.b2international.snowowl.core.api.IBranchPath;
 import com.b2international.snowowl.core.date.DateFormats;
 import com.b2international.snowowl.core.date.EffectiveTimes;
+import com.b2international.snowowl.datastore.BranchPathUtils;
 import com.b2international.snowowl.snomed.SnomedConstants.Concepts;
 import com.b2international.snowowl.snomed.api.rest.AbstractSnomedApiTest;
 import com.b2international.snowowl.snomed.api.rest.SnomedApiTestConstants;
@@ -34,7 +40,54 @@ public class SnomedModuleDependencyRefsetTest extends AbstractSnomedApiTest {
 
 	private static final String NORWEGIAN_MODULE_CONCEPT_ID = "51000202101";
 	
+	@Test
+	public void updateExistingModuleDependencyMembers() {
 
+		IBranchPath mainPath = BranchPathUtils.createMainPath();
+
+		Map<?, ?> conceptRequestBody = createConceptRequestBody(Concepts.MODULE_ROOT, Concepts.MODULE_SCT_CORE, // it must belong to the core module
+				SnomedApiTestConstants.UK_PREFERRED_MAP)
+				.put("commitComment", "Created concept with INT core module")
+				.build();
+
+		String conceptId = lastPathSegment(createComponent(mainPath, SnomedComponentType.CONCEPT, conceptRequestBody)
+				.statusCode(201)
+				.body(equalTo(""))
+				.extract().header("Location"));
+
+		getComponent(mainPath, SnomedComponentType.CONCEPT, conceptId)
+			.statusCode(200)
+			.body("effectiveTime", nullValue());
+
+		// version branch
+		final String versionEffectiveTime = getNextAvailableEffectiveDateAsString(
+				SnomedTerminologyComponentConstants.SNOMED_SHORT_NAME);
+		
+		final Date versionEffectiveDate = EffectiveTimes.parse(versionEffectiveTime, DateFormats.SHORT);
+		
+		final String versionId = "updateExistingModuleDependencyMembers";
+		createVersion(SnomedTerminologyComponentConstants.SNOMED_SHORT_NAME, versionId, versionEffectiveTime).statusCode(201);
+		getVersion(SnomedTerminologyComponentConstants.SNOMED_SHORT_NAME, versionId).statusCode(200);
+
+		getComponent(mainPath, SnomedComponentType.CONCEPT, conceptId)
+			.statusCode(200)
+			.body("effectiveTime", equalTo(versionEffectiveTime));
+		
+		SnomedReferenceSetMembers coreModuleDependencyMembersAfterVersioning = SnomedRequests.prepareSearchMember()
+				.all()
+				.filterByActive(true)
+				.filterByModule(Concepts.MODULE_SCT_CORE) // filter for members where the module id is the SCT core module
+				.filterByRefSet(Concepts.REFSET_MODULE_DEPENDENCY_TYPE)
+				.build(SnomedDatastoreActivator.REPOSITORY_UUID, mainPath.getPath())
+				.execute(getBus())
+				.getSync();
+
+		coreModuleDependencyMembersAfterVersioning.forEach(member -> {
+			assertEquals("Effective time must be updated after versioning", versionEffectiveDate, member.getEffectiveTime());
+		});
+		
+	}
+	
 	@Test
 	public void moduleDependecyDuplicateMemberTest() {
 
