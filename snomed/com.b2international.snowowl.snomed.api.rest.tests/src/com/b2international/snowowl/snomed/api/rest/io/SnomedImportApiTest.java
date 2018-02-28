0534a6b95accb3cb10bf5469608c361aaf24aa36
Gabor Nagy <gnagy@b2international.com>
2018-02-28 11:35:03 +0100

    SO-2902: Simplify test case
    https://snowowl.atlassian.net/browse/SO-2902

--- snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/io/SnomedImportApiTest.java
+++ snomed/com.b2international.snowowl.snomed.api.rest.tests/src/com/b2international/snowowl/snomed/api/rest/io/SnomedImportApiTest.java
@@ -306,24 +306,12 @@ public class SnomedImportApiTest extends AbstractSnomedApiTest {
 		final String svDescriptionId = "24688171113";
 		final String enLanguageRefsetMemberId = "34d07985-48a0-41e7-b6ec-b28e6b00adfc";
 		final String svLanguageRefsetMemberId = "34d07985-48a0-41e7-b6ec-b28e6b00adfb";
-		final String conceptIdOfDescription = "301795004";
 		
 		getComponent(branchPath, SnomedComponentType.DESCRIPTION, enDescriptionId).statusCode(404);
 		getComponent(branchPath, SnomedComponentType.DESCRIPTION, svDescriptionId).statusCode(404);
 		getComponent(branchPath, SnomedComponentType.MEMBER, enLanguageRefsetMemberId).statusCode(404);
 		getComponent(branchPath, SnomedComponentType.MEMBER, svLanguageRefsetMemberId).statusCode(404);
-		getComponent(branchPath, SnomedComponentType.CONCEPT, conceptIdOfDescription).statusCode(404);
-		
-		final Map<?, ?> importConfiguration = ImmutableMap.builder()
-				.put("type", Rf2ReleaseType.DELTA.name())
-				.put("branchPath",  branchPath.getPath())
-				.put("createVersions", false)
-				.build();
-		
-		importArchive("SnomedCT_Release_INT_20150131_new_concept_for_languageCode.zip");
-		getComponent(branchPath, SnomedComponentType.CONCEPT, conceptIdOfDescription).statusCode(200);
-		
-		importArchive("SnomedCT_Release_INT_20150201_new_description_with2_language_code.zip", importConfiguration);
+		importArchive("SnomedCT_Release_INT_20150201_descriptions_with_multiple_language_codes.zip");
 		getComponent(branchPath, SnomedComponentType.DESCRIPTION, enDescriptionId).statusCode(200);
 		getComponent(branchPath, SnomedComponentType.DESCRIPTION, svDescriptionId).statusCode(200);
 		getComponent(branchPath, SnomedComponentType.MEMBER, enLanguageRefsetMemberId).statusCode(200);
