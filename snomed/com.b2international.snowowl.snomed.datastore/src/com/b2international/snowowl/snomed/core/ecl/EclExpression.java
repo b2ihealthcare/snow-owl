eb3f59d3ad93ced85a36be6a58a7a82dd4b70da0
Mark Czotter <mczotter@b2international.com>
2018-03-13 17:48:59 +0100

    [ecl] synchronously evaluate String ECL expression to Expression object

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/ecl/EclExpression.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/ecl/EclExpression.java
@@ -98,9 +98,8 @@ public final class EclExpression {
 	public Promise<Expression> resolveToExpression(final BranchContext context) {
 		if (expressionPromise == null) {
 			expressionPromise = SnomedRequests.prepareEclEvaluation(ecl)
-					.build(context.id(), context.branchPath())
-					.execute(context.service(IEventBus.class))
-					.thenWith(result -> result);
+					.build()
+					.execute(context);
 		}
 		return expressionPromise;
 	}
