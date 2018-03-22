9271942ee31bf5b43f82949b222bd88ad5f186ec
Zoltan Molnar <zmolnar@b2international.com>
2018-03-22 10:21:16 +0100

    SO-2929: Refactored rule to use ID-s only, and to use scrolling as well.

--- com.b2international.snowowl.validation.snomed/src/main/resources/scripts/ruleSnomedCommon1.groovy
+++ com.b2international.snowowl.validation.snomed/src/main/resources/scripts/ruleSnomedCommon1.groovy
@@ -1,66 +1,56 @@
-import com.b2international.index.aggregations.Aggregation
-import com.b2international.index.aggregations.AggregationBuilder
-import com.b2international.index.aggregations.Bucket
+import com.b2international.index.Hits
 import com.b2international.index.query.Expression
-import java.util.stream.Collectors
+import com.b2international.index.query.Expressions
+import com.b2international.index.query.Query
 import com.b2international.index.revision.RevisionSearcher
 import com.b2international.snowowl.core.ComponentIdentifier
 import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants
-import com.b2international.snowowl.snomed.core.domain.SnomedConcepts
-import com.b2international.snowowl.snomed.core.domain.SnomedRelationships
+import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptDocument
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRelationshipIndexEntry
-import com.b2international.snowowl.snomed.datastore.request.SnomedRequests
-import com.b2international.snowowl.core.domain.IComponent
 import com.google.common.collect.Sets
 
-Set<ComponentIdentifier> invalidIds = Sets.newHashSet()
+Iterable<Hits<String>> inactiveConceptHits = ctx.service(RevisionSearcher.class)
+		.scroll(Query.select(String.class)
+		.from(SnomedConceptDocument.class)
+		.fields(SnomedConceptDocument.Fields.ID)
+		.where(SnomedConceptDocument.Expressions.active(false))
+		.limit(100_099)
+		.build())
+		
+Collection<String> inactiveConceptIds = Sets.newHashSet();
+inactiveConceptHits.each( { hits -> 
+	for (String hit : hits) {
+		inactiveConceptIds.add(hit);
+	}
+})
+		
+Expression shouldrelationshipExpression = Expressions.builder()
+		.should(SnomedRelationshipIndexEntry.Expressions.sourceIds(inactiveConceptIds))
+		.should(SnomedRelationshipIndexEntry.Expressions.typeIds(inactiveConceptIds))
+		.should(SnomedRelationshipIndexEntry.Expressions.destinationIds(inactiveConceptIds))
+		.build()
+		
+Expression expression = Expressions.builder()
+		.should(shouldrelationshipExpression)
+		.filter(SnomedRelationshipIndexEntry.Expressions.active())
+		.build()
 
-SnomedConcepts inactiveConcepts = SnomedRequests.prepareSearchConcept()
-	.all()
-	.filterByActive(false)
-	.build()
-	.execute(ctx)	
-	
-def inactiveConceptIds = inactiveConcepts.stream().map({comp -> comp.getId()}).collect(Collectors.toSet());
-
-SnomedRelationships invalidSourceRelationships = SnomedRequests.prepareSearchRelationship()
-	.all()
-	.filterBySource(inactiveConceptIds)
-	.filterByActive(true)
-	.build()
-	.execute(ctx)
-	
-if (invalidSourceRelationships.getTotal() > 0) {
-	def invalidRelationshipComponents = invalidSourceRelationships.stream().map({comp -> ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, comp.getId())}).collect(Collectors.toSet())
-	invalidIds.addAll(invalidRelationshipComponents)
-}
-
-SnomedRelationships invalidDestionationRelationships = SnomedRequests.prepareSearchRelationship()
-	.all()
-	.filterByDestination(inactiveConceptIds)
-	.filterByActive(true)
-	.build()
-	.execute(ctx)
-	
-if (invalidDestionationRelationships.getTotal() > 0) {
-	def invalidRelationshipComponents = invalidDestionationRelationships.stream().map({comp -> ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, comp.getId())}).collect(Collectors.toSet())
-	invalidIds.addAll(invalidRelationshipComponents)
-}
-
-SnomedRelationships invalidTypeRelationships = SnomedRequests.prepareSearchRelationship()
-	.all()
-	.filterByType(inactiveConceptIds)
-	.filterByActive(true)
-	.build()
-	.execute(ctx)
-	
-if (invalidTypeRelationships.getTotal() > 0) {
-	def invalidRelationshipComponents = invalidTypeRelationships.stream().map({comp -> ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, comp.getId())}).collect(Collectors.toSet())
-	invalidIds.addAll(invalidRelationshipComponents)
-}
-
-
-return new ArrayList<>(invalidIds)
+Iterable<Hits<String>> invalidRelationshipHits = ctx.service(RevisionSearcher.class)
+		.scroll(Query.select(String.class)
+		.from(SnomedRelationshipIndexEntry.class)
+		.fields(SnomedRelationshipIndexEntry.Fields.ID)
+		.where(expression)
+		.limit(10_000)
+		.build())
+		
+Collection<ComponentIdentifier> invalidRelationshipIds = Sets.newHashSet();
+invalidRelationshipHits.each( { hits ->
+	for (String hit : hits) {
+		invalidRelationshipIds.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, hit));
+	}
+})
+		
+return new ArrayList<>(invalidRelationshipIds)
 
 
 		
