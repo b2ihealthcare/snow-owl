e4032966e5032be852619d06b375708b2e87d17b
Mark Czotter <mczotter@b2international.com>
2018-03-29 15:15:21 +0200

    SO-2917: allow encoded slash in URLs

--- core/com.b2international.snowowl.fhir.api/src/com/b2international/snowowl/fhir/api/FhirApiSecurityConfig.java
+++ core/com.b2international.snowowl.fhir.api/src/com/b2international/snowowl/fhir/api/FhirApiSecurityConfig.java
@@ -16,12 +16,16 @@
 package com.b2international.snowowl.fhir.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 /**
  * @since 6.4
@@ -33,6 +37,13 @@ public class FhirApiSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthenticationProvider authenticationProvider;
 
+	@Bean
+	public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+	    StrictHttpFirewall firewall = new StrictHttpFirewall();
+	    firewall.setAllowUrlEncodedSlash(true);
+	    return firewall;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -50,6 +61,12 @@ public class FhirApiSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) {
 		auth.authenticationProvider(authenticationProvider);
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		super.configure(web);
+		web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
 	}
 
 }
