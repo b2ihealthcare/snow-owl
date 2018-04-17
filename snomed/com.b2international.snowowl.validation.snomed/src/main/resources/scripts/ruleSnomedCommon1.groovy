791acc178f008efab2a07a5fdc2acbc278da78bc
Mark Czotter <mczotter@b2international.com>
2018-04-17 16:28:02 +0200

    [validation] fix return type of ruleSnomedCommon1.groovy script
    Fix executed test class in pom.xml.

--- snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/ruleSnomedCommon1.groovy
+++ snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/ruleSnomedCommon1.groovy
@@ -4,9 +4,12 @@ import com.b2international.index.query.Expressions
 import com.b2international.index.query.Query
 import com.b2international.index.revision.RevisionSearcher
 import com.b2international.snowowl.core.ComponentIdentifier
+import com.b2international.snowowl.core.validation.issue.IssueDetail
+import com.b2international.snowowl.snomed.common.SnomedRf2Headers
 import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptDocument
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRelationshipIndexEntry
+import com.google.common.collect.ImmutableMap
 import com.google.common.collect.Sets
 
 Hits<String> inactiveConceptHits = ctx.service(RevisionSearcher.class)
@@ -27,18 +30,26 @@ Expression expression = Expressions.builder()
 		.build()
 		
 
-Iterable<Hits<String>> invalidRelationshipHits = ctx.service(RevisionSearcher.class)
-		.scroll(Query.select(String.class)
+Iterable<Hits<String[]>> invalidRelationshipHits = ctx.service(RevisionSearcher.class)
+		.scroll(Query.select(String[].class)
 		.from(SnomedRelationshipIndexEntry.class)
-		.fields(SnomedRelationshipIndexEntry.Fields.ID)
+		.fields(SnomedRelationshipIndexEntry.Fields.ID, SnomedRelationshipIndexEntry.Fields.MODULE_ID)
 		.where(expression)
 		.limit(10_000)
 		.build())
 		
-Collection<ComponentIdentifier> invalidRelationshipIds = Sets.newHashSet();
+Collection<IssueDetail> invalidRelationshipIds = Sets.newHashSet();
 invalidRelationshipHits.each( { hits ->
-	for (String hit : hits) {
-		invalidRelationshipIds.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, hit));
+	for (String[] hit : hits) {
+		invalidRelationshipIds.add(
+			new IssueDetail(
+				ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, hit[0]),
+				ImmutableMap.of(
+					SnomedRf2Headers.FIELD_ACTIVE, true,
+					SnomedRf2Headers.FIELD_MODULE_ID, hit[1]
+				) 
+			)
+		);
 	}
 })
 		
