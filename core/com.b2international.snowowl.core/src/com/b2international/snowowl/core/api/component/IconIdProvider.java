5c33848f2123ef91058812a250a85f84c30a6cac
Tamas Mucs <tmucs@b2international.com>
2018-05-03 13:54:53 +0200

    SO-2875 implement IconIdProvider, put override annot. where applicable

---
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/api/component/IconIdProvider.java
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2018 B2i Healthcare. All rights reserved.
+ *******************************************************************************/
+package com.b2international.snowowl.core.api.component;
+
+/**
+ * @since 6.5
+ */
+public interface IconIdProvider {
+	
+	public String getIconId();
+	
+}
