95464737ddfdd4ada654d06fda13cfd0ad3ee691
Mark Czotter <mczotter@b2international.com>
2018-05-04 14:41:50 +0200

    [snomed] include refSet properties when creating new doc from domain obj

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/SnomedRefSetLookupService.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/SnomedRefSetLookupService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2018 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import com.b2international.snowowl.core.exceptions.NotFoundException;
 import com.b2international.snowowl.datastore.AbstractLookupService;
 import com.b2international.snowowl.datastore.cdo.CDOUtils;
 import com.b2international.snowowl.eventbus.IEventBus;
-import com.b2international.snowowl.snomed.core.domain.refset.SnomedReferenceSet;
+import com.b2international.snowowl.snomed.core.domain.SnomedDescription;
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptDocument;
 import com.b2international.snowowl.snomed.datastore.request.SnomedRequests;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSet;
@@ -37,19 +37,18 @@ public final class SnomedRefSetLookupService extends AbstractLookupService<Snome
 	@Override
 	public SnomedConceptDocument getComponent(final IBranchPath branchPath, final String id) {
 		try {
-			final IEventBus bus = ApplicationContext.getServiceForClass(IEventBus.class);
-			final SnomedReferenceSet refSet = SnomedRequests
-					.prepareGetReferenceSet(id)
+			return SnomedRequests.prepareGetConcept(id)
+					.setExpand("referenceSet()")
 					.build(SnomedDatastoreActivator.REPOSITORY_UUID, branchPath.getPath())
-					.execute(bus)
+					.execute(ApplicationContext.getServiceForClass(IEventBus.class))
+					.then(concept -> {
+						final SnomedDescription pt = concept.getPt();
+						final String preferredTerm = pt == null ? concept.getId() : pt.getTerm();
+						return SnomedConceptDocument.builder(concept)
+								.label(preferredTerm)
+								.build();
+					})
 					.getSync();
-			
-			final SnomedConceptDocument concept = new SnomedConceptLookupService().getComponent(branchPath, id);
-			
-			return SnomedConceptDocument
-					.builder(concept)
-					.refSet(refSet)
-					.build();
 		} catch (NotFoundException e) {
 			return null;
 		}
