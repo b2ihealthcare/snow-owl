a6510b78d0f2c402a5a1be6c68a90f888521da41
András Péteri <apeteri@b2international.com>
2018-05-08 18:40:22 +0200

    SO-3045: Move utility method out of PredicateUtils, remove commented...
    ...section of code

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/PredicateUtils.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/PredicateUtils.java
@@ -17,14 +17,12 @@ package com.b2international.snowowl.snomed.datastore;
 
 import java.util.Set;
 
-import com.b2international.snowowl.snomed.SnomedConstants.Concepts;
 import com.b2international.snowowl.snomed.mrcm.CardinalityPredicate;
 import com.b2international.snowowl.snomed.mrcm.CompositeConceptSetDefinition;
 import com.b2international.snowowl.snomed.mrcm.ConceptSetDefinition;
 import com.b2international.snowowl.snomed.mrcm.EnumeratedConceptSetDefinition;
 import com.b2international.snowowl.snomed.mrcm.HierarchyConceptSetDefinition;
 import com.b2international.snowowl.snomed.mrcm.ReferenceSetConceptSetDefinition;
-import com.b2international.snowowl.snomed.mrcm.RelationshipConceptSetDefinition;
 import com.b2international.snowowl.snomed.mrcm.util.MrcmSwitch;
 
 /**
@@ -93,117 +91,6 @@ public abstract class PredicateUtils {
 			}
 		}.doSwitch(domain);
 	}
-	
-	/**
-	 * Builds an ESCG expression that represents the specified concept set definition.
-	 * <br>This method is a union of all {@link ConceptSetProcessor#getEscgExpression()} method behavior.
-	 * 
-	 * @param conceptSetDefinition the concept set definition.
-	 * @return built expression that can be processed as an ESCG expression.
-	 * 
-	 * 
-	 */
-	public static String toEclExpression(final ConceptSetDefinition conceptSetDefinition) {
-		if (conceptSetDefinition instanceof HierarchyConceptSetDefinition) {
-			final HierarchyConceptSetDefinition hierarchyConceptSetDefinition = (HierarchyConceptSetDefinition)conceptSetDefinition;
-			
-			switch (hierarchyConceptSetDefinition.getInclusionType()) {
-				
-				case SELF: 
-					return hierarchyConceptSetDefinition.getFocusConceptId();
-					
-				case DESCENDANT: 
-					return new StringBuilder(DESCENDANT).append(hierarchyConceptSetDefinition.getFocusConceptId()).toString();
-					
-				case SELF_OR_DESCENDANT: 
-					return new StringBuilder(SELF_OR_DESCENDANT).append(hierarchyConceptSetDefinition.getFocusConceptId()).toString();
-				
-				default: 
-					throw new IllegalArgumentException("Unknown inclusion type: " + hierarchyConceptSetDefinition.getInclusionType());
-			}
-		} else if (conceptSetDefinition instanceof ReferenceSetConceptSetDefinition) {
-			final ReferenceSetConceptSetDefinition referenceSetConceptSetDefinition = (ReferenceSetConceptSetDefinition) conceptSetDefinition;
-			return new StringBuilder("^").append(referenceSetConceptSetDefinition.getRefSetIdentifierConceptId()).toString();
-		} else if (conceptSetDefinition instanceof RelationshipConceptSetDefinition) {
-			final RelationshipConceptSetDefinition relationshipConceptSetDefinition = (RelationshipConceptSetDefinition) conceptSetDefinition;
-
-			final StringBuilder resultBuilder = new StringBuilder(SELF_OR_DESCENDANT);
-			resultBuilder.append(Concepts.ROOT_CONCEPT);
-			resultBuilder.append(":");
-			resultBuilder.append(relationshipConceptSetDefinition.getTypeConceptId());
-			resultBuilder.append("=");
-			resultBuilder.append(relationshipConceptSetDefinition.getDestinationConceptId());
-			return resultBuilder.toString();
-		} else if (conceptSetDefinition instanceof EnumeratedConceptSetDefinition) {
-			
-			final EnumeratedConceptSetDefinition enumeratedConceptSetDefinition = (EnumeratedConceptSetDefinition) conceptSetDefinition;
-			final StringBuilder sb = new StringBuilder();
-			
-			for (final String conceptId : enumeratedConceptSetDefinition.getConceptIds()) {
-				if (sb.length() > 0) {
-					sb.append(OR);
-				}
-				sb.append(conceptId);
-			}
-			return sb.toString();
-			
-		} else if (conceptSetDefinition instanceof CompositeConceptSetDefinition) {
-			
-			final CompositeConceptSetDefinition compositeConceptSetDefinition = (CompositeConceptSetDefinition) conceptSetDefinition;
-			final StringBuilder sb = new StringBuilder();
-			for (final ConceptSetDefinition child : compositeConceptSetDefinition.getChildren()) {
-				if (sb.length() > 0) {
-					sb.append(OR);
-				}
-				sb.append(toEclExpression(child));
-			}
-			return sb.toString();
-			
-		}
-		
-		throw new IllegalArgumentException("Unexpected concept set definition: " + conceptSetDefinition);
-		
-	}
-	
-//	public static Set<ConstraintDomain> processConstraintDomain(final long storageKey, final ConceptSetDefinition domain) {
-//
-//		final Set<ConstraintDomain> $ = Sets.newHashSet();
-//		
-//		if (domain instanceof HierarchyConceptSetDefinition) {
-//			
-//			final HierarchyConceptSetDefinition hierarchyConceptSetDefinition = (HierarchyConceptSetDefinition) domain;
-//			final Long focusConceptId = Long.valueOf(hierarchyConceptSetDefinition.getFocusConceptId());
-//			final HierarchyInclusionType inclusionType = hierarchyConceptSetDefinition.getInclusionType();
-//			$.add(new ConstraintDomain(focusConceptId, inclusionType.name(), storageKey));
-//			
-//		} else if (domain instanceof ReferenceSetConceptSetDefinition) {
-//			
-//			final ReferenceSetConceptSetDefinition referenceSetConceptSetDefinition = (ReferenceSetConceptSetDefinition) domain;
-//			final long refSetIdentifierConceptId = Long.valueOf(referenceSetConceptSetDefinition.getRefSetIdentifierConceptId());
-//			$.add(new ConstraintDomain(refSetIdentifierConceptId, REFSET_PREDICATE_KEY_PREFIX, storageKey));
-//			
-//		} else if (domain instanceof RelationshipConceptSetDefinition) {
-//			
-//			final RelationshipConceptSetDefinition relationshipConceptSetDefinition = (RelationshipConceptSetDefinition) domain;
-//			final long destinationConceptId = Long.valueOf(relationshipConceptSetDefinition.getDestinationConceptId());
-//			$.add(new ConstraintDomain(destinationConceptId, relationshipConceptSetDefinition.getTypeConceptId(), storageKey));
-//			
-//		} else if (domain instanceof EnumeratedConceptSetDefinition) {
-//			
-//			final EnumeratedConceptSetDefinition enumeratedConceptSetDefinition = (EnumeratedConceptSetDefinition) domain;
-//			for (final String conceptId : enumeratedConceptSetDefinition.getConceptIds()) {
-//				final long id = Long.valueOf(conceptId);
-//				$.add(new ConstraintDomain(id, HierarchyInclusionType.SELF.name(), storageKey));
-//			}
-//			
-//		} else if (domain instanceof CompositeConceptSetDefinition) {
-//			final CompositeConceptSetDefinition compositeConceptSetDefinition = (CompositeConceptSetDefinition) domain;
-//			for (ConceptSetDefinition childConceptSetDefinition : compositeConceptSetDefinition.getChildren()) {
-//				$.addAll(processConstraintDomain(storageKey, childConceptSetDefinition));
-//			}
-//		}
-//		return $;
-//	}
 
 	/**
 	 * Enumeration for concept set definition type.
