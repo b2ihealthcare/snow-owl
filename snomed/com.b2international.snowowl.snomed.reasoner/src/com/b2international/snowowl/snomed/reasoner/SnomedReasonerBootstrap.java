086a92b9165ab94c64bd7cf72399af6dde563378
András Péteri <apeteri@b2international.com>
2018-06-22 17:42:46 +0200

    [reasoner] Add bootstrap class to snomed.reasoner bundle

---
+++ snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/SnomedReasonerBootstrap.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.snomed.reasoner;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import com.b2international.snowowl.core.config.SnowOwlConfiguration;
+import com.b2international.snowowl.core.setup.DefaultBootstrapFragment;
+import com.b2international.snowowl.core.setup.Environment;
+import com.b2international.snowowl.snomed.datastore.config.SnomedCoreConfiguration;
+import com.b2international.snowowl.snomed.reasoner.classification.SnomedReasonerServerService;
+import com.b2international.snowowl.snomed.reasoner.classification.SnomedReasonerService;
+import com.b2international.snowowl.snomed.reasoner.ontology.SnomedOntologyService;
+import com.b2international.snowowl.snomed.reasoner.preferences.IReasonerPreferencesService;
+import com.b2international.snowowl.snomed.reasoner.preferences.ReasonerPreferencesService;
+
+/**
+ * @since 7.0
+ */
+public final class SnomedReasonerBootstrap extends DefaultBootstrapFragment {
+
+	@Override
+	public void run(final SnowOwlConfiguration configuration, 
+			final Environment env, 
+			final IProgressMonitor monitor) throws Exception {
+
+		if (env.isServer() || env.isEmbedded()) {
+
+			env.services().registerService(SnomedOntologyService.class, new SnomedOntologyService());
+			env.services().registerService(IReasonerPreferencesService.class, new ReasonerPreferencesService());
+
+			final SnomedCoreConfiguration snomedConfig = configuration.getModuleConfig(SnomedCoreConfiguration.class);
+			final int maximumReasonerCount = snomedConfig.getMaxReasonerCount();
+			final int maximumTaxonomiesToKeep = snomedConfig.getMaxReasonerResults();
+			final SnomedReasonerServerService reasonerService = new SnomedReasonerServerService(maximumReasonerCount, maximumTaxonomiesToKeep);
+			env.services().registerService(SnomedReasonerService.class, reasonerService);
+			reasonerService.registerListeners();
+		}
+	}
+}
