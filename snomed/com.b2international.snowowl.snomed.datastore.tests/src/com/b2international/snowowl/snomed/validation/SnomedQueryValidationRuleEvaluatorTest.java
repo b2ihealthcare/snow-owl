97163b7a3789f94b4d9180b58abdc729045fdd82
norban <norban@b2international.com>
2018-06-27 14:45:20 +0200

    [validation-test] fix exceptions

--- snomed/com.b2international.snowowl.snomed.datastore.tests/src/com/b2international/snowowl/snomed/validation/SnomedQueryValidationRuleEvaluatorTest.java
+++ snomed/com.b2international.snowowl.snomed.datastore.tests/src/com/b2international/snowowl/snomed/validation/SnomedQueryValidationRuleEvaluatorTest.java
@@ -31,9 +31,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.b2international.collections.PrimitiveCollectionModule;
+import com.b2international.commons.options.Options;
 import com.b2international.index.Index;
 import com.b2international.index.Indexes;
 import com.b2international.index.mapping.Mappings;
+import com.b2international.index.query.Expressions.ExpressionBuilder;
 import com.b2international.index.revision.BaseRevisionIndexTest;
 import com.b2international.index.revision.RevisionIndex;
 import com.b2international.snowowl.core.ComponentIdentifier;
@@ -43,6 +45,8 @@ import com.b2international.snowowl.core.internal.validation.ValidationThreadPool
 import com.b2international.snowowl.core.validation.ValidationRequests;
 import com.b2international.snowowl.core.validation.eval.ValidationRuleEvaluator;
 import com.b2international.snowowl.core.validation.issue.ValidationIssue;
+import com.b2international.snowowl.core.validation.issue.ValidationIssueDetailExtension;
+import com.b2international.snowowl.core.validation.issue.ValidationIssueDetailExtensionProvider;
 import com.b2international.snowowl.core.validation.issue.ValidationIssues;
 import com.b2international.snowowl.core.validation.rule.ValidationRule;
 import com.b2international.snowowl.core.validation.rule.ValidationRule.Severity;
@@ -72,9 +76,29 @@ import com.google.inject.Injector;
  */
 public class SnomedQueryValidationRuleEvaluatorTest extends BaseRevisionIndexTest {
 
+	/**
+	 * Usage of this class intended for testing purposes only
+	 */
+	private static final class TestValidationDetailExtension implements ValidationIssueDetailExtension {
+
+		@Override
+		public void prepareQuery(ExpressionBuilder queryBuilder, Options options) {}
+
+		@Override
+		public void extendIssuesWithDetails(BranchContext context, Collection<ValidationIssue> issue) {}
+
+		@Override
+		public String getToolingId() { 
+			return SnomedQueryValidationRuleEvaluatorTest.TOOLING_ID; 
+		}
+		
+	}
+	
 	private BranchContext context;
 	private SnomedQueryValidationRuleEvaluator evaluator;
 	private ValidationRepository repository;
+	
+	private static final String TOOLING_ID = "toolingId";
 
 	@Override
 	protected Collection<Class<?>> getTypes() {
@@ -107,6 +131,10 @@ public class SnomedQueryValidationRuleEvaluatorTest extends BaseRevisionIndexTes
 		if (!ValidationRuleEvaluator.Registry.types().contains(evaluator.type())) {
 			ValidationRuleEvaluator.Registry.register(evaluator);
 		}
+		
+		ValidationIssueDetailExtensionProvider extensionProvider = ValidationIssueDetailExtensionProvider.INSTANCE;
+		extensionProvider.addExtension(new TestValidationDetailExtension());
+		
 	}
 	
 	@After
@@ -241,7 +269,7 @@ public class SnomedQueryValidationRuleEvaluatorTest extends BaseRevisionIndexTes
 			.setMessageTemplate("Error")
 			.setSeverity(Severity.ERROR)
 			.setImplementation(context.service(ObjectMapper.class).writeValueAsString(ruleQuery))
-			.setToolingId("toolingId")
+			.setToolingId(TOOLING_ID)
 			.build()
 			.execute(context);
 	}
