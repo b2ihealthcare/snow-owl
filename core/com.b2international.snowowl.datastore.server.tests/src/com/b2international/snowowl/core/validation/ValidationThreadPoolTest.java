8c74d31eba80ef551e8553aabde8c627d950f29d
Zoltan Molnar <zmolnar@b2international.com>
2018-06-28 17:27:41 +0200

    SO-3125: Added test cases for most use-case functionalities

--- core/com.b2international.snowowl.datastore.server.tests/src/com/b2international/snowowl/core/validation/ValidationThreadPoolTest.java
+++ core/com.b2international.snowowl.datastore.server.tests/src/com/b2international/snowowl/core/validation/ValidationThreadPoolTest.java
@@ -15,7 +15,7 @@
  */
 package com.b2international.snowowl.core.validation;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
@@ -33,60 +33,137 @@ import com.google.common.collect.Lists;
  * @since 6.6
  */
 public class ValidationThreadPoolTest {
-	
-	private static final int FAST_VALIDATION_THREAD_COUNT = 6;
-	private static final int MEDIUM_VALIDATION_THREAD_COUNT = 4;
-	private static final int EXPENSIVE_VALIDATION_THREAD_COUNT = 3;
-	
+
+	private static final int MAXIMUM_AMOUNT_OF_RUNNING_EXPENSIVE_JOBS = 1;
+
+	private static final int MAXIMUM_AMOUNT_OF_RUNNING_NORMAL_JOBS = 4;
+
+	private static final int VALIDATION_THREAD_COUNT = 6;
+
 	@Test
 	public void testConcurrentExpensiveJobs() {
-		final ValidationThreadPool pool = new ValidationThreadPool(EXPENSIVE_VALIDATION_THREAD_COUNT);
-		
+		final ValidationThreadPool pool = new ValidationThreadPool(VALIDATION_THREAD_COUNT);
+
 		final IJobManager manager = Job.getJobManager();
-		final Runnable expensiveRunnable = () -> {
-			try {
-				Job[] expensiveJobs = manager.find(CheckType.EXPENSIVE.getName());
-				int jobsInManager = expensiveJobs.length;
-				long runningJobs = Arrays.asList(expensiveJobs).stream().filter(job -> job.getState() == Job.RUNNING).count();
-				assertEquals(1L, runningJobs);
-				Thread.sleep(2000); 
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		final Runnable expensiveRunnable = createValidatableRunnable(CheckType.EXPENSIVE, manager);
+
+		final List<Promise<Object>> validationPromises = Lists.newArrayList();
+		for (int i = 0; i < 10; i++) {
+			validationPromises.add(pool.submit(CheckType.EXPENSIVE, expensiveRunnable));
+		}
+
+		Promise.all(validationPromises).getSync();
+	}
+
+	@Test
+	public void testConcurrentFastJobs() {
+
+		final ValidationThreadPool pool = new ValidationThreadPool(VALIDATION_THREAD_COUNT);
+
+		final IJobManager manager = Job.getJobManager();
+
+		final Runnable fastRunnable = createValidatableRunnable(CheckType.FAST, manager);
+
+		final List<Promise<Object>> validationPromises = Lists.newArrayList();
+		for (int i = 0; i < 10; i++) {
+			validationPromises.add(pool.submit(CheckType.FAST, fastRunnable));
+		}
+
+		Promise.all(validationPromises).getSync();
+	}
+
+	@Test
+	public void TestConcurrentNormalJobs() {
+		final ValidationThreadPool pool = new ValidationThreadPool(VALIDATION_THREAD_COUNT);
+
+		final IJobManager manager = Job.getJobManager();
+
+		final Runnable fastRunnable = createValidatableRunnable(CheckType.NORMAL, manager);
+
+		final List<Promise<Object>> validationPromises = Lists.newArrayList();
+		for (int i = 0; i < 10; i++) {
+			validationPromises.add(pool.submit(CheckType.NORMAL, fastRunnable));
+		}
+
+		Promise.all(validationPromises).getSync();
+	}
+
+	@Test
+	public void testConcurrentFastJobsWithExpensiveOnes() {
+		final ValidationThreadPool pool = new ValidationThreadPool(VALIDATION_THREAD_COUNT);
+
+		final IJobManager manager = Job.getJobManager();
+
+		final List<Promise<Object>> validationPromises = Lists.newArrayList();
+		for (int i = 0; i < 10; i++) {
+			validationPromises.add(pool.submit(CheckType.FAST, createValidatableRunnable(CheckType.FAST, manager)));
+			if (i % 3 == 0) {
+				validationPromises.add(pool.submit(CheckType.EXPENSIVE, createValidatableRunnable(CheckType.EXPENSIVE, manager)));
 			}
-		};
-		final Promise<Object> expensivePromise1 = pool.submit(CheckType.EXPENSIVE, expensiveRunnable);
-		final Promise<Object> expensivePromise2 = pool.submit(CheckType.EXPENSIVE, expensiveRunnable);
-		// there should be no running jobs
-		assertEquals(0, Arrays.asList(manager.find(CheckType.EXPENSIVE)).stream().filter(job -> job.getState() == Job.RUNNING).count());
-		Promise.all(expensivePromise1, expensivePromise2).getSync();
+		}
+
+		Promise.all(validationPromises).getSync();
 	}
 	
 	@Test
-	public void testConcurrentFastJobs() {
-	
-		final ValidationThreadPool pool = new ValidationThreadPool(FAST_VALIDATION_THREAD_COUNT);
-		
+	public void testConcurrentNormalJobsWithFastOnes() {
+		final ValidationThreadPool pool = new ValidationThreadPool(VALIDATION_THREAD_COUNT);
+
 		final IJobManager manager = Job.getJobManager();
-		final Runnable fastRunnable = () -> {
+
+		final List<Promise<Object>> validationPromises = Lists.newArrayList();
+		for (int i = 0; i < 10; i++) {
+			validationPromises.add(pool.submit(CheckType.NORMAL, createValidatableRunnable(CheckType.FAST, manager)));
+			if (i % 2 == 0) {
+				validationPromises.add(pool.submit(CheckType.FAST, createValidatableRunnable(CheckType.FAST, manager)));
+			}
+		}
+
+		Promise.all(validationPromises).getSync();
+	}
+
+	@Test
+	public void test50JobsOfAllTypes() {
+		final ValidationThreadPool pool = new ValidationThreadPool(VALIDATION_THREAD_COUNT);
+
+		final IJobManager manager = Job.getJobManager();
+
+		final List<Promise<Object>> validationPromises = Lists.newArrayList();
+		validationPromises.add(pool.submit(CheckType.EXPENSIVE, createValidatableRunnable(CheckType.EXPENSIVE, manager)));
+		for (int i = 0; i < 10; i++) {
+			validationPromises.add(pool.submit(CheckType.FAST, createValidatableRunnable(CheckType.FAST, manager)));
+			validationPromises.add(pool.submit(CheckType.NORMAL, createValidatableRunnable(CheckType.NORMAL, manager)));
+			if (i % 3 == 0) {
+				validationPromises.add(pool.submit(CheckType.EXPENSIVE, createValidatableRunnable(CheckType.EXPENSIVE, manager)));
+			}
+		}
+
+		Promise.all(validationPromises).getSync();
+	}
+
+	private Runnable createValidatableRunnable(CheckType checkType, IJobManager manager) {
+		return () -> {
 			try {
-				Job[] fastJobs = manager.find(CheckType.FAST.getName());
-				int jobsInManager = fastJobs.length;
-				long runningJobs = Arrays.asList(fastJobs).stream().filter(job -> job.getState() == Job.RUNNING).count();
-				System.err.println(runningJobs);
-				assertEquals(1L, runningJobs);
-				Thread.sleep(2000); 
+				long runningExpensiveJobs = Arrays.asList(manager.find(CheckType.EXPENSIVE.getName())).stream().filter(job -> job.getState() == Job.RUNNING).count();
+				long runningNormalJobs = Arrays.asList(manager.find(CheckType.NORMAL.getName())).stream().filter(job -> job.getState() == Job.RUNNING).count();
+				long runningFastJobs = Arrays.asList(manager.find(CheckType.FAST.getName())).stream().filter(job -> job.getState() == Job.RUNNING).count();
+
+				long allRunningJobs = runningExpensiveJobs + runningFastJobs + runningNormalJobs;
+
+				assertTrue(allRunningJobs <= VALIDATION_THREAD_COUNT);
+
+				if (CheckType.EXPENSIVE == checkType) {
+					assertTrue(runningExpensiveJobs == MAXIMUM_AMOUNT_OF_RUNNING_EXPENSIVE_JOBS);
+				}
+
+				if (CheckType.NORMAL == checkType) {
+					assertTrue(runningNormalJobs <= MAXIMUM_AMOUNT_OF_RUNNING_NORMAL_JOBS);
+				}
+				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		};
-		
-		List<Promise<Object>> fastValidationPromises = Lists.newArrayList();
-		for (int i = 0; i < FAST_VALIDATION_THREAD_COUNT; i++) {
-			fastValidationPromises.add(pool.submit(CheckType.FAST, fastRunnable));
-		}
-		// there should be no running jobs
-		assertEquals(0, Arrays.asList(manager.find(CheckType.EXPENSIVE)).stream().filter(job -> job.getState() == Job.RUNNING).count());
-		Promise.all(fastValidationPromises).getSync();
 	}
-	
+
 }
