3f07123c82bb9fe86ff1b9f4dd096d0806f1d528
András Péteri <apeteri@b2international.com>
2018-07-05 15:03:25 +0200

    [reasoner] Move ClassifyOperation to its parent package

--- snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/classification/ClassifyOperation.java
+++ snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/classification/ClassifyOperation.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.snowowl.snomed.reasoner.classification.operation;
+package com.b2international.snowowl.snomed.reasoner.classification;
 
 import java.util.concurrent.TimeUnit;
 
