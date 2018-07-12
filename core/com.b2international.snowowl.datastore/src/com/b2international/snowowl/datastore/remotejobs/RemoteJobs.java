bdf242cfb648485997d4cdcb50ab9fb44c2dfa13
András Péteri <apeteri@b2international.com>
2018-07-12 18:17:42 +0200

    [core] Add support for searchAfter tokens in PageableCollectionResources

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/remotejobs/RemoteJobs.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/remotejobs/RemoteJobs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2017-2018 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,8 +26,7 @@ public final class RemoteJobs extends PageableCollectionResource<RemoteJobEntry>
 
 	private static final long serialVersionUID = 1L;
 
-	public RemoteJobs(List<RemoteJobEntry> items, String scrollId, Object[] searchAfter, int limit, int total) {
+	public RemoteJobs(List<RemoteJobEntry> items, String scrollId, String searchAfter, int limit, int total) {
 		super(items, scrollId, searchAfter, limit, total);
 	}
-	
 }
