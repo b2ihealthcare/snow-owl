abf0452e155bed7f81d785eeb8e1fd8b8a98a146
Balazs Banfai <bbanfai@b2international.com>
2018-08-07 11:19:16 +0200

    SO-3170 Automated test query type refset creation.

--- core/com.b2international.snowowl.fhir.api.tests/src/com/b2international/snowowl/fhir/api/tests/endpoints/codesystem/ValueSetRestTest.java
+++ core/com.b2international.snowowl.fhir.api.tests/src/com/b2international/snowowl/fhir/api/tests/endpoints/codesystem/ValueSetRestTest.java
@@ -15,15 +15,51 @@
  */
 package com.b2international.snowowl.fhir.api.tests.endpoints.codesystem;
 
+import static com.b2international.snowowl.snomed.SnomedConstants.Concepts.FULLY_SPECIFIED_NAME;
+import static com.b2international.snowowl.snomed.SnomedConstants.Concepts.IS_A;
+import static com.b2international.snowowl.snomed.SnomedConstants.Concepts.SYNONYM;
 import static com.b2international.snowowl.test.commons.rest.RestExtensions.givenAuthenticatedRequest;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
+import java.util.Date;
+import java.util.Map;
+import java.util.Optional;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.b2international.snowowl.core.ApplicationContext;
+import com.b2international.snowowl.core.ServiceProvider;
+import com.b2international.snowowl.core.api.IBranchPath;
+import com.b2international.snowowl.core.api.SnowowlRuntimeException;
+import com.b2international.snowowl.core.events.Request;
+import com.b2international.snowowl.datastore.remotejobs.RemoteJobEntry;
+import com.b2international.snowowl.datastore.request.CommitResult;
+import com.b2international.snowowl.datastore.request.job.JobRequests;
+import com.b2international.snowowl.eventbus.IEventBus;
 import com.b2international.snowowl.fhir.api.tests.FhirTest;
+import com.b2international.snowowl.identity.domain.User;
+import com.b2international.snowowl.snomed.SnomedConstants;
+import com.b2international.snowowl.snomed.SnomedConstants.Concepts;
+import com.b2international.snowowl.snomed.common.SnomedRf2Headers;
+import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants;
+import com.b2international.snowowl.snomed.core.domain.Acceptability;
+import com.b2international.snowowl.snomed.core.domain.CaseSignificance;
+import com.b2international.snowowl.snomed.core.domain.CharacteristicType;
+import com.b2international.snowowl.snomed.core.domain.RelationshipModifier;
+import com.b2international.snowowl.snomed.core.domain.SnomedConcept;
+import com.b2international.snowowl.snomed.core.domain.refset.SnomedReferenceSetMember;
+import com.b2international.snowowl.snomed.datastore.SnomedDatastoreActivator;
+import com.b2international.snowowl.snomed.datastore.SnomedRefSetUtil;
+import com.b2international.snowowl.snomed.datastore.request.SnomedDescriptionCreateRequestBuilder;
+import com.b2international.snowowl.snomed.datastore.request.SnomedRelationshipCreateRequestBuilder;
+import com.b2international.snowowl.snomed.datastore.request.SnomedRequests;
 import com.b2international.snowowl.snomed.fhir.SnomedUri;
+import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetType;
+import com.b2international.snowowl.terminologyregistry.core.request.CodeSystemRequests;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.config.LogConfig;
 import com.jayway.restassured.config.RestAssuredConfig;
@@ -34,6 +70,12 @@ import com.jayway.restassured.config.RestAssuredConfig;
  */
 public class ValueSetRestTest extends FhirTest {
 	
+	/**
+	 * 
+	 */
+	private static final String FHIR_QUERY_TYPE_REFSET_VERSION = "FHIR_QUERY_TYPE_REFSET_VERSION";
+	private static final String B2I_NAMESPACE = "1000154";
+	
 	@BeforeClass
 	public static void setupSpec() {
 		
@@ -42,7 +84,7 @@ public class ValueSetRestTest extends FhirTest {
 		RestAssured.given().config(config.logConfig(logConfig));
 	}
 	
-	@Test
+	//@Test
 	public void printValueSets() throws Exception {
 		
 		givenAuthenticatedRequest(FHIR_ROOT_CONTEXT)
@@ -50,7 +92,7 @@ public class ValueSetRestTest extends FhirTest {
 		.prettyPrint();
 	}
 	
-	@Test
+	//@Test
 	public void valueSetsTest() throws Exception {
 		
 		givenAuthenticatedRequest(FHIR_ROOT_CONTEXT)
@@ -78,7 +120,7 @@ public class ValueSetRestTest extends FhirTest {
 		.statusCode(200);
 	}
 	
-	@Test
+	//@Test
 	public void valueSetsSummaryTest() throws Exception {
 		
 		givenAuthenticatedRequest(FHIR_ROOT_CONTEXT)
@@ -105,7 +147,8 @@ public class ValueSetRestTest extends FhirTest {
 		.statusCode(200);
 	}
 	
-	@Test
+	//This is junk as the ID is hard-coded
+	//@Test
 	public void getSingleSnomedValueSetTest() {
 		givenAuthenticatedRequest(FHIR_ROOT_CONTEXT)
 		 	.pathParam("id", "snomedStore:MAIN/2018-01-31:723264001") 
@@ -116,11 +159,183 @@ public class ValueSetRestTest extends FhirTest {
 	//'Virtual' value set
 	@Test
 	public void getSingleQueryTypeValueSetTest() {
+		
+		String refsetLogicalId = getRefsetLogicalId();
+		System.out.println("Refset concept ID: " + refsetLogicalId);
+		
 		givenAuthenticatedRequest(FHIR_ROOT_CONTEXT)
-		 	.pathParam("id", "snomedStore:MAIN/FHIR_TEST_VERSION:98403008|84f56f72-9f8b-423d-98b8-25961811393c") 
+		 	.pathParam("id", "snomedStore:MAIN/" + FHIR_QUERY_TYPE_REFSET_VERSION + ":" + refsetLogicalId) 
 			.when().get("/ValueSet/{id}")
 			.prettyPrint();
 	}
+
 	
+	//************************************************************ //
+	/**
+	 * @return
+	 */
+	private String getRefsetLogicalId() {
+		
+		String mainBranch = IBranchPath.MAIN_BRANCH;
+		String refsetName = "FHIR Automated Test Query Type Refset";
+		
+		
+		Optional<SnomedConcept> refsetConcept = SnomedRequests.prepareSearchConcept()
+			.filterByTerm(refsetName)
+			.all()
+			.build(SnomedDatastoreActivator.REPOSITORY_UUID, mainBranch)
+			.execute(ApplicationContext.getServiceForClass(IEventBus.class))
+			.getSync()
+			.first();
+		
+		if (!refsetConcept.isPresent()) {
+			System.out.println("Creating test query type reference set...");
+			String combinedId = createRefset(mainBranch, refsetName);
+			System.out.println("Versioning content...");
+			createVersion();
+			
+			return combinedId;
+			
+		} else {
+			System.out.println("Found existing test query type reference set...");
+			String refsetId = refsetConcept.get().getId();
+			
+			//grab the first member
+			SnomedReferenceSetMember firstMember = SnomedRequests.prepareSearchMember()
+				.one()
+				.filterByRefSet(refsetId)
+				.build(SnomedDatastoreActivator.REPOSITORY_UUID, mainBranch)
+				.execute(ApplicationContext.getServiceForClass(IEventBus.class))
+				.getSync()
+				.stream()
+				.findFirst()
+				.get();
+			
+			return refsetId + "|" + firstMember.getId();
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void createVersion() {
+		
+		Request<ServiceProvider, Boolean> request = CodeSystemRequests.prepareNewCodeSystemVersion()
+			.setCodeSystemShortName(SnomedTerminologyComponentConstants.SNOMED_SHORT_NAME)
+			.setDescription("FHIR Test version for Query type reference sets")
+			.setVersionId(FHIR_QUERY_TYPE_REFSET_VERSION)
+			.setEffectiveTime(new Date())
+			.build();
+			
+		String jobId = JobRequests.prepareSchedule()
+			.setDescription(String.format("Creating version '%s/%s'", 
+					SnomedTerminologyComponentConstants.SNOMED_SHORT_NAME, FHIR_QUERY_TYPE_REFSET_VERSION))
+			.setUser(User.SYSTEM.getUsername())
+			.setRequest(request)
+			.buildAsync()
+			.execute(getEventBus())
+			.getSync();
+		
+		RemoteJobEntry job = null;
+		do {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				throw new SnowowlRuntimeException(e);
+			}
+			
+			job = JobRequests.prepareGet(jobId)
+					.buildAsync()
+					.execute(getEventBus())
+					.getSync();
+		} while (job == null || !job.isDone());
+	}
+
+	private String createRefset(String branchPath, String refsetName) {
+		
+		CommitResult commitResult = SnomedRequests.prepareNewConcept()
+			.setIdFromNamespace(B2I_NAMESPACE)
+			.setActive(true)
+			.setModuleId(Concepts.MODULE_SCT_CORE)
+			.addDescription(createDescription(refsetName + "FHIR Automated Test Query Type Refset (foundation metadata concept)", FULLY_SPECIFIED_NAME))
+			.addDescription(createDescription(refsetName, SYNONYM))
+			.addRelationship(createIsaRelationship(CharacteristicType.STATED_RELATIONSHIP, SnomedRefSetUtil.getParentConceptId(SnomedRefSetType.QUERY)))
+			.addRelationship(createIsaRelationship(CharacteristicType.INFERRED_RELATIONSHIP, SnomedRefSetUtil.getParentConceptId(SnomedRefSetType.QUERY)))
+			.setRefSet(SnomedRequests.prepareNewRefSet()
+					.setReferencedComponentType(SnomedTerminologyComponentConstants.CONCEPT)
+					.setType(SnomedRefSetType.QUERY))
+			.build(SnomedDatastoreActivator.REPOSITORY_UUID, branchPath, "info@b2international.com", "FHIR Automated Test Query Type Reference Set")
+			.execute(getEventBus())
+			.fail(t -> {
+				t.printStackTrace();
+				return null;
+			})
+			.getSync();
+		
+		String refsetId = commitResult.getResultAs(String.class);
+		
+		String referencedSimpleTypeRefsetId = createSimpleTypeRefsetConcept(branchPath);
+		
+		Map<String, Object> memberMap = Maps.newHashMap();
+		memberMap.put(SnomedRf2Headers.FIELD_QUERY, "<<49111001");
+		
+		String memberId = SnomedRequests.prepareNewMember()
+			.setReferenceSetId(refsetId)
+			.setModuleId(Concepts.MODULE_SCT_CORE)
+			.setActive(true)
+			.setProperties(memberMap)
+			.setReferencedComponentId(referencedSimpleTypeRefsetId)
+			.build(SnomedDatastoreActivator.REPOSITORY_UUID, branchPath, "info@b2international.com", "FHIR Automated Test Query Type Value Set")
+			.execute(getEventBus())
+			.getSync()
+			.getResultAs(String.class);
+		
+		return refsetId + "|" + memberId;
+	}
 	
+	private String createSimpleTypeRefsetConcept(String branchPath) {
+		return SnomedRequests.prepareNewConcept()
+			.setIdFromNamespace(B2I_NAMESPACE)
+			.setActive(true)
+			.setModuleId(Concepts.MODULE_SCT_CORE)
+			.addDescription(createDescription("FHIR Automated Test Simple Type Refset (foundation metadata concept)", FULLY_SPECIFIED_NAME))
+			.addDescription(createDescription("FHIR Automated Test Simple Type Refset", SYNONYM))
+			.addRelationship(createIsaRelationship(CharacteristicType.STATED_RELATIONSHIP, SnomedRefSetUtil.getParentConceptId(SnomedRefSetType.SIMPLE)))
+			.addRelationship(createIsaRelationship(CharacteristicType.INFERRED_RELATIONSHIP, SnomedRefSetUtil.getParentConceptId(SnomedRefSetType.SIMPLE)))
+			.setRefSet(SnomedRequests.prepareNewRefSet()
+					.setReferencedComponentType(SnomedTerminologyComponentConstants.CONCEPT)
+					.setType(SnomedRefSetType.SIMPLE))
+			.build(SnomedDatastoreActivator.REPOSITORY_UUID, branchPath, "info@b2international.com", "FHIR Automated Test Simple Type Value Set")
+			.execute(getEventBus())
+			.getSync()
+			.getResultAs(String.class);
+	}
+
+	private SnomedDescriptionCreateRequestBuilder createDescription(final String term, final String type) {
+		
+		return SnomedRequests.prepareNewDescription()
+			.setIdFromNamespace(B2I_NAMESPACE)
+			.setActive(true)
+			.setModuleId(Concepts.MODULE_SCT_CORE)
+			.setLanguageCode("en")
+			.setTypeId(type)
+			.setTerm(term)
+			.setCaseSignificance(CaseSignificance.CASE_INSENSITIVE)
+			.setAcceptability(ImmutableMap.of(SnomedConstants.Concepts.REFSET_LANGUAGE_TYPE_US, Acceptability.PREFERRED));
+}
+
+	private SnomedRelationshipCreateRequestBuilder createIsaRelationship(final CharacteristicType characteristicType, String destinationId) {
+		return SnomedRequests.prepareNewRelationship() 
+			.setIdFromNamespace(B2I_NAMESPACE)
+			.setActive(true)
+			.setModuleId(Concepts.MODULE_SCT_CORE)
+			.setDestinationId(destinationId)
+			.setTypeId(IS_A)
+			.setCharacteristicType(characteristicType)
+			.setModifier(RelationshipModifier.EXISTENTIAL);
+	}
+	
+	private IEventBus getEventBus() {
+		return ApplicationContext.getServiceForClass(IEventBus.class);
+	}
 }
