47292c39f9e86c715160c4d76145316a8130f8f0
Gabor Nagy <gnagy@b2international.com>
2018-08-10 10:29:55 +0200

    [snomed.import] Decrease number of elements to commit during import

--- snomed/com.b2international.snowowl.snomed.importer.rf2/src/com/b2international/snowowl/snomed/importer/rf2/model/AbstractSnomedImporter.java
+++ snomed/com.b2international.snowowl.snomed.importer.rf2/src/com/b2international/snowowl/snomed/importer/rf2/model/AbstractSnomedImporter.java
@@ -125,7 +125,7 @@ public abstract class AbstractSnomedImporter<T extends AbstractComponentRow, C e
 	protected static final int COMMIT_WORK_UNITS = 50;
 
 	/** A CDO transaction is committed when the number of processed elements % this value == 0. */
-	protected static final int COMMIT_EVERY_NUM_ELEMENTS = 50_000;
+	protected static final int COMMIT_EVERY_NUM_ELEMENTS = 25_000;
 
 	private static final int ID_IDX = 0;
 	/** 0-based index of the {@code effectiveTime} column in release files. */
