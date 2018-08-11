72267938107a06c43f5db0f8180e1b31f75a5cd1
Mark Czotter <mczotter@b2international.com>
2018-08-11 13:43:25 +0200

    SO-3109: fix serialization/deserialization of effectiveTime properties..
    ...on SNOMED CT component APIs
    
    Remove ISnomedComponentMixin add annotations directly to the domain
    classes.

--- snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/config/SnomedApiConfig.java
+++ snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/config/SnomedApiConfig.java
@@ -65,7 +65,6 @@ import com.b2international.snowowl.eventbus.IEventBus;
 import com.b2international.snowowl.snomed.api.ISnomedExportService;
 import com.b2international.snowowl.snomed.api.ISnomedRf2ImportService;
 import com.b2international.snowowl.snomed.api.browser.ISnomedBrowserService;
-import com.b2international.snowowl.snomed.api.domain.browser.ISnomedBrowserComponent;
 import com.b2international.snowowl.snomed.api.impl.SnomedBrowserService;
 import com.b2international.snowowl.snomed.api.impl.SnomedExportService;
 import com.b2international.snowowl.snomed.api.impl.SnomedRf2ImportService;
@@ -74,10 +73,8 @@ import com.b2international.snowowl.snomed.api.rest.SnowOwlAuthenticationProvider
 import com.b2international.snowowl.snomed.api.rest.domain.BranchMixin;
 import com.b2international.snowowl.snomed.api.rest.domain.BranchStateMixin;
 import com.b2international.snowowl.snomed.api.rest.domain.CollectionResourceMixin;
-import com.b2international.snowowl.snomed.api.rest.domain.ISnomedComponentMixin;
 import com.b2international.snowowl.snomed.api.rest.domain.ReviewMixin;
 import com.b2international.snowowl.snomed.api.rest.util.CsvMessageConverter;
-import com.b2international.snowowl.snomed.core.domain.SnomedComponent;
 import com.b2international.snowowl.snomed.datastore.config.SnomedCoreConfiguration;
 import com.fasterxml.classmate.TypeResolver;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -138,8 +135,6 @@ public class SnomedApiConfig extends WebMvcConfigurerAdapter {
 		objectMapper.setDateFormat(dateFormat);
 		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 		objectMapper.addMixIn(CollectionResource.class, CollectionResourceMixin.class);
-		objectMapper.addMixIn(SnomedComponent.class, ISnomedComponentMixin.class);
-		objectMapper.addMixIn(ISnomedBrowserComponent.class, ISnomedComponentMixin.class);
 		objectMapper.addMixIn(Branch.class, BranchMixin.class);
 		objectMapper.addMixIn(Metadata.class, MetadataMixin.class);
 		objectMapper.addMixIn(MetadataHolder.class, MetadataHolderMixin.class);
