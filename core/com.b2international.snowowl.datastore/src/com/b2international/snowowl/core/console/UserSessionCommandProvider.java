c24c89ffa15919383690664fd08132b97c1e45f3
Mark Czotter <mczotter@b2international.com>
2018-08-12 15:55:17 +0200

    SO-3109: add snowowl locks shell command

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/core/console/UserSessionCommandProvider.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/core/console/UserSessionCommandProvider.java
@@ -24,9 +24,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.eclipse.emf.cdo.session.remote.CDORemoteSession;
 import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.eclipse.osgi.framework.console.CommandProvider;
 import org.slf4j.Logger;
@@ -35,32 +33,9 @@ import org.slf4j.LoggerFactory;
 import com.b2international.commons.CompareUtils;
 import com.b2international.commons.Pair;
 import com.b2international.commons.StringUtils;
-import com.b2international.index.revision.BaseRevisionBranching;
-import com.b2international.index.revision.RevisionBranch;
 import com.b2international.snowowl.core.ApplicationContext;
-import com.b2international.snowowl.core.LogUtils;
-import com.b2international.snowowl.core.Repository;
-import com.b2international.snowowl.core.RepositoryManager;
-import com.b2international.snowowl.core.api.IBranchPath;
-import com.b2international.snowowl.core.date.DateFormats;
-import com.b2international.snowowl.core.date.Dates;
-import com.b2international.snowowl.datastore.BranchPathUtils;
-import com.b2international.snowowl.datastore.cdo.ICDORepositoryManager;
-import com.b2international.snowowl.datastore.cdo.ICDORepositoryManager.ISessionOperationCallback;
-import com.b2international.snowowl.datastore.oplock.IOperationLockTarget;
-import com.b2international.snowowl.datastore.oplock.OperationLockException;
-import com.b2international.snowowl.datastore.oplock.OperationLockInfo;
-import com.b2international.snowowl.datastore.oplock.impl.AllRepositoriesLockTarget;
-import com.b2international.snowowl.datastore.oplock.impl.DatastoreLockContext;
-import com.b2international.snowowl.datastore.oplock.impl.DatastoreLockContextDescriptions;
-import com.b2international.snowowl.datastore.oplock.impl.DatastoreOperationLockManager;
-import com.b2international.snowowl.datastore.oplock.impl.IDatastoreOperationLockManager;
-import com.b2international.snowowl.datastore.oplock.impl.SingleRepositoryAndBranchLockTarget;
-import com.b2international.snowowl.datastore.oplock.impl.SingleRepositoryLockTarget;
 import com.b2international.snowowl.datastore.session.IApplicationSessionManager;
-import com.b2international.snowowl.identity.domain.User;
 import com.google.common.base.Strings;
-import com.google.common.primitives.Ints;
 
 /**
  * OSGI command contribution with Snow Owl commands to manage user sessions.
@@ -77,11 +52,6 @@ public class UserSessionCommandProvider implements CommandProvider {
 		buffer.append("\tsession users - List the users that are currently logged in\n");
 		buffer.append("\tsession disconnect [userName1,userName2,userNameN|ALL] - Disconnect active user(s) or all users.  Do not use space between the users.\n");
 		buffer.append("\tsession login [enabled|disabled|status] - Enables/disables login for new, non-administrator sessions.\n");
-		buffer.append("\tsession showlocks - Displays all currently acquired locks on the server.\n");
-		buffer.append("\tsession lock [allRepositories|repositoryUuid|repositoryUuid branchPath] - Acquires a lock in repository on the specified branch.\n");
-		buffer.append("\tsession unlock [allRepositories|repositoryUuid|repositoryUuid branchPath] - Releases a lock in the repository on the specified branch.\n");
-		buffer.append("\tsession forceunlock [all|lock identifier] - Forcefully releases the specified locks.\n");
-		buffer.append("\tsession repositories - Lists all available repositories.\n");
 		return buffer.toString();
 	}
 
@@ -98,26 +68,8 @@ public class UserSessionCommandProvider implements CommandProvider {
 			final String cmd = Strings.nullToEmpty(interpreter.nextArgument());
 			
 			switch (cmd) {
-				case "users":
-					users(interpreter);
-					break;
-				case "disconnect":
-					disconnect(interpreter);
-					break;
 				case "login":
 					login(interpreter);
-					break;
-				case "lock":
-					lock(interpreter);
-					break;
-				case "unlock":
-					unlock(interpreter);
-					break;
-				case "showlocks":
-					showLocks(interpreter);
-					break;
-				case "forceunlock":
-					forceUnlock(interpreter);
 					break;
 				default:
 					interpreter.println(getHelp());
@@ -158,47 +110,6 @@ public class UserSessionCommandProvider implements CommandProvider {
 
 	}
 
-	/**
-	 * Disconnect the users.
-	 * 
-	 * @param interpreter
-	 */
-	public synchronized void disconnect(final CommandInterpreter interpreter) {
-		final String userNamesParameter = interpreter.nextArgument();
-
-		if (StringUtils.isEmpty(userNamesParameter)) {
-			interpreter.print("Command usage: session disconnect [userName1,userName2,userNameN|ALL]");
-			return;
-		}
-
-		final AtomicBoolean success = new AtomicBoolean(false);
-		final ICDORepositoryManager repositoryManager = ApplicationContext.getInstance().getService(ICDORepositoryManager.class);
-
-		final ISessionOperationCallback callback = new ISessionOperationCallback() {
-			
-			@Override public void done(final CDORemoteSession session) {
-				interpreter.println("User: " + session.getUserID() + " | session ID: " + session.getSessionID() + " was disconnected.");
-				LogUtils.logUserEvent(USER_ACTIVITY_LOGGER, "admin", "Disconnected user: " + session.getUserID() + " from session: " + session.getSessionID() + ".");
-				success.compareAndSet(false, true);
-			}
-		};
-
-		if ("ALL".equals(userNamesParameter)) {
-			
-			repositoryManager.disconnectAll(callback);
-			
-		} else {
-			
-			repositoryManager.disconnect(tokenizeParameter(userNamesParameter), callback);
-			
-		}
-
-		if (!success.get()) {
-			interpreter.println("Failed to disconnect user(s): " + userNamesParameter + ". Are these users active? Currently active users are:\n");
-			users(interpreter);
-		}
-	}
-
 	private static final List<String> ALLOWED_SUBCOMMANDS = Arrays.asList("enabled", "disabled", "status"); 
 	
 	public synchronized void login(final CommandInterpreter interpreter) {
@@ -234,171 +145,4 @@ public class UserSessionCommandProvider implements CommandProvider {
 		return userList;
 	}
 
-	private static final String COLUMN_FORMAT = "%4s | %3s | %-16s | %-50s | %-50s";
-
-	private synchronized void showLocks(final CommandInterpreter interpreter) {
-	
-		final IDatastoreOperationLockManager lockManager = ApplicationContext.getInstance().getService(IDatastoreOperationLockManager.class);
-		final List<OperationLockInfo<DatastoreLockContext>> locks = ((DatastoreOperationLockManager) lockManager).getLocks();
-		
-		if (locks.isEmpty()) {
-			interpreter.println("No locks are currently granted on this server.");
-			return;
-		}
-		
-		interpreter.println();
-		interpreter.println(String.format(COLUMN_FORMAT, "Id", "Lvl", "Created on", "Locked area", "Owner context"));
-		interpreter.println(Strings.repeat("-", 135));
-		
-		for (final OperationLockInfo<DatastoreLockContext> lockEntry : locks) {
-			interpreter.println(String.format(COLUMN_FORMAT, 
-					lockEntry.getId(),
-					lockEntry.getLevel(),
-					Dates.formatByHostTimeZone(lockEntry.getCreationDate(), DateFormats.MEDIUM),
-					StringUtils.truncate(StringUtils.capitalizeFirstLetter(lockEntry.getTarget().toString()), 50),
-					StringUtils.truncate("Lock owner: " +lockEntry.getContext().getUserId(), 50)));
-			
-			interpreter.println(String.format(COLUMN_FORMAT, "", "", "", "",
-					StringUtils.truncate(StringUtils.capitalizeFirstLetter(lockEntry.getContext().getDescription()), 50)));
-			
-			interpreter.println(Strings.repeat("-", 135));
-		}
-		
-	}
-	
-	private synchronized void forceUnlock(final CommandInterpreter interpreter) {
-		
-		final DatastoreOperationLockManager lockManager = (DatastoreOperationLockManager) ApplicationContext.getInstance().getService(IDatastoreOperationLockManager.class);
-		final String argument = interpreter.nextArgument();
-		
-		if (null == argument) {
-			interpreter.println("Missing parameter. Usage: forceunlock [all|lock identifier]");
-			return;
-		}
-		
-		if ("all".equalsIgnoreCase(argument)) {
-			lockManager.unlockAll();
-			interpreter.println("Forcefully released all acquired locks.");
-			return;
-		}
-		
-		final Integer parsedLockId = Ints.tryParse(argument);
-		
-		if (null != parsedLockId) {
-			
-			if (lockManager.unlockById(parsedLockId)) {
-				interpreter.println("Forcefully released lock with identifier " + parsedLockId + ".");
-			} else {
-				interpreter.println("Lock with identifier " + parsedLockId + " could not be found.");
-			}
-			
-		} else {
-			interpreter.println("Lock identifier is not an interger. Usage: forceunlock [all|lock identifier]");
-		}
-	}
-	
-	private synchronized void lock(final CommandInterpreter interpreter) {
-		
-		final IOperationLockTarget target = parseLockTarget(interpreter); 
-		
-		if (null == target) {
-			return;
-		}
-		
-		final IDatastoreOperationLockManager lockManager = ApplicationContext.getInstance().getService(IDatastoreOperationLockManager.class);
-		final DatastoreLockContext context = new DatastoreLockContext(User.SYSTEM.getUsername(), DatastoreLockContextDescriptions.MAINTENANCE);
-		
-		try {
-			lockManager.lock(context, 3000L, target);
-		} catch (final OperationLockException | InterruptedException e) {
-			interpreter.println(e);
-			return;
-		}
-		
-		interpreter.println("Acquired lock for " + target + ".");
-	}
-	
-	private synchronized void unlock(final CommandInterpreter interpreter) {
-		
-		final IOperationLockTarget target = parseLockTarget(interpreter);
-		
-		if (null == target) {
-			return;
-		}
-
-		final IDatastoreOperationLockManager lockManager = ApplicationContext.getInstance().getService(IDatastoreOperationLockManager.class);
-		final DatastoreLockContext context = new DatastoreLockContext(User.SYSTEM.getUsername(), DatastoreLockContextDescriptions.MAINTENANCE);
-
-		try {
-			lockManager.unlock(context, target);
-		} catch (final OperationLockException e) {
-			interpreter.print(e);
-			return;
-		}
-		
-		interpreter.println("Released lock for " + target + ".");
-	}
-	
-	private IOperationLockTarget parseLockTarget(final CommandInterpreter interpreter) {
-		
-		final String uuidOrAll = interpreter.nextArgument();
-
-		if (StringUtils.isEmpty(uuidOrAll)) {
-			interpreter.println("Repository UUID or \"allRepositories\" should be specified.");
-			interpreter.println(getHelp());
-			return null;
-		}
-		
-		if ("allRepositories".equalsIgnoreCase(uuidOrAll)) {
-			return AllRepositoriesLockTarget.INSTANCE;
-		}
-		
-		final RepositoryManager repositoryManager = ApplicationContext.getInstance().getService(RepositoryManager.class);
-		final Repository repository = repositoryManager.get(uuidOrAll);
-		if (null == repository) {
-			
-			interpreter.println("Repository does not exist with UUID: '" + uuidOrAll + "'.");
-			interpreter.println("Available stores are the followings:");
-			interpreter.println("------------------------------------");
-			for (final Repository r : repositoryManager.repositories()) {
-				interpreter.println("\t" + r.id());
-			}
-			interpreter.println("------------------------------------");
-			
-			interpreter.println(getHelp());
-			return null;
-		}
-		
-		final String path = interpreter.nextArgument();
-		
-		if (StringUtils.isEmpty(path)) {
-			return new SingleRepositoryLockTarget(uuidOrAll);
-		}
-		
-		IBranchPath branchPath = null;
-		
-		try {
-
-			branchPath = BranchPathUtils.createPath(path);
-			//assuming active connection manager service here
-			
-			RevisionBranch branch = repository.service(BaseRevisionBranching.class).getBranch(path);
-			if (null == branch) {
-				interpreter.println("Branch does not exist. Branch path: '" + branchPath + "'. Repository UUID: '" + uuidOrAll + "'.");
-				interpreter.println(getHelp());
-				return null;
-			}
-			
-		} catch (final Throwable t) {
-			interpreter.println("Branch does not exist. Branch path: '" + path + "'. Repository UUID: '" + uuidOrAll + "'.");
-			interpreter.println(getHelp());
-			return null;
-		}
-		
-		if (null == branchPath) {
-			return null;
-		}
-		
-		return new SingleRepositoryAndBranchLockTarget(uuidOrAll, branchPath);
-	}
 }
---
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/core/locks/LocksCommand.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2018 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.core.locks;
+
+import java.util.List;
+
+import com.b2international.commons.StringUtils;
+import com.b2international.commons.extension.Component;
+import com.b2international.index.revision.BaseRevisionBranching;
+import com.b2international.index.revision.RevisionBranch;
+import com.b2international.snowowl.core.ApplicationContext;
+import com.b2international.snowowl.core.Repository;
+import com.b2international.snowowl.core.RepositoryManager;
+import com.b2international.snowowl.core.api.IBranchPath;
+import com.b2international.snowowl.core.console.Command;
+import com.b2international.snowowl.core.console.CommandLineStream;
+import com.b2international.snowowl.core.date.DateFormats;
+import com.b2international.snowowl.core.date.Dates;
+import com.b2international.snowowl.datastore.BranchPathUtils;
+import com.b2international.snowowl.datastore.oplock.IOperationLockTarget;
+import com.b2international.snowowl.datastore.oplock.OperationLockException;
+import com.b2international.snowowl.datastore.oplock.OperationLockInfo;
+import com.b2international.snowowl.datastore.oplock.impl.AllRepositoriesLockTarget;
+import com.b2international.snowowl.datastore.oplock.impl.DatastoreLockContext;
+import com.b2international.snowowl.datastore.oplock.impl.DatastoreLockContextDescriptions;
+import com.b2international.snowowl.datastore.oplock.impl.DatastoreOperationLockManager;
+import com.b2international.snowowl.datastore.oplock.impl.IDatastoreOperationLockManager;
+import com.b2international.snowowl.datastore.oplock.impl.SingleRepositoryAndBranchLockTarget;
+import com.b2international.snowowl.datastore.oplock.impl.SingleRepositoryLockTarget;
+import com.b2international.snowowl.identity.domain.User;
+import com.google.common.base.Strings;
+import com.google.common.primitives.Ints;
+
+import picocli.CommandLine;
+import picocli.CommandLine.HelpCommand;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+/**
+ * @since 7.0
+ */
+@Component
+@CommandLine.Command(
+	name = "locks",
+	header = "Display and manage locks",
+	description = "Displays",
+	subcommands = {
+		HelpCommand.class,
+		LocksCommand.AddLockCommand.class,
+		LocksCommand.RemoveLockCommand.class
+	}
+)
+public final class LocksCommand extends Command {
+
+	private static final String COLUMN_FORMAT = "%4s | %3s | %-16s | %-50s | %-50s";
+	private static final String ALL = "ALL";
+	private static final DatastoreLockContext CONSOLE_CONTEXT = new DatastoreLockContext(User.SYSTEM.getUsername(), DatastoreLockContextDescriptions.MAINTENANCE);
+	
+	@Override
+	public void run(CommandLineStream out) {
+		final IDatastoreOperationLockManager lockManager = ApplicationContext.getInstance().getService(IDatastoreOperationLockManager.class);
+		final List<OperationLockInfo<DatastoreLockContext>> locks = ((DatastoreOperationLockManager) lockManager).getLocks();
+		
+		if (locks.isEmpty()) {
+			out.println("No locks are currently granted on this server.");
+			return;
+		}
+		
+		out.println();
+		out.println(COLUMN_FORMAT, "Id", "Lvl", "Created on", "Locked area", "Owner context");
+		out.println(Strings.repeat("-", 135));
+		
+		for (final OperationLockInfo<DatastoreLockContext> lockEntry : locks) {
+			out.println(String.format(COLUMN_FORMAT, 
+					lockEntry.getId(),
+					lockEntry.getLevel(),
+					Dates.formatByHostTimeZone(lockEntry.getCreationDate(), DateFormats.MEDIUM),
+					StringUtils.truncate(StringUtils.capitalizeFirstLetter(lockEntry.getTarget().toString()), 50),
+					StringUtils.truncate("Lock owner: " +lockEntry.getContext().getUserId(), 50)));
+			
+			out.println(String.format(COLUMN_FORMAT, "", "", "", "",
+					StringUtils.truncate(StringUtils.capitalizeFirstLetter(lockEntry.getContext().getDescription()), 50)));
+			
+			out.println(Strings.repeat("-", 135));
+		}
+	}
+	
+	@CommandLine.Command(
+		name = "add",
+		header = "Locks a resource",
+		description = "Locks the specified resource in the system making it read-only for other users"
+	)
+	public static final class AddLockCommand extends Command {
+
+		@Parameters(paramLabel = "REPOSITORY[:BRANCH]|ALL", description = "The repository and optionally a branch to acquire lock for. 'ALL' value can be used to acquire lock for all available repositories.")
+		String repositoryOrAll;
+		
+		@Override
+		public void run(CommandLineStream out) {
+			final IOperationLockTarget target = parseLockTarget(repositoryOrAll); 
+			
+			if (null == target) {
+				out.println("Couldn't find resource '%s' to acquire lock for.", repositoryOrAll);
+				return;
+			}
+			
+			final IDatastoreOperationLockManager lockManager = getLockManager();
+			final DatastoreLockContext context = new DatastoreLockContext(User.SYSTEM.getUsername(), DatastoreLockContextDescriptions.MAINTENANCE);
+			
+			try {
+				lockManager.lock(context, 3000L, target);
+				out.println("Acquired lock for %s.", target);
+			} catch (final OperationLockException | InterruptedException e) {
+				out.println(e);
+			}
+		}
+		
+	}
+	
+	@CommandLine.Command(
+		name = "remove",
+		header = "Unlocks a resource",
+		description = "Unlocks the specified resource in the system making it both writeable and readable for other users"
+	)
+	public static final class RemoveLockCommand extends Command {
+
+		@Parameters(paramLabel = "REPOSITORY[:BRANCH]|LOCK_ID|ALL", description = "The lock target (repository and optional branch) or ID release. 'ALL' value can be used to release all currently acquired locks.")
+		String lockTargetOrLockIdOrAll;
+		
+		@Option(names = { "-f", "--force" }, required = false, description = "To forcefully release the lock when it cannot be released in the normal way.")
+		boolean force;
+		
+		@Override
+		public void run(CommandLineStream out) {
+			// first try to parse it as LOCK ID and release if succeeded
+			Integer lockId = Ints.tryParse(lockTargetOrLockIdOrAll);
+			IOperationLockTarget target = null;
+			if (lockId == null) {
+				// then try to parse it as lock target
+				target = parseLockTarget(lockTargetOrLockIdOrAll);
+				if (null == target) {
+					out.println("Couldn't find resource '%s' to release the lock from.", lockTargetOrLockIdOrAll);
+					return;
+				}
+			}
+
+			try {
+				if (lockId != null) {
+					getLockManager().unlockById(lockId);
+					out.println("Released lock by ID '%s'.", lockId);
+				} else if (target == AllRepositoriesLockTarget.INSTANCE) {
+					getLockManager().unlockAll();
+					out.println("Released ALL locks.");
+				} else {
+					getLockManager().unlock(CONSOLE_CONTEXT, target);
+					out.println("Released lock previously acquired for resource '%s'.", target);
+				}
+			} catch (final OperationLockException e) {
+				out.print(e);
+			}
+		}
+		
+	}
+
+	private static DatastoreOperationLockManager getLockManager() {
+		return (DatastoreOperationLockManager) ApplicationContext.getInstance().getService(IDatastoreOperationLockManager.class);
+	}
+	
+	private static IOperationLockTarget parseLockTarget(final String lockTargetOrAll) {
+		if (ALL.equalsIgnoreCase(lockTargetOrAll)) {
+			return AllRepositoriesLockTarget.INSTANCE;
+		}
+		
+		final String repositoryId;
+		final String path;
+		
+		String[] parts = lockTargetOrAll.split(":");
+		if (parts.length == 2) {
+			repositoryId = parts[0];
+			path = parts[1];
+		} else if (parts.length == 1) {
+			repositoryId = parts[0];
+			path = null;
+		} else {
+			return null;
+		}
+
+		final RepositoryManager repositoryManager = ApplicationContext.getInstance().getService(RepositoryManager.class);
+		final Repository repository = repositoryManager.get(repositoryId);
+		
+		if (null == repository) {
+			return null;
+		}
+		
+		if (Strings.isNullOrEmpty(path)) {
+			return new SingleRepositoryLockTarget(repositoryId);
+		}
+		
+		IBranchPath branchPath = BranchPathUtils.createPath(path);
+		//assuming active connection manager service here
+		
+		RevisionBranch branch = repository.service(BaseRevisionBranching.class).getBranch(path);
+		if (null == branch) {
+			return null;
+		}
+		
+		return new SingleRepositoryAndBranchLockTarget(repositoryId, branchPath);
+	}
+	
+}
