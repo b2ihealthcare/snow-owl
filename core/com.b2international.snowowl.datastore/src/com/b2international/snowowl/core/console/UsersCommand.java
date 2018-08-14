d9edb738fe60fdf754ea69e5d0e7135161af1010
Mark Czotter <mczotter@b2international.com>
2018-08-14 11:42:45 +0200

    SO-3109: add users login subcommand

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/core/console/UsersCommand.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/core/console/UsersCommand.java
@@ -19,6 +19,7 @@ import static com.google.common.collect.Lists.newArrayList;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import com.b2international.commons.CompareUtils;
 import com.b2international.commons.Pair;
@@ -26,6 +27,10 @@ import com.b2international.commons.extension.Component;
 import com.b2international.snowowl.core.ApplicationContext;
 import com.b2international.snowowl.datastore.session.IApplicationSessionManager;
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
+
+import picocli.CommandLine.HelpCommand;
+import picocli.CommandLine.Parameters;
 
 /**
  * @since 7.0
@@ -34,7 +39,11 @@ import com.google.common.base.Strings;
 @picocli.CommandLine.Command(
 	name = "users",
 	header = "Displays and manages user sessions",
-	description = "Displays information about user sessions and provides functionality to disconnect them from the server or prevent them from logging in, when required"
+	description = "Displays information about user sessions and provides functionality to disconnect them from the server or prevent them from logging in, when required",
+	subcommands = {
+		HelpCommand.class,
+		UsersCommand.LoginCommand.class
+	}
 )
 public final class UsersCommand extends Command {
 
@@ -51,6 +60,38 @@ public final class UsersCommand extends Command {
 				out.println("User: %s | session ID: %s", pair.getA(), pair.getB());
 			}
 		}
+	}
+
+	@picocli.CommandLine.Command(
+		name = "login",
+		header = "Manages non-administrator logins.",
+		description = "Enables/disables login for new, non-administrator sessions."
+	)
+	private static final class LoginCommand extends Command {
+
+		private static final Set<String> ALLOWED_SUBCOMMANDS = ImmutableSet.of("enabled", "disabled", "status"); 
+		
+		@Parameters(paramLabel = "enabled|disabled|status", description = "Command to check ")
+		String subCommand;
+		
+		@Override
+		public void run(CommandLineStream out) {
+			if (!ALLOWED_SUBCOMMANDS.contains(subCommand.toLowerCase())) {
+				out.print("Command usage: session login [enabled|disabled|status]");
+				return;
+			}
+			
+			IApplicationSessionManager applicationSessionManager = ApplicationContext.getInstance().getService(IApplicationSessionManager.class);
+			if (subCommand.equalsIgnoreCase("status")) {
+				out.println("Non-administrative logins are currently %s.", (applicationSessionManager.isLoginEnabled() ? "enabled" : "disabled"));
+				return;
+			}
+
+			final boolean loginEnabled = subCommand.equalsIgnoreCase("enabled");
+			applicationSessionManager.enableLogins(loginEnabled);
+			out.println("%s non-administrative logins.", (loginEnabled ? "Enabled" : "Disabled"));
+		}
+		
 	}
 
 }
