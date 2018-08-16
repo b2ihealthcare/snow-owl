36f3f6b565483ce0a496bc43fdbc79e0b171bd7c
Mark Czotter <mczotter@b2international.com>
2018-08-16 10:45:11 +0200

    [snomed] improve bulk delete performance...
    ...by bulk loading all deleted components before actually deleting them.

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/request/DeleteRequest.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/request/DeleteRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2017 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2018 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 /**
  * @since 4.5
  */
-final class DeleteRequest implements Request<TransactionContext, Boolean> {
+public final class DeleteRequest implements Request<TransactionContext, Boolean> {
 
 	@JsonProperty
 	@NotNull
@@ -48,6 +48,10 @@ final class DeleteRequest implements Request<TransactionContext, Boolean> {
 	public Boolean execute(TransactionContext context) {
 		context.delete(context.lookup(componentId, type), force);
 		return Boolean.TRUE;
+	}
+
+	public String getComponentId() {
+		return componentId;
 	}
 	
 }
