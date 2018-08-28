d0a7e62bb82eeb38235c1cd8d6b37d4aebeeda15
András Péteri <apeteri@b2international.com>
2018-08-28 17:53:56 +0200

    [index] Refactor test cases in SearchAfterTest

--- commons/com.b2international.index.api.tests/src/com/b2international/index/SearchAfterTest.java
+++ commons/com.b2international.index.api.tests/src/com/b2international/index/SearchAfterTest.java
@@ -16,23 +16,21 @@
 package com.b2international.index;
 
 import static com.google.common.collect.Maps.newHashMap;
-import static com.google.common.collect.Sets.newTreeSet;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeSet;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.b2international.index.Fixtures.Data;
 import com.b2international.index.query.Expressions;
 import com.b2international.index.query.Query;
+import com.b2international.index.query.Query.QueryBuilder;
 import com.b2international.index.query.SortBy;
 import com.b2international.index.query.SortBy.Order;
 import com.google.common.collect.ImmutableList;
@@ -65,16 +63,23 @@ public class SearchAfterTest extends BaseIndexTest {
 	}
 	
 	@Test
-	public void searchAfterImplicitDocIdSort() throws Exception {
-		final Hits<Data> allDocsSortedByDocId = search(Query.select(Data.class)
+	public void searchAfterAllowNull() throws Exception {
+		search(Query.select(Data.class)
 				.where(Expressions.matchAll())
+				.searchAfter(null)
+				.limit(NUM_DOCS)
+				.build())
+				.getHits();
+	}
+	
+	private void searchAfter(Function<QueryBuilder<Data>, Query.AfterWhereBuilder<Data>> queryCustomizer) throws Exception {
+		final Hits<Data> allDocs = search(queryCustomizer.apply(Query.select(Data.class))
 				.limit(NUM_DOCS)
 				.build());
 		
-		final List<Data> expectedHitsInOrder = allDocsSortedByDocId.stream().limit(10).collect(Collectors.toList());
+		final List<Data> expectedHitsInOrder = allDocs.stream().limit(10).collect(Collectors.toList());
 		
-		final Hits<Data> first5ActualHits = search(Query.select(Data.class)
-				.where(Expressions.matchAll())
+		final Hits<Data> first5ActualHits = search(queryCustomizer.apply(Query.select(Data.class))
 				.limit(5)
 				.build());
 				
@@ -85,8 +90,7 @@ public class SearchAfterTest extends BaseIndexTest {
 		
 		assertThat(first5Actual).startsWith(expectedHitsInOrder.stream().limit(5).toArray(size -> new Data[size]));
 		
-		final Hits<Data> second5ActualHits = search(Query.select(Data.class)
-				.where(Expressions.matchAll())
+		final Hits<Data> second5ActualHits = search(queryCustomizer.apply(Query.select(Data.class))
 				.searchAfter(first5ActualHits.getSearchAfter())
 				.limit(5)
 				.build());
@@ -97,44 +101,25 @@ public class SearchAfterTest extends BaseIndexTest {
 				.collect(Collectors.toList());
 		
 		assertThat(second5Actual).startsWith(expectedHitsInOrder.stream().skip(5).limit(5).toArray(size -> new Data[size]));
+		
 	}
 	
 	@Test
-	public void searchAfterSortOnField() throws Exception {
-		final Hits<Data> allDocsSortedByDocId = search(Query.select(Data.class)
-				.where(Expressions.matchAll())
-				.sortBy(SortBy.field("field1", Order.DESC))
-				.limit(NUM_DOCS)
-				.build());
-		
-		final List<Data> expectedHitsInOrder = allDocsSortedByDocId.stream().limit(10).collect(Collectors.toList());
-		
-		final Hits<Data> first5ActualHits = search(Query.select(Data.class)
-				.where(Expressions.matchAll())
-				.sortBy(SortBy.field("field1", Order.DESC))
-				.limit(5)
-				.build());
-				
-		final List<Data> first5Actual = first5ActualHits
-				.stream()
-				.limit(5)
-				.collect(Collectors.toList());
-		
-		assertThat(first5Actual).startsWith(expectedHitsInOrder.stream().limit(5).toArray(size -> new Data[size]));
-		
-		final Hits<Data> second5ActualHits = search(Query.select(Data.class)
-				.where(Expressions.matchAll())
-				.sortBy(SortBy.field("field1", Order.DESC))
-				.searchAfter(first5ActualHits.getSearchAfter())
-				.limit(5)
-				.build());
-
-		final List<Data> second5Actual = second5ActualHits
-				.stream()
-				.limit(5)
-				.collect(Collectors.toList());
-		
-		assertThat(second5Actual).startsWith(expectedHitsInOrder.stream().skip(5).limit(5).toArray(size -> new Data[size]));
+	public void searchAfterIndexSort() throws Exception {
+		searchAfter(b -> b.where(Expressions.matchAll()));
 	}
 	
+	@Test
+	public void searchAfterScoreSort() throws Exception {
+		searchAfter(b -> b
+				.where(Expressions.prefixMatch("field1", "field"))
+				.sortBy(SortBy.SCORE));
+	}
+	
+	@Test
+	public void searchAfterFieldSort() throws Exception {
+		searchAfter(b -> b
+				.where(Expressions.matchAll())
+				.sortBy(SortBy.field("field1", Order.DESC)));
+	}
 }
