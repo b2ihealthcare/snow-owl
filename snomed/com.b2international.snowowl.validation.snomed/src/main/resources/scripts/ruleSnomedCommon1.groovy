5f8b7dfc12c07f7f8252d020949ee3f5707d9cc0
norban <norban@b2international.com>
2018-08-31 11:04:06 +0200

    SO-3188 add effective times, to test effective time based validation

--- snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/ruleSnomedCommon1.groovy
+++ snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/ruleSnomedCommon1.groovy
@@ -1,13 +1,16 @@
 package scripts;
 
 import com.b2international.index.Hits
-import com.b2international.index.query.Expression
 import com.b2international.index.query.Expressions
 import com.b2international.index.query.Query
+import com.b2international.index.query.Expressions.ExpressionBuilder
 import com.b2international.index.revision.RevisionSearcher
 import com.b2international.snowowl.core.ComponentIdentifier
+import com.b2international.snowowl.core.date.EffectiveTimes
+import com.b2international.snowowl.snomed.common.SnomedRf2Headers
 import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptDocument
+import com.b2international.snowowl.snomed.datastore.index.entry.SnomedDocument
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRelationshipIndexEntry
 import com.google.common.collect.Lists
 
@@ -20,30 +23,42 @@ Iterable<Hits<String>> inactiveConceptBatches = searcher.scroll(Query.select(Str
 		.limit(10_000)
 		.build())
 
-List<ComponentIdentifier> issueDetails = Lists.newArrayList()
+List<ComponentIdentifier> issues = Lists.newArrayList()
 
+long effectiveTimeFilterValue = options.get(SnomedRf2Headers.FIELD_EFFECTIVE_TIME)
 inactiveConceptBatches.each({ conceptBatch ->
 	List<String> inactiveConceptIds = conceptBatch.getHits()
 
-	Expression invalidRelationshipExpression = Expressions.builder()
+	ExpressionBuilder invalidRelationshipExpression = Expressions.builder()
 			.filter(SnomedRelationshipIndexEntry.Expressions.active())
 			.should(SnomedRelationshipIndexEntry.Expressions.sourceIds(inactiveConceptIds))
 			.should(SnomedRelationshipIndexEntry.Expressions.typeIds(inactiveConceptIds))
 			.should(SnomedRelationshipIndexEntry.Expressions.destinationIds(inactiveConceptIds))
-			.build()
 
-	Iterable<Hits<String>> invalidRelationshipBatches = searcher.scroll(Query.select(String.class)
+	if (effectiveTimeFilterValue == 1) {
+		invalidRelationshipExpression.mustNot(SnomedDocument.Expressions.effectiveTime(EffectiveTimes.UNSET_EFFECTIVE_TIME))
+	}
+	
+	Iterable<Hits<String[]>> invalidRelationshipBatches = searcher.scroll(Query.select(String[].class)
 			.from(SnomedRelationshipIndexEntry.class)
-			.fields(SnomedRelationshipIndexEntry.Fields.ID)
-			.where(invalidRelationshipExpression)
+			.fields(SnomedRelationshipIndexEntry.Fields.ID, SnomedRelationshipIndexEntry.Fields.EFFECTIVE_TIME)
+			.where(invalidRelationshipExpression.build())
 			.limit(10_000)
 			.build())
 
 	invalidRelationshipBatches.each({ relationshipBatch ->
-		relationshipBatch.each({ relationshipId ->
-			issueDetails.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, relationshipId))
+		relationshipBatch.each({ hit ->
+			
+			if (EffectiveTimes.UNSET_EFFECTIVE_TIME == effectiveTimeFilterValue) {
+				if (EffectiveTimes.UNSET_EFFECTIVE_TIME == Long.parseLong(hit[1])) {
+					issues.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, hit[0]))
+				}
+			} else {
+				issues.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, hit[0]))
+			}
+			
 		})
 	})
 })
 
-return issueDetails
+return issues
