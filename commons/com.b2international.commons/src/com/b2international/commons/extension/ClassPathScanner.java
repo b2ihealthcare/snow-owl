1727e774d5d827fb43b2d12face71106ddc317c9
Zoltan Molnar <zmolnar@b2international.com>
2018-12-18 15:37:38 +0100

    [upgrade] Remove inner class and reflection

--- commons/com.b2international.commons/src/com/b2international/commons/extension/ClassPathScanner.java
+++ commons/com.b2international.commons/src/com/b2international/commons/extension/ClassPathScanner.java
@@ -17,9 +17,7 @@ package com.b2international.commons.extension;
 
 import static com.google.common.collect.Lists.newArrayList;
 
-import java.io.File;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Collection;
 import java.util.List;
@@ -30,30 +28,10 @@ import org.eclipse.osgi.service.resolver.BundleDescription;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.wiring.BundleWiring;
 
-import com.google.common.collect.ImmutableList;
-
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ClassInfoList;
 import io.github.classgraph.ScanResult;
-import nonapi.io.github.classgraph.ScanSpec;
-import nonapi.io.github.classgraph.classloaderhandler.AntClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.ClassLoaderHandlerRegistry;
-import nonapi.io.github.classgraph.classloaderhandler.ClassLoaderHandlerRegistry.ClassLoaderHandlerRegistryEntry;
-import nonapi.io.github.classgraph.classloaderhandler.EquinoxClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.EquinoxContextFinderClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.FelixClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.JBossClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.JPMSClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.OSGiDefaultClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.ParentLastDelegationOrderTestClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.URLClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.WeblogicClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.WebsphereLibertyClassLoaderHandler;
-import nonapi.io.github.classgraph.classloaderhandler.WebsphereTraditionalClassLoaderHandler;
-import nonapi.io.github.classgraph.classpath.ClasspathOrder;
-import nonapi.io.github.classgraph.utils.LogNode;
-import nonapi.io.github.classgraph.utils.ReflectionUtils;
 
 /**
  * @since 7.0
@@ -82,43 +60,11 @@ public enum ClassPathScanner {
 			}
 		}
 		
-		// XXX hacking default class loader registry to make ClassGraph work with Equinox 3.9
-		try {
-			Field f = ClassLoaderHandlerRegistry.class.getDeclaredField("CLASS_LOADER_HANDLERS");
-			f.setAccessible(true);
-			
-			Field modifiersField = Field.class.getDeclaredField("modifiers");
-			modifiersField.setAccessible(true);
-			modifiersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
-			
-			f.set(null, ImmutableList.<ClassLoaderHandlerRegistryEntry>builder()
-					// ClassLoaderHandlers for other ClassLoaders that are handled by ClassGraph
-					.add(new ClassLoaderHandlerRegistryEntry(AntClassLoaderHandler.class))
-			        .add(new ClassLoaderHandlerRegistryEntry(EquinoxClassLoaderHandler.class))
-			        .add(new ClassLoaderHandlerRegistryEntry(EquinoxContextFinderClassLoaderHandler.class))
-			        .add(new ClassLoaderHandlerRegistryEntry(FelixClassLoaderHandler.class))
-			        .add(new ClassLoaderHandlerRegistryEntry(JBossClassLoaderHandler.class))
-			        .add(new ClassLoaderHandlerRegistryEntry(WeblogicClassLoaderHandler.class))
-			        .add(new ClassLoaderHandlerRegistryEntry(WebsphereLibertyClassLoaderHandler.class))
-			        .add(new ClassLoaderHandlerRegistryEntry(WebsphereTraditionalClassLoaderHandler.class))
-			        // XXX Register PDE Dev Mode aware class loader handler to make it work with Equinox 3.9 class loading
-			        .add(new ClassLoaderHandlerRegistryEntry(PDEOSGiDefaultClassLoaderHandler.class))
-			        .add(new ClassLoaderHandlerRegistryEntry(OSGiDefaultClassLoaderHandler.class))
-	                // For unit testing of PARENT_LAST delegation order
-			        .add(new ClassLoaderHandlerRegistryEntry(ParentLastDelegationOrderTestClassLoaderHandler.class))
-	                // JPMS support
-			        .add(new ClassLoaderHandlerRegistryEntry(JPMSClassLoaderHandler.class))
-	                // Java 7/8 support (list last, as fallback)
-			        .add(new ClassLoaderHandlerRegistryEntry(URLClassLoaderHandler.class))
-					.build());
-		} catch (Exception e) {
-			throw new RuntimeException("Couldn't hack ClassGraph :-(", e);
-		}
-				
 		registry = new ClassGraph()
-			.disableRuntimeInvisibleAnnotations()
-			.overrideClassLoaders(classLoaders.toArray(new ClassLoader[classLoaders.size()]))
-			.scan();
+				.disableRuntimeInvisibleAnnotations()
+				.overrideClassLoaders(classLoaders.toArray(new ClassLoader[classLoaders.size()]))
+				.enableAllInfo()
+				.scan();
 	}
 	
 	/**
@@ -212,45 +158,6 @@ public enum ClassPathScanner {
 				}
 			})
 			.collect(Collectors.toList());
-	}
-
-	public static class PDEOSGiDefaultClassLoaderHandler extends OSGiDefaultClassLoaderHandler {
-	
-		@Override
-		public void handle(ScanSpec scanSpec, ClassLoader classloader, ClasspathOrder classpathOrderOut, LogNode log) {
-			final Object classpathManager = ReflectionUtils.invokeMethod(classloader, "getClasspathManager", false);
-	        final Object[] entries = (Object[]) ReflectionUtils.getFieldVal(classpathManager, "entries", false);
-	        if (entries != null) {
-	            for (int i = 0; i < entries.length; i++) {
-	                final Object bundleFile = ReflectionUtils.invokeMethod(entries[i], "getBundleFile", false);
-	                final File baseFile = (File) ReflectionUtils.invokeMethod(bundleFile, "getBaseFile", false);
-	                if (baseFile != null) {
-	                	try {
-							Class<?> devClassPathHelperClass = Class.forName("org.eclipse.osgi.internal.baseadaptor.DevClassPathHelper", false, classloader);
-							handleEquinox39(baseFile, classloader, classpathOrderOut, log, devClassPathHelperClass);
-						} catch (ClassNotFoundException ignored) {
-						}
-	                }
-	            }
-	        }
-		}
-
-		private static void handleEquinox39(final File baseFile, ClassLoader classloader, ClasspathOrder classpathOrderOut, LogNode log, Class<?> devClassPathHelperClass) {
-			Boolean inDevMode = (Boolean) ReflectionUtils.invokeStaticMethod(devClassPathHelperClass, "inDevelopmentMode", true);
-        	if (inDevMode && baseFile.isDirectory()) {
-        		Bundle bundle = (Bundle) ReflectionUtils.invokeMethod(classloader, "getBundle", true);
-	    		String[] devClassPath = (String[]) ReflectionUtils.invokeStaticMethod(devClassPathHelperClass, "getDevClassPath", String.class, bundle.getSymbolicName(), true);
-	    		for (String cp : devClassPath) {
-	    			final File cpFile = new File(baseFile, cp);
-	    			if (cpFile.isDirectory()) {
-	    				classpathOrderOut.addClasspathElement(cpFile.getPath(), classloader, log);
-	    			}
-	    		}
-	    	} else {
-	    		classpathOrderOut.addClasspathElement(baseFile.getPath(), classloader, log);
-	    	}
-		}
-		
 	}
 
 }
