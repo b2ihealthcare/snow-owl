873d2c4e2410055bd39dbe7951228c0f89b04cf7
Zoltan Molnar <zmolnar@b2international.com>
2019-01-31 10:50:58 +0100

    SO-3380: Make export type configurable, add base URL configurable var...
    ... add support for saving to predetermined folder, fix comments,
    separate variables

--- releng/com.b2international.snowowl.server.update/assembly/common/scripts/snowowl_export.sh
+++ releng/com.b2international.snowowl.server.update/assembly/common/scripts/snowowl_export.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2018 B2i Healthcare Pte Ltd, http://b2i.sg
+# Copyright 2019 B2i Healthcare Pte Ltd, http://b2i.sg
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,9 +16,11 @@
 #
 
 #
-# Snow Owl terminology server deploy script
+# Snow Owl terminology server export script
 # See usage or execute the script with the -h flag to get further information.
 #
+
+##### Configurable params #####
 
 # The user for the user authentication for the REST API.
 SNOW_OWL_USER=""
@@ -29,44 +31,40 @@ SNOW_OWL_PASSWORD=""
 # The export type to use for the export config.
 EXPORT_TYPE="DELTA"
 
-# Forces the export config to export concepts and relationships only.
-IS_CONCEPT_AND_RELATIONSHIPS_ONLY=${false}
-
 # The target folder to save the exported snapshot.
 TARGET_FOLDER=""
 
 # The server where the export should be initiated on
 TARGET_ENVIRONMENT=""
 
-# UUID of the snapshot export.
-EXPORT_UUID=""
+# Base URL for the server endpoints
+SNOW_OWL_BASE_URL=""
+
+##### Global variables / Constants #####
 
 # Media type to use for the export configuration.
 MEDIA_TYPE="application/vnd.com.b2international.snowowl+json"
 
-# Accept header for archive download.
-ACCEPT_HEADER="application/octet-stream"
-
-# The address where the export config endpoint can be found.
-EXPORT_CONFIG_POST_ENDPOINT="snowowl/snomed-ct/v3/exports"
-
-# The address where the branch get endpoint can be found.
-BRANCH_GET_ENDPOINT="snowowl/snomed-ct/v3/branches"
+# The uuid of the export request given by Snow Owl.
+EXPORT_UUID=""
 
 # The input data for the export config.
 EXPORT_CONFIG_POST_INPUT=""
 
-# The uuid of the export request given by Snow Owl.
-EXPORT_UUID=""
+# Accept header for archive download.
+ACCEPT_HEADER="application/octet-stream"
 
-# The input data for the code system version create config.
-CODE_SYSTEM_VERSION_CREATE_POST_INPUT=""
+# Forces the export config to export concepts and relationships only.
+IS_CONCEPT_AND_RELATIONSHIPS_ONLY=${false}
+
+# The address where the export config endpoint can be found.
+EXPORT_CONFIG_POST_ENDPOINT=""
 
 # The initial name of the export.
-EXPORT_FILE_NAME="snow_owl_delta_export"
+EXPORT_FILE_NAME="snow_owl_${EXPORT_TYPE}_export"
 
 # The renamed version of the export file.
-RENAMED_EXPORT_FILE="snow_owl_delta_export"
+RENAMED_EXPORT_FILE=""
 
 # Calculates the current location of this specific bash script (works until the path is not a symlink)
 SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -74,9 +72,11 @@ SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 usage() {
 
 	cat <<EOF
+
 NAME:
-	Automated delta export script. The export file will be saved where the script is located. By defining a target envrionemnt this script doesn't need to be present on the server.
-OPTIONS:
+	Snow Owl export script. The export file will be saved where the user has set it to.
+    
+    OPTIONS:
 	-h
 		Show this help
 	-u
@@ -85,12 +85,20 @@ OPTIONS:
 		Define the password for the above user
     -t
         Target environment which server the export should be initiated on
+    -f
+        Target folder where the exported content should be saved
+    -e
+        Export type for the export configuration (possible values are SNAPSHOT, DELTA, FULL)
+    -b
+        Base URL of the Snow Owl server
 NOTES:
 	This script can be used to initiate a delta export job that will run an export from a snow owl server every day at 20:00 server time and it will save it to the folder the script is within.
 	Mandatory variables:
-		- SNOW OWL user to use for the snapshot export
+		- SNOW OWL user to use for the Snow Owl export
 	    - SNOW OWL password for the above user
         - The target enviroment where the Snow Owl server runs
+        - Target folder to save the exported content
+        - Snow Owl server base url eg.: "snowowl/snomed-ct/v3"
 EOF
 
 }
@@ -100,6 +108,14 @@ validate_variables() {
 	check_if_empty "${SNOW_OWL_PASSWORD}" "Snow Owl password must be specified"
 	check_if_empty "${TARGET_FOLDER}" "Target folder must be specified"
     check_if_empty "${TARGET_ENVIRONMENT}" "Target environment must be specified"
+    check_if_empty "${SNOW_OWL_BASE_URL}" "Snow Owl base URL must be specified"
+
+    if [[ "${EXPORT_TYPE}" != "DELTA" && "${EXPORT_TYPE}" != "SNAPSHOT" && "${EXPORT_TYPE}" != "FULL" ]]; then
+        echo "ERROR: Unrecognized export type was given as parameter: ${EXPORT_TYPE}"
+        exit 1;
+    fi
+
+    EXPORT_CONFIG_POST_ENDPOINT="${SNOW_OWL_BASE_URL}/exports"
 
     if [[ ! -d "${TARGET_FOLDER}" ]]; then
         echo "Creating target folder"
@@ -117,24 +133,29 @@ check_if_empty() {
 
 export_delta() {
     echo "Creating snapshot export config"
-    EXPORT_CONFIG_POST_INPUT='{"branchPath":"MAIN","codeSystemShortName":"SNOMEDCT","type":"DELTA"}'
-    EXPORT_UUID="$(curl -u "${SNOW_OWL_USER}:${SNOW_OWL_PASSWORD}" -i -X POST -H "Content-type: ${MEDIA_TYPE}" "http://${TARGET_ENVIRONMENT}:8080/${EXPORT_CONFIG_POST_ENDPOINT}" -d "${EXPORT_CONFIG_POST_INPUT}" | tr -d '\r' | sed -En 's/^Location: (.*)/\1/p')"
+    EXPORT_CONFIG_POST_INPUT='{"branchPath": "MAIN", "type": "'"${EXPORT_TYPE}"'", "codeSystemShortName": "SNOMEDCT"}'
+    EXPORT_LOCATION="http://"${TARGET_ENVIRONMENT}":8080/"${EXPORT_CONFIG_POST_ENDPOINT}""
+
+    echo "Initating "${EXPORT_TYPE}" export with config: "${EXPORT_CONFIG_POST_INPUT}" on target: "${EXPORT_LOCATION}""
+
+    EXPORT_UUID="$(curl -u "${SNOW_OWL_USER}:${SNOW_OWL_PASSWORD}" -i -X POST -H "Content-type: ${MEDIA_TYPE}" "${EXPORT_LOCATION}" -d "${EXPORT_CONFIG_POST_INPUT}" | tr -d '\r' | sed -En 's/^Location: (.*)/\1/p')"
     download_delta
 }
 
 download_delta() {
-    echo "EXPORT_UUID: ${EXPORT_UUID}"
     EXPORT_DOWNLOAD_GET_ENDPOINT="${EXPORT_UUID}/archive"
 
-    echo "Downloading snapshot export with UUID: ${EXPORT_DOWNLOAD_GET_ENDPOINT}"
-
-    curl -u "${SNOW_OWL_USER}:${SNOW_OWL_PASSWORD}" -X GET -H "Accept: ${ACCEPT_HEADER}" -ko "${EXPORT_FILE_NAME}" "${EXPORT_DOWNLOAD_GET_ENDPOINT}" 
+    echo "Downloading ${EXPORT_TYPE} export with UUID: ${EXPORT_DOWNLOAD_GET_ENDPOINT}"
 
     DATE=$(date +"%Y%m%d_%H%M%S")
 
     RENAMED_EXPORT_FILE="${EXPORT_FILE_NAME}_${DATE}.zip"
+
+    curl -u "${SNOW_OWL_USER}:${SNOW_OWL_PASSWORD}" -X GET -H "Accept: ${ACCEPT_HEADER}" -ko "${RENAMED_EXPORT_FILE}" "${EXPORT_DOWNLOAD_GET_ENDPOINT}"
      
-    mv "${EXPORT_FILE_NAME}" "${RENAMED_EXPORT_FILE}"
+    cd "${SCRIPT_LOCATION}"
+
+    mv "${RENAMED_EXPORT_FILE}" "${TARGET_FOLDER}/${RENAMED_EXPORT_FILE}"
 }
 
 execute() {
@@ -148,7 +169,7 @@ execute() {
     exit 0
 }
 
-while getopts "u:p::t:h" option; do
+while getopts "u:p::t:f:eb:h" option; do
     case "${option}" in
     h)
         usage
@@ -160,6 +181,12 @@ while getopts "u:p::t:h" option; do
         SNOW_OWL_PASSWORD=${OPTARG};;
     t)
         TARGET_ENVIRONMENT=${OPTARG};;
+    f)
+        TARGET_FOLDER=${OPTARG};;
+    e)
+        EXPORT_TYPE=${OPTARG};;
+    b)
+        SNOW_OWL_BASE_URL=${OPTARG};;
     \?)
         echo "Invalid option: {$OPTARG}." >&2
         usage
