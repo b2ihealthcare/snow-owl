3b2a89c14fe6ecdc5d29276ab7085928a8beca81
András Péteri <apeteri@b2international.com>
2019-02-01 15:26:40 +0100

    [snomed] Add line breaks to ClassificationTaskDocument update scripts

--- snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/index/ClassificationTaskDocument.java
+++ snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/index/ClassificationTaskDocument.java
@@ -36,13 +36,15 @@ import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
  */
 @Doc(type="classificationtask")
 @JsonDeserialize(builder=ClassificationTaskDocument.Builder.class)
-@Script(name=ClassificationTaskDocument.Scripts.RUNNING, script="ctx._source.status = 'RUNNING'; ctx._source.timestamp = params.timestamp")
+@Script(name=ClassificationTaskDocument.Scripts.RUNNING, script="ctx._source.status = 'RUNNING'; "
+		+ "ctx._source.timestamp = params.timestamp")
 @Script(name=ClassificationTaskDocument.Scripts.COMPLETED, script="ctx._source.status = 'COMPLETED'; "
 		+ "ctx._source.completionDate = params.completionDate; "
 		+ "ctx._source.hasInferredChanges = params.hasInferredChanges; "
 		+ "ctx._source.hasRedundantStatedChanges = params.hasRedundantStatedChanges; "
 		+ "ctx._source.hasEquivalentConcepts = params.hasEquivalentConcepts")
-@Script(name=ClassificationTaskDocument.Scripts.FAILED, script="ctx._source.status = 'FAILED'; ctx._source.completionDate = params.completionDate")
+@Script(name=ClassificationTaskDocument.Scripts.FAILED, script="ctx._source.status = 'FAILED'; "
+		+ "ctx._source.completionDate = params.completionDate")
 @Script(name=ClassificationTaskDocument.Scripts.DELETED, script="ctx._source.deleted = true")
 @Script(name=ClassificationTaskDocument.Scripts.SAVING_IN_PROGRESS, script="ctx._source.status = 'SAVING_IN_PROGRESS'")
 @Script(name=ClassificationTaskDocument.Scripts.SAVED, script="ctx._source.status = 'SAVED'; "
