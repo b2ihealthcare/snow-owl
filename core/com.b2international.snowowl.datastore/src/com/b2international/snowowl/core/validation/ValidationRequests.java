20e0e2a72ad8864c96d711014ea3d25f9e8ebb46
Zoltan Molnar <zmolnar@b2international.com>
2019-02-20 16:22:58 +0100

    SO-3379: Move validation related logic to ValidationRequests from the UI

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/core/validation/ValidationRequests.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/core/validation/ValidationRequests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2017-2019 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,14 +15,18 @@
  */
 package com.b2international.snowowl.core.validation;
 
+import com.b2international.snowowl.core.branch.Branch;
 import com.b2international.snowowl.core.validation.issue.ValidationIssueRequests;
 import com.b2international.snowowl.core.validation.rule.ValidationRuleRequests;
 import com.b2international.snowowl.core.validation.whitelist.ValidationWhiteListRequests;
+import com.b2international.snowowl.datastore.remotejobs.RemoteJobEntry;
 
 /**
  * @since 6.0
  */
 public final class ValidationRequests {
+	
+	public static final String VALIDATION_JOB_ID_PREFIX = "validation-" ; //$NON-NLS-N$
 
 	private ValidationRequests() {}
 	
@@ -42,5 +46,23 @@ public final class ValidationRequests {
 		
 		return new ValidateRequestBuilder();
 	}
-
+	
+	/**
+	 * Creates a unique validation id
+	 * 
+	 * @param codeSystemEntry
+	 * @return the unique id.
+	 */
+	public static String createUniqueValidationId(String shortName, String branch) {
+		return String.format("%s%s%s%s", VALIDATION_JOB_ID_PREFIX, shortName, Branch.SEPARATOR, branch);
+	}
+	
+	public static boolean isValidationJob(RemoteJobEntry job) {
+		return job != null && job.getId().startsWith(ValidationRequests.VALIDATION_JOB_ID_PREFIX);
+	}
+	
+	public static boolean isRelatedBranch(RemoteJobEntry job, String branch) {
+		return isValidationJob(job) && job.getId().endsWith(branch);
+	}
+	
 }
