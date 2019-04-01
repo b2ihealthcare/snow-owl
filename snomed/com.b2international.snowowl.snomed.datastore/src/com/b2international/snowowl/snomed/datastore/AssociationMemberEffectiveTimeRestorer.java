40b9da9344fe777ea27ff0137263636bdfa6153f
Zoltan Molnar <zmolnar@b2international.com>
2019-04-01 18:12:55 +0200

    OWLAP-51: Fix class cast exception

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/AssociationMemberEffectiveTimeRestorer.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/AssociationMemberEffectiveTimeRestorer.java
@@ -16,6 +16,7 @@
 package com.b2international.snowowl.snomed.datastore;
 
 import com.b2international.snowowl.snomed.common.SnomedRf2Headers;
+import com.b2international.snowowl.snomed.core.domain.SnomedConcept;
 import com.b2international.snowowl.snomed.core.domain.refset.SnomedReferenceSetMember;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedAssociationRefSetMember;
 import com.b2international.snowowl.snomed.snomedrefset.SnomedRefSetMember;
@@ -28,9 +29,9 @@ public final class AssociationMemberEffectiveTimeRestorer extends MemberEffectiv
 	@Override
 	protected boolean canRestoreMemberEffectiveTime(SnomedRefSetMember memberToRestore, SnomedReferenceSetMember previousMember) {
 		final SnomedAssociationRefSetMember associationMemberToRestore = (SnomedAssociationRefSetMember) memberToRestore;
-		final String previousMemberTargetComponentId = (String) previousMember.getProperties().get(SnomedRf2Headers.FIELD_TARGET_COMPONENT);
+		final SnomedConcept previousMemberTargetComponent = (SnomedConcept) previousMember.getProperties().get(SnomedRf2Headers.FIELD_TARGET_COMPONENT);
 		
-		return previousMemberTargetComponentId != null && previousMemberTargetComponentId.equals(associationMemberToRestore.getTargetComponentId());
+		return previousMemberTargetComponent != null && previousMemberTargetComponent.getId().equals(associationMemberToRestore.getTargetComponentId());
 	}
 
 	
