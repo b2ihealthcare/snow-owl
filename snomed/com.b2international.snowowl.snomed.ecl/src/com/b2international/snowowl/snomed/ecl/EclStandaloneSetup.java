d9f4c9354333579377338a1594242d0952c7f16d
Zoltan Molnar <zmolnar@b2international.com>
2019-04-25 14:14:45 +0200

    [build] Convert Xtend classes to Java classes, remove Xtend dependencies
    Conflicts:
    	snomed/com.b2international.snowowl.snomed.datastore/pom.xml
    	snomed/com.b2international.snowowl.snomed.ql/pom.xml
    	tests/com.b2international.snowowl.test.commons/pom.xml
    	tests/com.b2international.snowowl.test.commons/src/com/b2international/snowowl/test/commons/rest/RestExtensions.xtend

--- snomed/com.b2international.snowowl.snomed.ecl/src/com/b2international/snowowl/snomed/ecl/EclStandaloneSetup.java
+++ snomed/com.b2international.snowowl.snomed.ecl/src/com/b2international/snowowl/snomed/ecl/EclStandaloneSetup.java
@@ -1,26 +1,26 @@
 /*
- * Copyright 2011-2017 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2019 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.snowowl.snomed.ecl
+package com.b2international.snowowl.snomed.ecl;
 
 /**
  * Initialization support for running Xtext languages without Equinox extension registry.
  */
-class EclStandaloneSetup extends EclStandaloneSetupGenerated {
+public class EclStandaloneSetup extends EclStandaloneSetupGenerated {
 
-	def static void doSetup() {
-		new EclStandaloneSetup().createInjectorAndDoEMFRegistration()
+	public static void doSetup() {
+		new EclStandaloneSetup().createInjectorAndDoEMFRegistration();
 	}
 }
