3faf781eaabf9dd437d4bd4d44eaf969ac6694b0
Mark Czotter <mczotter@b2international.com>
2019-09-13 10:39:26 +0200

    [build] try to fix javax.naming compile error issue

--- core/com.b2international.snowowl.identity/src/com/b2international/snowowl/identity/IdentityPlugin.java
+++ core/com.b2international.snowowl.identity/src/com/b2international/snowowl/identity/IdentityPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2017-2019 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ package com.b2international.snowowl.identity;
 import java.util.List;
 
 import com.b2international.commons.extension.Component;
-import com.b2international.snowowl.core.SnowOwl;
 import com.b2international.snowowl.core.config.SnowOwlConfiguration;
 import com.b2international.snowowl.core.setup.ConfigurationRegistry;
 import com.b2international.snowowl.core.setup.Environment;
