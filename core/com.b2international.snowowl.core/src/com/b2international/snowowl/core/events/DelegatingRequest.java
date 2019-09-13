ecaf2c0224810f69ca6e889bc254eb35ea872c90
Mark Czotter <mczotter@b2international.com>
2019-09-13 15:08:49 +0200

    SO-3750: pull up getNestedRequests() to Request interface

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/events/DelegatingRequest.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/events/DelegatingRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2018 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2019 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 package com.b2international.snowowl.core.events;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Collection;
 
 import com.b2international.snowowl.core.ServiceProvider;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -91,5 +93,10 @@ public abstract class DelegatingRequest<C extends ServiceProvider, T extends Ser
 	public final ClassLoader getClassLoader() {
 		return next.getClassLoader();
 	}
-
+	
+	@Override
+	public Collection<Request<?, ?>> getNestedRequests() {
+		return next.getNestedRequests();
+	}
+	
 }
