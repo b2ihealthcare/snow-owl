a621ffa2f90d90755a3ff7a956eda84911c4034e
Zoltan Molnar <zmolnar@b2international.com>
2019-09-20 16:11:32 +0200

    SO-3763: Implement generic description length validation script

--- snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule_description_text_length.groovy
+++ snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule_description_text_length.groovy
@@ -7,6 +7,23 @@ import com.b2international.index.query.Expressions.ExpressionBuilder
 import com.b2international.index.revision.RevisionSearcher
 import com.b2international.snowowl.core.ComponentIdentifier
 import com.b2international.snowowl.core.date.EffectiveTimes
+import com.b2international.index.Hits
+import com.b2international.index.aggregations.Aggregation
+import com.b2international.index.aggregations.AggregationBuilder
+import com.b2international.index.query.Expressions
+import com.b2international.index.query.Query
+import com.b2international.index.query.Expressions.ExpressionBuilder
+import com.b2international.index.revision.RevisionSearcher
+import com.b2international.snowowl.core.ComponentIdentifier
+import com.b2international.snowowl.core.date.EffectiveTimes
+import com.b2international.snowowl.core.domain.BranchContext
+import com.b2international.snowowl.snomed.SnomedConstants.Concepts
+import com.b2international.snowowl.snomed.common.SnomedRf2Headers
+import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants
+import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptDocument
+import com.b2international.snowowl.snomed.datastore.index.entry.SnomedDescriptionIndexEntry
+import com.b2international.snowowl.snomed.datastore.index.entry.SnomedDocument
+
 import com.b2international.snowowl.snomed.SnomedConstants.Concepts
 import com.b2international.snowowl.snomed.common.SnomedRf2Headers
 import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants
@@ -17,6 +34,8 @@ import com.b2international.snowowl.snomed.datastore.index.entry.SnomedDocument
 import com.b2international.snowowl.snomed.datastore.request.SnomedRequests
 import com.google.common.collect.HashMultimap
 import com.google.common.collect.Multimap
+import com.google.common.collect.Lists
+import com.google.common.collect.ImmutableMap
 
 final RevisionSearcher searcher = ctx.service(RevisionSearcher.class)
 
@@ -42,31 +61,25 @@ for (Integer length : descriptionTypesByMaxLength.keySet()) {
 
     final ExpressionBuilder activeDescriptionFilter = Expressions.builder()
             .filter(SnomedDescriptionIndexEntry.Expressions.active())
-            .filter(SnomedDescriptionIndexEntry.Expressions.type(descriptionTypes))
-            .filter(SnomedDescriptionIndexEntry.Expressions.matchTermRegex(getRegex(length)))
+            .filter(SnomedDescriptionIndexEntry.Expressions.types(descriptionTypes))
+            .filter(Expressions.scriptQuery("descriptionLength", ImmutableMap.of("length", length)))
 
     if (params.isUnpublishedOnly) {
         activeDescriptionFilter.filter(SnomedDocument.Expressions.effectiveTime(EffectiveTimes.UNSET_EFFECTIVE_TIME))
     }
 
-    final Iterable<Hits<String>> queryResult =  searcher.scroll(Query.select(String.class)
+    final Iterable<Hits<String>> invalidDescriptions = searcher.scroll(Query.select(String.class)
             .from(SnomedDescriptionIndexEntry.class)
             .fields(SnomedDescriptionIndexEntry.Fields.ID)
             .where(activeDescriptionFilter.build())
-            .limit(10_000)
+            .limit(20_000)
             .build())
 
-
-
-    queryResult.each({hits ->
-        for (String descriptionId : hits) {
-            issues.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.DESCRIPTION_NUMBER, descriptionId))
-        }
+    invalidDescriptions.each({ descriptionBatch ->
+        descriptionBatch.each({ id ->
+            issues.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.DESCRIPTION_NUMBER, id))
+        })
     })
 }
 
 return issues
-
-def getRegex(String maximumCharacters) {
-    return String.format("^(?=[\\S\\s]{%s,}\$)[\\S\\s]*", maximumCharacters)
-}
