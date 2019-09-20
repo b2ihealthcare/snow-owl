3ef3be5b61ca753f9df75eed5af7cb18b340d961
Mark Czotter <mczotter@b2international.com>
2019-09-20 09:17:53 +0200

    SO-3750: move SnomedApiConfig class to component-scanned package

--- snomed/com.b2international.snowowl.snomed.core.rest/src/com/b2international/snowowl/core/rest/SnomedApiConfig.java
+++ snomed/com.b2international.snowowl.snomed.core.rest/src/com/b2international/snowowl/core/rest/SnomedApiConfig.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.snowowl.snomed.core.rest;
+package com.b2international.snowowl.core.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
