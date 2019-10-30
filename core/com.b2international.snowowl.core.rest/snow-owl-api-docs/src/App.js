bf7db0edbc8bda0c4be5378b85675fe225ab0be9
Mark Czotter <mczotter@b2international.com>
2019-10-30 18:08:47 +0100

    [docs] add frontend-maven-plugin with skeleton create-react-app...
    ...to build custom api-docs site

---
+++ core/com.b2international.snowowl.core.rest/snow-owl-api-docs/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
+ 
+const App = () => <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+
+export default App;
