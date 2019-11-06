cfa797a8cd1714dbf5fc0a840f88777f339dd802
Mark Czotter <mczotter@b2international.com>
2019-11-06 13:35:31 +0100

    [api] commit progress on swagger UI refactor

--- core/com.b2international.snowowl.core.rest/snow-owl-api-docs/src/App.js
+++ core/com.b2international.snowowl.core.rest/snow-owl-api-docs/src/App.js
@@ -1,12 +1,13 @@
 import 'antd/dist/antd.css';
-import 'swagger-ui-react/swagger-ui.css';
+//import 'swagger-ui-react/swagger-ui.css';
+import 'swagger-ui-themes/themes/3.x/theme-flattop.css'
 
 import React from 'react';
 import SwaggerUI from "swagger-ui-react"
-import { Layout, Tabs, Row, Col, BackTop } from 'antd';
+import { Layout, Tabs, BackTop, Menu, Icon } from 'antd';
 
 const { TabPane } = Tabs;
-const { Header, Content } = Layout;
+const { Header, Content, Sider } = Layout;
 
 const soBaseUrl = process.env.REACT_APP_SO_BASE_URL
 
@@ -14,32 +15,45 @@ const App = () => (
   <>
     <BackTop />
     <Layout>
-      <Header>
-        <Row>
-          <Col span={12} offset={6}>
-            <img src={`${process.env.PUBLIC_URL}/logo-snow-owl.png`} alt="logo-snow-owl" height="50" style={{"max-width":"100%"}} />
-          </Col>
-        </Row>
-      </Header>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+        }}
+      >
+        <div className="logo">
+          <img src={`${process.env.PUBLIC_URL}/logo-snow-owl.png`} alt="logo-snow-owl" height="50" style={{"max-width":"100%"}} />
+        </div>
+        <Menu
+          style={{ width: 256 }}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark"
+        >
+          <Menu.Item key="1">Admin API</Menu.Item>
+          <Menu.Item key="2">SNOMED CT API</Menu.Item>
+          <Menu.Item key="3">FHIR API</Menu.Item>
+          <Menu.Item key="4">CIS API</Menu.Item>
+        </Menu>
+      </Sider>
       <Content>
-        <Row>
-          <Col span={12} offset={6}>
-            <Tabs defaultActiveKey="admin">
-              <TabPane tab="Admin API" key="admin">
-                <SwaggerUI url={`${soBaseUrl}/api-docs?group=admin`} />
-              </TabPane>
-              <TabPane tab="SNOMED CT API" key="snomed">
-                <SwaggerUI url={`${soBaseUrl}/api-docs?group=snomed`} />
-              </TabPane>
-              <TabPane tab="FHIR API" key="fhir">
-                <SwaggerUI url={`${soBaseUrl}/api-docs?group=fhir`} />
-              </TabPane>
-              <TabPane tab="CIS API" key="cis">
-                <SwaggerUI url={`${soBaseUrl}/api-docs?group=cis`} />
-              </TabPane>
-            </Tabs>
-          </Col>
-        </Row>
+        {/* <Tabs defaultActiveKey="admin">
+          <TabPane tab="Admin API" key="admin">
+            <SwaggerUI url={`${soBaseUrl}/api-docs?group=admin`} />
+          </TabPane>
+          <TabPane tab="SNOMED CT API" key="snomed">
+            <SwaggerUI url={`${soBaseUrl}/api-docs?group=snomed`} />
+          </TabPane>
+          <TabPane tab="FHIR API" key="fhir">
+            <SwaggerUI url={`${soBaseUrl}/api-docs?group=fhir`} />
+          </TabPane>
+          <TabPane tab="CIS API" key="cis">
+            <SwaggerUI url={`${soBaseUrl}/api-docs?group=cis`} />
+          </TabPane>
+        </Tabs> */}
       </Content>
     </Layout>
   </>
