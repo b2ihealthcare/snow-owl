f50a193fdb8061034aea69cc0aa4dc49749ca0ab
Mark Czotter <mczotter@b2international.com>
2019-11-07 11:17:32 +0100

    [api] commit final version of new minimal API docs site

--- core/com.b2international.snowowl.core.rest/snow-owl-api-docs/src/App.js
+++ core/com.b2international.snowowl.core.rest/snow-owl-api-docs/src/App.js
@@ -1,62 +1,64 @@
 import 'antd/dist/antd.css';
-//import 'swagger-ui-react/swagger-ui.css';
-import 'swagger-ui-themes/themes/3.x/theme-flattop.css'
+import 'swagger-ui-react/swagger-ui.css';
 
 import React from 'react';
 import SwaggerUI from "swagger-ui-react"
-import { Layout, Tabs, BackTop, Menu, Icon } from 'antd';
+import { Layout, BackTop, Menu } from 'antd';
 
-const { TabPane } = Tabs;
-const { Header, Content, Sider } = Layout;
+const { Content, Sider } = Layout;
 
-const soBaseUrl = process.env.REACT_APP_SO_BASE_URL
+class App extends React.Component {
 
-const App = () => (
-  <>
-    <BackTop />
-    <Layout>
-      <Sider
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: 0,
-        }}
-      >
-        <div className="logo">
-          <img src={`${process.env.PUBLIC_URL}/logo-snow-owl.png`} alt="logo-snow-owl" height="50" style={{"max-width":"100%"}} />
-        </div>
-        <Menu
-          style={{ width: 256 }}
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          theme="dark"
-        >
-          <Menu.Item key="1">Admin API</Menu.Item>
-          <Menu.Item key="2">SNOMED CT API</Menu.Item>
-          <Menu.Item key="3">FHIR API</Menu.Item>
-          <Menu.Item key="4">CIS API</Menu.Item>
-        </Menu>
-      </Sider>
-      <Content>
-        {/* <Tabs defaultActiveKey="admin">
-          <TabPane tab="Admin API" key="admin">
-            <SwaggerUI url={`${soBaseUrl}/api-docs?group=admin`} />
-          </TabPane>
-          <TabPane tab="SNOMED CT API" key="snomed">
-            <SwaggerUI url={`${soBaseUrl}/api-docs?group=snomed`} />
-          </TabPane>
-          <TabPane tab="FHIR API" key="fhir">
-            <SwaggerUI url={`${soBaseUrl}/api-docs?group=fhir`} />
-          </TabPane>
-          <TabPane tab="CIS API" key="cis">
-            <SwaggerUI url={`${soBaseUrl}/api-docs?group=cis`} />
-          </TabPane>
-        </Tabs> */}
-      </Content>
-    </Layout>
-  </>
-)
+  state = {
+    selectedKey: 'admin'
+  }
+
+  onMenuSelect = (e) => {
+    this.setState({
+      selectedKey: e.key
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <BackTop />
+        <Layout>
+          <Sider
+            style={{
+              overflow: 'auto',
+              height: '100vh',
+              position: 'fixed',
+              left: 0,
+            }}
+          >
+            <div className="logo">
+              <img src={`${process.env.PUBLIC_URL}/logo-snow-owl.png`} alt="logo-snow-owl" height="60" style={{maxWidth:"100%"}} />
+            </div>
+            <Menu
+              onSelect = { this.onMenuSelect }
+              defaultOpenKeys={['sub1']}
+              selectedKeys = { [ this.state.selectedKey ] }
+              mode="inline"
+              theme="dark"
+            >
+              <Menu.Item key="admin">Admin API</Menu.Item>
+              <Menu.Item key="snomed">SNOMED CT API</Menu.Item>
+              <Menu.Item key="fhir">FHIR API</Menu.Item>
+              <Menu.Item key="cis">CIS API</Menu.Item>
+            </Menu>
+          </Sider>
+          <Content>
+            <SwaggerUI 
+              url={`${process.env.REACT_APP_SO_BASE_URL}/api-docs?group=${this.state.selectedKey}`} 
+              docExpansion = "list"
+            />
+          </Content>
+        </Layout>
+      </>
+    )
+  }
+
+}
 
 export default App;
