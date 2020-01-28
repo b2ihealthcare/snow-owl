9ce6603e44197c1c9dc5007bb93b9c3a14d0c931
András Péteri <apeteri@b2international.com>
2020-01-28 15:46:31 +0100

    [core] Fix AccessControl subclass on RepositoryClearRequest, move...
    ...factory method of request builder to RepositoryRequests

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/request/repository/RepositoryClearRequestBuilder.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/request/repository/RepositoryClearRequestBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2017-2020 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,16 +22,16 @@ import com.b2international.snowowl.datastore.request.TransactionalRequestBuilder
 
 /**
  * Builder for request that clear the repository content.
+ * 
  * @since 5.12
  */
-public final class RepositoryClearRequestBuilder extends BaseRequestBuilder<RepositoryClearRequestBuilder, TransactionContext, Boolean> implements TransactionalRequestBuilder<Boolean>{
-	
-	RepositoryClearRequestBuilder() {}
+public final class RepositoryClearRequestBuilder 
+	extends BaseRequestBuilder<RepositoryClearRequestBuilder, TransactionContext, Boolean> 
+	implements TransactionalRequestBuilder<Boolean>{
 	
 	@Override
 	protected Request<TransactionContext, Boolean> doBuild() {
 		final RepositoryClearRequest req = new RepositoryClearRequest();
 		return req;
 	}
-
 }
