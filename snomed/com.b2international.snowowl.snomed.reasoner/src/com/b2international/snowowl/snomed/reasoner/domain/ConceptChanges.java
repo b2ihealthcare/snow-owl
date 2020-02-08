cb17a33905fbb40907ed736539c5c2ef7db9e6c4
Mark Czotter <mczotter@b2international.com>
2020-02-08 09:36:33 +0100

    [qa] fix lgtm issue https://lgtm.com/rules/2049510531/

--- snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/domain/ConceptChanges.java
+++ snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/domain/ConceptChanges.java
@@ -27,11 +27,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public final class ConceptChanges extends PageableCollectionResource<ConceptChange> {
 
+	private static final long serialVersionUID = 1L;
+
 	/**
 	 * Instantiates an empty pageable collection for concept changes.
 	 * 
-	 * @param limit of items for a single page
-	 * @param total number of items in the result set
+	 * @param limit
+	 *            - number of items for a single page
+	 * @param total
+	 *            - number of items in the result set
 	 */
 	public ConceptChanges(final int limit, final int total) {
 		super(Collections.emptyList(), null, limit, total);
@@ -40,10 +44,14 @@ public final class ConceptChanges extends PageableCollectionResource<ConceptChan
 	/**
 	 * Instantiates a pageable collection of concept changes.
 	 * 
-	 * @param list of @link {@link ConceptChange}s
-	 * @param searchAfter for paging the result set with a live cursor
-	 * @param limit of items for a single page
-	 * @param total number of items in the result set
+	 * @param items
+	 *            - list of @link {@link ConceptChange}s
+	 * @param searchAfter
+	 *            - for paging the result set with a live cursor
+	 * @param limit
+	 *            - number of items for a single page
+	 * @param total
+	 *            - number of items in the result set
 	 */
 	@JsonCreator
 	public ConceptChanges(
