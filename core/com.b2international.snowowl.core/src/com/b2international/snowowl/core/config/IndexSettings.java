b802e591449b2525630d4dc6c5c2998fdc95e64b
Mark Czotter <mczotter@b2international.com>
2020-03-21 19:40:54 +0100

    [core] move datastore.config to core.config

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/config/IndexSettings.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/config/IndexSettings.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.snowowl.datastore.config;
+package com.b2international.snowowl.core.config;
 
 import com.b2international.commons.options.HashMapOptions;
 
--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/config/RepositoryConfiguration.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/config/RepositoryConfiguration.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.snowowl.datastore.config;
+package com.b2international.snowowl.core.config;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
