6f3a9467dddf37a2286bf81be4f268e235dd243a
Mark Czotter <mczotter@b2international.com>
2020-05-19 20:31:15 +0200

    [core] add new, file-based upload/download methods to Attachment API

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/attachments/Attachment.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/attachments/Attachment.java
@@ -15,7 +15,11 @@
  */
 package com.b2international.snowowl.core.attachments;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -23,9 +27,12 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import com.b2international.snowowl.core.ServiceProvider;
+import com.b2international.snowowl.core.api.SnowowlRuntimeException;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
 
 /**
  * Descriptor for attachments attached to the {@link AttachmentRegistry}.
@@ -111,6 +118,36 @@ public final class Attachment implements Serializable {
 	@Override
 	public String toString() {
 		return MoreObjects.toStringHelper(getClass()).add("attachmentId", attachmentId).add("fileName", fileName).toString();
+	}
+
+	/**
+	 * Downloads the file to the specified exportPath. The exportPath argument accepts both directory and file paths. Passing a file path replaces the
+	 * default file name with the desired one. If the target path is a file path and there is an existing file, it will automatically overrides the
+	 * target file.
+	 * 
+	 * @param context
+	 *            - the context to use for downloading the file
+	 * @param exportPath
+	 *            - either target directory where the file needs to be downloaded or an abolute path to a file which will replace the default file
+	 *            name included in this attachment
+	 * @return the absolute file path to the downloaded file
+	 * @since 7.7
+	 */
+	public Path download(ServiceProvider context, Path exportPath) {
+		Preconditions.checkNotNull(context, "Context cannot be null");
+		Preconditions.checkNotNull(exportPath, "ExportPath cannot be null");
+		Path resultFile;
+		if (Files.isDirectory(exportPath)) {
+			resultFile = exportPath.resolve(getFileName());
+		} else {
+			resultFile = exportPath;
+		}
+		try (OutputStream out = Files.newOutputStream(resultFile)) {
+			context.service(AttachmentRegistry.class).download(attachmentId, out);
+		} catch (IOException e) {
+			throw new SnowowlRuntimeException(String.format("Couldn't download file '%s'.", getFileName()), e);
+		}
+		return resultFile;
 	}
 
 }
