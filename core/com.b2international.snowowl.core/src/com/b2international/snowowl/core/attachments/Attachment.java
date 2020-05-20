17cc90f9ff69e9d45532232d580397a77a27e283
Mark Czotter <mczotter@b2international.com>
2020-05-20 11:41:35 +0200

    Update core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/attachments/Attachment.java
    Co-authored-by: András Péteri <apeteri@b2international.com>

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/attachments/Attachment.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/attachments/Attachment.java
@@ -123,7 +123,7 @@ public final class Attachment implements Serializable {
 
 	/**
 	 * Downloads the file to the specified exportPath. The exportPath argument accepts both directory and file paths. Passing a file path replaces the
-	 * default file name with the desired one. If the target path is a file path and there is an existing file, it will automatically overrides the
+	 * default file name with the desired one. If the target path is a file path and there is an existing file, it will automatically overwrite the
 	 * target file.
 	 * 
 	 * @param context
