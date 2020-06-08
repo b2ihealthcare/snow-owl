57c2daf599f6d049f5461a1b50fb48c0ae3ddd8c
aalaswadi <aalaswadi@b2international.com>
2020-06-08 17:56:55 +0200

    SO-4111: Add OWL axiom support to rule 663

--- snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule663.groovy
+++ snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule663.groovy
@@ -12,6 +12,7 @@ import com.b2international.snowowl.snomed.common.SnomedRf2Headers
 import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptDocument
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedDocument
+import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRefSetMemberIndexEntry
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRelationshipIndexEntry
 import com.google.common.collect.Lists
 
@@ -29,6 +30,7 @@ List<ComponentIdentifier> issues = Lists.newArrayList()
 inactiveConceptBatches.each({ conceptBatch ->
 	List<String> inactiveConceptIds = conceptBatch.getHits()
 	
+	//Look for relationships with references to inactive concepts
 	ExpressionBuilder invalidRelationshipExpression = Expressions.builder()
 			.filter(SnomedRelationshipIndexEntry.Expressions.active())
 			.should(SnomedRelationshipIndexEntry.Expressions.sourceIds(inactiveConceptIds))
@@ -51,6 +53,29 @@ inactiveConceptBatches.each({ conceptBatch ->
 			issues.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.RELATIONSHIP_NUMBER, id))
 		})
 	})
+	
+	//Look for OWL axiom members with references to inactive concepts
+	ExpressionBuilder invalidOWLAxiomExpression = Expressions.builder()
+		.filter(SnomedRefSetMemberIndexEntry.Expressions.active())
+		.should(SnomedRefSetMemberIndexEntry.Expressions.referencedComponentIds(inactiveConceptIds))
+		.should(SnomedRefSetMemberIndexEntry.Expressions.owlExpressionConcept(inactiveConceptIds))
+	
+	if (params.isUnpublishedOnly) {
+		invalidOWLAxiomExpression.filter(SnomedRefSetMemberIndexEntry.Expressions.effectiveTime(EffectiveTimes.UNSET_EFFECTIVE_TIME))
+	}
+	
+	Iterable<Hits<String>> invalidOWLAxiomBatches = searcher.scroll(Query.select(String.class)
+		.from(SnomedRefSetMemberIndexEntry.class)
+		.fields(SnomedRefSetMemberIndexEntry.Fields.ID)
+		.where(invalidOWLAxiomExpression.build())
+		.limit(10_000)
+		.build())
+
+	invalidOWLAxiomBatches.each({ axiomBatch ->
+		axiomBatch.each({ id ->
+			issues.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.REFSET_MEMBER_NUMBER, id))
+	})
+})
 })
 
 return issues
