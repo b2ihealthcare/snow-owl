7d13d7c23b4056e3f3f188886a2059733e60218f
Laura Molnár <lmolnar@b2international.com>
2020-06-16 12:14:23 +0200

    SO-4103: change filtering logic

--- snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule671.groovy
+++ snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule671.groovy
@@ -1,74 +1,119 @@
 package scripts;
 
-import com.b2international.index.Hits
 import com.b2international.index.query.Expressions
 import com.b2international.index.query.Query
 import com.b2international.index.query.Expressions.ExpressionBuilder
 import com.b2international.index.revision.RevisionSearcher
 import com.b2international.snowowl.core.ComponentIdentifier
 import com.b2international.snowowl.core.date.EffectiveTimes
-import com.b2international.snowowl.snomed.common.SnomedRf2Headers
 import com.b2international.snowowl.snomed.common.SnomedTerminologyComponentConstants
 import com.b2international.snowowl.snomed.common.SnomedConstants.Concepts
 import com.b2international.snowowl.snomed.core.domain.SnomedDescription
 import com.b2international.snowowl.snomed.core.domain.SnomedDescriptions
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptDocument
+import com.b2international.snowowl.snomed.datastore.index.entry.SnomedDescriptionIndexEntry
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedRefSetMemberIndexEntry
 import com.b2international.snowowl.snomed.datastore.request.SnomedRequests
+import com.google.common.collect.ArrayListMultimap
+import com.google.common.collect.ImmutableList
 import com.google.common.collect.Lists
-import com.google.common.collect.Maps
 import com.google.common.collect.Sets
 
 final Set<ComponentIdentifier> issues = Sets.newHashSet()
 final RevisionSearcher searcher = ctx.service(RevisionSearcher.class)
 
-ExpressionBuilder filterExpressionBuilder = Expressions.builder()
+ExpressionBuilder filterInactiveConceptsExpressionBuilder = Expressions.builder()
 		.filter(SnomedConceptDocument.Expressions.inactive())
 
-if (params.isUnpublishedOnly) {
-	filterExpressionBuilder.filter(SnomedConceptDocument.Expressions.effectiveTime(EffectiveTimes.UNSET_EFFECTIVE_TIME))
-}
-
-List<String> inactiveIds = Lists.newArrayList()
+Set<String> inactiveIds = Sets.newHashSet()
 
 searcher.scroll(Query.select(String.class)
 		.from(SnomedConceptDocument.class)
 		.fields(SnomedConceptDocument.Fields.ID)
-		.where(filterExpressionBuilder.build())
+		.where(filterInactiveConceptsExpressionBuilder.build())
 		.limit(10_000)
 		.build())
-		.each({id ->
-			inactiveIds.add(id[0])
+		.each({hits ->
+			hits.each({ id ->
+				inactiveIds.add(id)
+			})
+		})
+		
+ExpressionBuilder filterActiveSnomedDescriptions = Expressions.builder()
+		.filter(SnomedDescriptionIndexEntry.Expressions.concepts(inactiveIds))
+		.must(SnomedDescriptionIndexEntry.Expressions.active())
+		
+ExpressionBuilder filterInactiveSnomedDescriptions = Expressions.builder()
+		.filter(SnomedDescriptionIndexEntry.Expressions.concepts(inactiveIds))
+		.must(SnomedDescriptionIndexEntry.Expressions.inactive())
+
+		
+final Set<String> activeDescriptionIds = Sets.newHashSet()
+
+searcher.scroll(Query.select(String.class)
+		.from(SnomedDescriptionIndexEntry.class)
+		.fields(SnomedDescriptionIndexEntry.Fields.ID)
+		.where(filterActiveSnomedDescriptions.build())
+		.limit(10_000)
+		.build())
+		.each({hits ->
+			hits.each({ id ->
+				activeDescriptionIds.add(id)
+			})
+		})
+		
+final Set<String> inactiveDescriptionIds = Sets.newHashSet()
+
+searcher.scroll(Query.select(String.class)
+		.from(SnomedDescriptionIndexEntry.class)
+		.fields(SnomedDescriptionIndexEntry.Fields.ID)
+		.where(filterActiveSnomedDescriptions.build())
+		.limit(10_000)
+		.build())
+		.each({hits ->
+			hits.each({ id ->
+				inactiveDescriptionIds.add(id)
+			})
 		})
 
-Map<String, SnomedRefSetMemberIndexEntry> members = Maps.newHashMap()
+ExpressionBuilder filterActiveDescriptionsExpressionBuilder = Expressions.builder()
+		.filter(SnomedRefSetMemberIndexEntry.Expressions.referenceSetId(Concepts.REFSET_DESCRIPTION_INACTIVITY_INDICATOR))
+        .filter(SnomedRefSetMemberIndexEntry.Expressions.active())
+        .filter(SnomedRefSetMemberIndexEntry.Expressions.referencedComponentIds(activeDescriptionIds))
+		.mustNot(SnomedRefSetMemberIndexEntry.Expressions.valueIds(ImmutableList.of(Concepts.PENDING_MOVE, Concepts.LIMITED, Concepts.CONCEPT_NON_CURRENT)))
+
+
+ExpressionBuilder filterInactiveDescriptionsExpressionBuilder = Expressions.builder()
+		.filter(SnomedRefSetMemberIndexEntry.Expressions.referenceSetId(Concepts.REFSET_DESCRIPTION_INACTIVITY_INDICATOR))
+        .filter(SnomedRefSetMemberIndexEntry.Expressions.active())
+        .filter(SnomedRefSetMemberIndexEntry.Expressions.referencedComponentIds(inactiveDescriptionIds))
+		.filter(SnomedRefSetMemberIndexEntry.Expressions.valueIds(ImmutableList.of(Concepts.PENDING_MOVE, Concepts.LIMITED, Concepts.CONCEPT_NON_CURRENT)))
+
+if (params.isUnpublishedOnly) {
+	filterActiveDescriptionsExpressionBuilder.filter(SnomedRefSetMemberIndexEntry.Expressions.effectiveTime(EffectiveTimes.UNSET_EFFECTIVE_TIME))
+	filterInactiveDescriptionsExpressionBuilder.filter(SnomedRefSetMemberIndexEntry.Expressions.effectiveTime(EffectiveTimes.UNSET_EFFECTIVE_TIME))
+}
 
 searcher.scroll(Query.select(SnomedRefSetMemberIndexEntry.class)
 		.from(SnomedRefSetMemberIndexEntry.class)
-		.where(SnomedRefSetMemberIndexEntry.Expressions.referenceSetId(Concepts.REFSET_DESCRIPTION_INACTIVITY_INDICATOR))
+		.where(filterActiveDescriptionsExpressionBuilder.build())
 		.limit(10_000)
 		.build())
 		.each({hits ->
 			hits.each({ hit ->
-				members.put(hit.getReferencedComponentId(), hit)
+				issues.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.DESCRIPTION_NUMBER, hit.getReferencedComponentId()))
 			})
 		})
 
-final SnomedDescriptions descriptions = SnomedRequests.prepareSearchDescription()
-		.all()
-		.filterByConceptId(inactiveIds)
-		.build()
-		.execute(ctx)
-
-descriptions.forEach({SnomedDescription description ->
-	if (description.isActive()) {
-		def inactivationIndicatorId = members.get(description.getId()).getProperties().get(SnomedRf2Headers.FIELD_VALUE_ID)
-		if (!(Concepts.PENDING_MOVE.equals(inactivationIndicatorId) || Concepts.LIMITED.equals(inactivationIndicatorId) ||
-		Concepts.CONCEPT_NON_CURRENT.equals(inactivationIndicatorId))) {
-			issues.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.DESCRIPTION_NUMBER, description.getId()))
-		}
-	}
-
-})
+searcher.scroll(Query.select(SnomedRefSetMemberIndexEntry.class)
+		.from(SnomedRefSetMemberIndexEntry.class)
+		.where(filterInactiveDescriptionsExpressionBuilder.build())
+		.limit(10_000)
+		.build())
+		.each({hits ->
+			hits.each({ hit ->
+				issues.add(ComponentIdentifier.of(SnomedTerminologyComponentConstants.DESCRIPTION_NUMBER, hit.getReferencedComponentId()))
+			})
+		})
 
 return issues.toList()
