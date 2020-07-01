ecc778652008d98ab24c774c0fbc92ed96943e8b
András Péteri <apeteri@b2international.com>
2020-07-01 14:18:17 +0200

    [core] Use single boolean property in TerminologyComponent annotation... (#601)
    ...to determine whether a component is allowed to participate in a
    SNOMED CT map type reference member as the map target.

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/domain/refset/SnomedReferenceSetMember.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/domain/refset/SnomedReferenceSetMember.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2019 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2020 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,7 +74,8 @@ import com.google.common.collect.ImmutableSet;
 	shortId = SnomedTerminologyComponentConstants.REFSET_MEMBER_NUMBER,
 	name = "SNOMED CT Reference Set Member",
 	componentCategory = ComponentCategory.SET_MEMBER,
-	docType = SnomedRefSetMemberIndexEntry.class
+	docType = SnomedRefSetMemberIndexEntry.class,
+	allowedAsMapTarget = false
 )
 public final class SnomedReferenceSetMember extends SnomedComponent {
 
