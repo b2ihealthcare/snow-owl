a3406f35fc3668e661d1ac5a42264cfd27fb0132
Mark Czotter <mczotter@b2international.com>
2020-10-11 13:30:14 +0200

    [index] organize imports

--- commons/com.b2international.index/src/com/b2international/index/Activator.java
+++ commons/com.b2international.index/src/com/b2international/index/Activator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2018 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2020 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.concurrent.Callable;
-import java.util.stream.StreamSupport;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.lucene.codecs.Codec;
