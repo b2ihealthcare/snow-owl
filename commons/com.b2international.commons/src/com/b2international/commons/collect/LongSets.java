458964621a5da076d491cfde1df38d583508d606
Mark Czotter <mczotter@b2international.com>
2020-10-13 18:15:59 +0200

    [build] remove even more unused code fragments, statement from commons

--- commons/com.b2international.commons/src/com/b2international/commons/collect/LongSets.java
+++ commons/com.b2international.commons/src/com/b2international/commons/collect/LongSets.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2020 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,15 +16,11 @@
 package com.b2international.commons.collect;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
-import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Sets.newHashSet;
-import static com.google.common.collect.Sets.newHashSetWithExpectedSize;
 
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
 import com.b2international.collections.PrimitiveSets;
@@ -34,10 +30,8 @@ import com.b2international.collections.longs.LongCollections;
 import com.b2international.collections.longs.LongIterator;
 import com.b2international.collections.longs.LongSet;
 import com.b2international.commons.CompareUtils;
-import com.google.common.base.Function;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import com.google.common.primitives.Longs;
+import com.google.common.collect.Sets;
 
 /**
  * Utility class for {@link LongSet}s.
@@ -111,32 +105,6 @@ public class LongSets {
 	}
 	
 	/**
-	 * Returns with a list containing all long values.
-	 * @param collection the collection of primitive longs
-	 * @return a list of long values.
-	 */
-	public static List<Long> toList(final LongCollection collection) {
-		checkNotNull(collection, "Long collection argument cannot be null.");
-		collection.trimToSize();
-		return Longs.asList(collection.toArray());
-	}
-	
-	/**
-	 * Returns with a set containing all long values.
-	 * @param collection the collection of primitive longs
-	 * @return a set of long values.
-	 */
-	public static Set<Long> toSet(final LongCollection collection) {
-		checkNotNull(collection, "Long collection argument cannot be null.");
-		collection.trimToSize();
-		final Set<Long> $ = newHashSetWithExpectedSize(collection.size());
-		for (final LongIterator itr = collection.iterator(); itr.hasNext(); /* */) {
-			$.add(itr.next());
-		}
-		return $;
-	}
-	
-	/**
 	 * Returns with an {@link List} containing all long values as a string.
 	 * @param collection the collection of primitive longs.
 	 * @return an list of strings.
@@ -156,46 +124,6 @@ public class LongSets {
 		return newHashSet(toStringArray(collection));
 	}
 
-	/**
-	 * Creates and returns with a new mutable {@link LongOpenHashSet} instance containing the 
-	 * elements in indeterministic order.  
-	 * @param itr the elements that the new set instance should contain.
-	 * @return the new set instance containing each element of the iterator without duplicates. 
-	 */
-	public static LongSet newLongSet(final LongIterator itr) {
-		
-		final LongSet $ = PrimitiveSets.newLongOpenHashSet();
-		while (itr.hasNext()) {
-			
-			$.add(itr.next());
-			
-		}
-		
-		return $;
-		
-	}
-	
-	/**
-	 * Evaluates the procedure argument on each element of the given iterator.
-	 * @param itr the iterator which elements will be evaluated against the procedure.
-	 * @param procedure the procedure to apply on each element of the given iterator of primitive long numbers.
-	 */
-	public static void forEach(final LongIterator itr, final LongCollectionProcedure procedure) {
-		checkNotNull(procedure, "procedure");
-		while (itr.hasNext()) {
-			procedure.apply(itr.next());
-		}
-	}
-	
-	/**
-	 * Evaluates the procedure argument on each element of the given collection.
-	 * @param collection the collection which each element will be evaluated against the procedure.
-	 * @param procedure the procedure to apply on each element of the given collection of primitive long elements.
-	 */
-	public static void forEach(final LongCollection collection, final LongCollectionProcedure procedure) {
-		forEach(collection.iterator(), procedure);
-	}
-	
 	/**
 	 * Returns with an iterator instance that applies the {@code function} to each element of the
 	 * given {@code from} iterable. 
@@ -239,7 +167,7 @@ public class LongSets {
 		checkNotNull(function, "function");
 		
 		@SuppressWarnings("unchecked")
-		final Collection<T> toCollection = (Collection<T>) (fromCollection instanceof LongSet ? newHashSet() : newArrayList());
+		final Collection<T> toCollection = (Collection<T>) (fromCollection instanceof LongSet ? Sets.newHashSetWithExpectedSize(fromCollection.size()) : Lists.newArrayListWithExpectedSize(fromCollection.size()));
 		
 		for (final LongIterator itr = fromCollection.iterator(); itr.hasNext();  /**/) {
 			toCollection.add(function.apply(itr.next()));
@@ -247,103 +175,8 @@ public class LongSets {
 
 		return toCollection;
 	}
-	
-	/**
-	 * Combines the {@link LongIterator iterator} of the given collections into a single one.
-	 * @param collection the first collection.
-	 * @param others the other ones.
-	 * @return the combined iterator.
-	 */
-	public static LongIterator iterator(final LongCollection collection, final LongCollection... others) {
-		return concat(Iterables.transform(Lists.asList(checkNotNull(collection, "collection"), others), new Function<LongCollection, LongIterator>() {
-			@Override public LongIterator apply(final LongCollection longCollection) {
-				return longCollection.iterator();
-			}
-		}).iterator());
-	}
-	
-	/**
-	 * Concatenates the given long iterators into a single iterators.
-	 * @param itr the first iterator.
-	 * @param others the other ones.
-	 * @return the concatenated iterators.
-	 */
-	public static LongIterator concat(final LongIterator itr, final LongIterator... others) {
-		return concat(Lists.asList(checkNotNull(itr, "itr"), others).iterator());
-	}
-	
-	/**
-	 * Concatenates the given {@link LongIterator iterators} into a single iterators.
-	 * @param iterators the iterators to combine.
-	 * @return the concatenated iterators.
-	 */
-	public static LongIterator concat(final Iterator<LongIterator> iterators) {
-		
-		return new LongIterator() {
-		
-			private LongIterator current = LongCollections.emptyIterator();
-			private LongIterator removeFrom;
-			
-			@Override
-			public void remove() {
-				checkState(removeFrom != null);
-				removeFrom.remove();
-				removeFrom = null;
-			}
-			
-			@Override
-			public long next() {
-		        if (!hasNext()) {
-		            throw new NoSuchElementException();
-		          }
-		          removeFrom = current;
-		          return current.next();
-			}
-			
-			@Override
-			public boolean hasNext() {
-				boolean currentHasNext;
-				while (!(currentHasNext = checkNotNull(current).hasNext()) && iterators.hasNext()) {
-					current = iterators.next();
-				}
-				return currentHasNext;
-			}
-		};
-		
-	}
-	
-	/**
-	 * Returns the elements of {@code unfiltered} that satisfy the given {@link LongPredicate predicate}. 
-	 * @param unfiltered the set to filter.
-	 * @param predicate the predicate.
-	 * @return a modifiable filtered collection based on the {@code unfiltered} one.
-	 */
-	public static LongSet filter(final LongSet unfiltered, final LongPredicate predicate) {
-		checkNotNull(unfiltered, "unfiltered");
-		checkNotNull(predicate, "predicate");
-		final LongSet copy = PrimitiveSets.newLongOpenHashSet(unfiltered);
-		for (final LongIterator itr = copy.iterator(); itr.hasNext(); /* */) {
-			final long value = itr.next();
-			if (!predicate.apply(value)) {
-				itr.remove();
-			}
-		} 
-		return copy;
-	}
 
 	private LongSets() { /*suppress instantiation*/
-	}
-
-	/**
-	 * Procedure acting as a {@link LongFunction function} and providing no return value.
-	 */
-	public static interface LongCollectionProcedure {
-		
-		/**
-		 * Applies the current function to the primitive long {@code input}.
-		 * @param input the input primitive long to apply on the current procedure.
-		 */
-		void apply(final long input);
 	}
 	
 	/**
@@ -380,102 +213,6 @@ public class LongSets {
 		 */
 		T apply(final long input);
 		
-	}
-	
-	/**
-	 * Determines a {@code true} or {@code false} value for a given primitive long input.
-	 */
-	public static interface LongPredicate {
-
-		/**
-		 * Returns the result of applying this predicate to {@code input} primitive long.
-		 */
-		boolean apply(final long input);
-
-		/**Predicate always producing {@code true} result.*/
-		public static final LongPredicate ALL_PREDICATE = new LongPredicate() {
-			@Override public boolean apply(final long input) {
-				return true;
-			}
-		};
-		
-	}
-	
-	/**
-	 * Predicate that evaluates to {@code true} if the primitive long being tested is a member of 
-	 * any of the underlying collections.
-	 * 
-	 */
-	/*default*/ static final class InLongPredicate implements LongPredicate {
-		
-		private final Iterable<? extends LongCollection> collections;
-
-		/*default*/ InLongPredicate(final Iterable<? extends LongCollection> collections) {
-			this.collections = checkNotNull(collections, "collections");
-		}
-
-		@Override
-		public boolean apply(final long input) {
-			for (final LongCollection collection : collections) {
-				if (collection.contains(input)) {
-					return true;
-				}
-			}				
-			
-			return false;
-		}
-		
-	}
-	
-	/**
-	 * Predicates that negates evaluation of the delegate predicate.
-	 *
-	 */
-	/*default*/ static final class NotLongPredicate implements LongPredicate {
-
-		private final LongPredicate predicate;
-
-		/*default*/ NotLongPredicate(final LongPredicate predicate) {
-			this.predicate = checkNotNull(predicate, "predicate");
-		}
-		
-		@Override
-		public boolean apply(final long input) {
-			return !predicate.apply(input);
-		}
-		
-		
-	}
-
-	/**
-	 * Creates and returns with a new {@link LongPredicate} instance that evaluates to {@code true} if the primitive
-	 * long being tested is a member of any of the underlying long collections.
-	 * @param collections the collections to check against the membership.
-	 * @return the predicate.
-	 */
-	public static LongPredicate in(final Iterable<? extends LongCollection> collections) {
-		return new InLongPredicate(checkNotNull(collections, "collections"));
-	}
-
-	
-	/**
-	 * Creates and returns with a new {@link LongPredicate} instance that evaluates to {@code true} if the primitive
-	 * long being tested is a member of any of the underlying long collections.
-	 * @param collection the collection.
-	 * @param others the others.
-	 * @return the predicate.
-	 */
-	public static LongPredicate in(final LongCollection collection, final LongCollection... others) {
-		return new InLongPredicate(Lists.asList(checkNotNull(collection, "collection"), checkNotNull(others, "others")));
-	}
-	
-	/**
-	 * Creates a new predicate instance that always negates the argument one.
-	 * @param predicate the predicate to negate.
-	 * @return a new NOT predicate.
-	 */
-	public static LongPredicate not(final LongPredicate predicate) {
-		return new NotLongPredicate(checkNotNull(predicate, "predicate"));
 	}
 	
 }
