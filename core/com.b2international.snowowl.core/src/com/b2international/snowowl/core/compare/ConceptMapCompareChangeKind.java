a6ae9d2b4666d73efef38f38ce5505c1e67e3faa
Laura Molnar <lmolnar@b2international.com>
2020-10-22 13:55:19 +0200

    SO-4236: simplify enum

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/compare/ConceptMapCompareChangeKind.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/compare/ConceptMapCompareChangeKind.java
@@ -21,38 +21,27 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * @since 7.10
  */
 public enum ConceptMapCompareChangeKind {
-	PRESENT("Present in Base"),
-	NOT_PRESENT("Not Present in Base"),
-	
-	DIFFERENT_TARGET_PRESENT("Different Target - Present in Base"),
-	DIFFERENT_TARGET_NOT_PRESENT("Different Target - Not Present in Base"),
-	
-	DIFFERENT_STATUS_PRESENT("Different Status - Present in Base"),
-	DIFFERENT_STATUS_NOT_PRESENT("Different Status - Not Present in Base"),
-	
-	DIFFERENT_MAP_ADVICE_PRESENT("Different Map Advice - Present in Base"),
-	DIFFERENT_MAP_ADVICE_NOT_PRESENT("Different Map Advice - Not Present in Base"),
-	
-	DIFFERENT_MAP_GROUP_PRESENT("Different Map Group - Present in Base"),
-	DIFFERENT_MAP_GROUP_NOT_PRESENT("Different Map Group - Not Present in Base"),
-	
-	DIFFERENT_MAPPING_CORRELATION_PRESENT("Different Mapping Correlation - Present in Base"),
-	DIFFERENT_MAPPING_CORRELATION_NOT_PRESENT("Different Mapping Correlation - Not Present in Base"),
-	
-	DIFFERENT_MAP_PRIORITY_PRESENT("Different Map Priority - Present in Base"),
-	DIFFERENT_MAP_PRIORITY_NOT_PRESENT("Different Map Priority - Not Present in Base"),
-	
-	DIFFERENT_MAP_RULE_PRESENT("Different Map Rule - Present in Base"),
-	DIFFERENT_MAP_RULE_NOT_PRESENT("Different Map Rule - Not Present in Base"),
-	
+	PRESENT("Present"),
+	NOT_PRESENT("Not Present"),
+
+	DIFFERENT_TARGET("Different Target"),
+
+	DIFFERENT_STATUS("Different Status"),
+
+	DIFFERENT_MAP_ADVICE("Different Map Advice"),
+	DIFFERENT_MAP_GROUP("Different Map Group"),
+	DIFFERENT_MAPPING_CORRELATION("Different Mapping Correlation"),
+	DIFFERENT_MAP_PRIORITY("Different Map Priority"),
+	DIFFERENT_MAP_RULE("Different Map Rule"),
+
 	UNCHANGED("Unchanged");
-	
+
 	private final String label;
-	
+
 	private ConceptMapCompareChangeKind(final String label) {
 		this.label = checkNotNull(label, "label");
 	}
-	
+
 	@Override
 	public String toString() {
 		return label;
