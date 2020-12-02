2312fe6486dba9eff5fb02ac01fefb5a5d33d737
Kovacs Tamas <tkovacs@b2international.com>
2020-12-02 14:41:35 +0100

    SO-4525: Add DSV export request

---
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/ConceptMapCompareDsvExportRequest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018-2020 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.core.request;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import com.b2international.snowowl.core.ServiceProvider;
+import com.b2international.snowowl.core.compare.ConceptMapCompareChangeKind;
+import com.b2international.snowowl.core.compare.ConceptMapCompareDsvExporter;
+import com.b2international.snowowl.core.compare.ConceptMapCompareResultItem;
+
+/**
+ * @since 7.13
+ */
+final class ConceptMapCompareDsvExportRequest extends ExportRequest<ServiceProvider> {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<ConceptMapCompareResultItem> items;
+	private List<ConceptMapCompareChangeKind> changeKinds;
+	private Set<String> columns;
+	
+	ConceptMapCompareDsvExportRequest() {
+	}
+
+	@Override
+	protected File doExport(ServiceProvider context) throws IOException {
+		return ConceptMapCompareDsvExporter.export(items, changeKinds, columns);
+	}
+
+	@Override
+	public String getResource(ServiceProvider context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void setItems(List<ConceptMapCompareResultItem> items) {
+		this.items = items;
+	}
+	
+	public void setChangeKinds(List<ConceptMapCompareChangeKind> changeKinds) {
+		this.changeKinds = changeKinds;
+	}
+
+	public void setColumns(Set<String> columns) {
+		this.columns = columns;
+	}
+}
