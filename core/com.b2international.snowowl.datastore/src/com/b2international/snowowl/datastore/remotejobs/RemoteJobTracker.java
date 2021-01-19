1097e7284198eac7b30f499e541c10f0452ae1f1
András Péteri <apeteri@b2international.com>
2021-01-19 10:59:35 +0100

    [remotejobs] Set job status with INFO severity to succeeded

--- core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/remotejobs/RemoteJobTracker.java
+++ core/com.b2international.snowowl.datastore/src/com/b2international/snowowl/datastore/remotejobs/RemoteJobTracker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2017-2021 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -298,7 +298,7 @@ public final class RemoteJobTracker implements IDisposableService {
 				final RemoteJobState newState;
 				if (result == null) {
 					newState = RemoteJobState.CANCELED;
-				} else if (result.isOK()) {
+				} else if (result.isOK() || result.matches(IStatus.INFO)) {
 					newState = RemoteJobState.FINISHED;
 				} else if (result.matches(IStatus.CANCEL)) {
 					newState = RemoteJobState.CANCELED;
