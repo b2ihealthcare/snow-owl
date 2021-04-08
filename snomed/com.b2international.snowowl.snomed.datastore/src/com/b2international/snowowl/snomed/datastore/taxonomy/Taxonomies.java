2b3a674366c8b0a249e6cbcee4592e1470bdcb11
Mark Czotter <mczotter@b2international.com>
2021-04-08 18:45:50 +0200

    fix: register reactivated concepts for taxonomy updates
    This fixes an issue when an inactive concept with active OWL axiom
    and/or relationships got reactivated but it did not get the necessary
    tree updates to register it in the stated/inferred tree.
    
    SO-4755 #resolve

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/taxonomy/Taxonomies.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/taxonomy/Taxonomies.java
@@ -240,6 +240,11 @@ public final class Taxonomies {
 	}
 	
 	private static void updateEdge(SnomedRefSetMemberIndexEntry member, TaxonomyGraph graphToUpdate, SnomedOWLExpressionConverter expressionConverter) {
+		if ("c57106ed-dc45-4264-a1bf-080a9781d73e".equals(member.getId())
+				|| "684f8761-f673-ffd1-4484-e137cd0b58a3".equals(member.getId())
+				|| "448964007".equals(member.getReferencedComponentId())) {
+			System.err.println();
+		}
 		if (member.isActive()) {
 			SnomedOWLExpressionConverterResult result = expressionConverter.toSnomedOWLRelationships(member.getReferencedComponentId(), member.getOwlExpression());
 			if (!CompareUtils.isEmpty(result.getClassAxiomRelationships())) {
@@ -318,6 +323,9 @@ public final class Taxonomies {
 					.build();
 			Hits<SnomedRefSetMemberIndexEntry> activeAxiomISARelationships = searcher.search(activeAxiomISARelationshipsQuery);
 			activeAxiomISARelationships.forEach(owlMember -> {
+				if ("c57106ed-dc45-4264-a1bf-080a9781d73e".equals(owlMember.getId()) || "684f8761-f673-ffd1-4484-e137cd0b58a3".equals(owlMember.getId())) {
+					System.err.println();
+				}
 				if (!CompareUtils.isEmpty(owlMember.getClassAxiomRelationships())) {
 					long[] destinationIds = owlMember.getClassAxiomRelationships()
 						.stream()
