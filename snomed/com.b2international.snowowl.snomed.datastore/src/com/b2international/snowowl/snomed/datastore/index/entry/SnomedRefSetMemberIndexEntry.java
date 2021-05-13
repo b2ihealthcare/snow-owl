e6ab1a5f1986ffc2b8dadec3c933d475699479ec
András Péteri <apeteri@b2international.com>
2021-05-13 16:27:10 +0200

    feat(snomed): Add query expression for filtering OWL relationships...
    ...by destination ID
    
    Its only caller is rule_mrcm_constraint, which should be reviewed, as
    some of its constraints should be placed under a single nested query
    clause instead of filtering independently.

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/index/entry/SnomedRefSetMemberIndexEntry.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/index/entry/SnomedRefSetMemberIndexEntry.java
@@ -480,6 +480,13 @@ public final class SnomedRefSetMemberIndexEntry extends SnomedDocument {
 			return query.build();
 		}
 		
+		public static Expression owlExpressionWithDestinationId() {
+			final ExpressionBuilder query = com.b2international.index.query.Expressions.builder();
+			query.should(nestedMatch("classAxiomRelationships", exists("destinationId")));
+			query.should(nestedMatch("gciAxiomRelationships", exists("destinationId")));
+			return query.build();
+		}
+		
 		public static Expression owlExpressionDestination(Iterable<String> destinationIds) {
 			final ExpressionBuilder query = com.b2international.index.query.Expressions.builder();
 			query.should(nestedMatch("classAxiomRelationships", matchAny("destinationId", destinationIds)));
