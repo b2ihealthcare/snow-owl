09a2afa86412879a5818e199e98f8daa85f1e2d5
Mark Czotter <mczotter@b2international.com>
2021-07-09 11:57:32 +0200

    fix: use select component to change API doc

--- core/com.b2international.snowowl.core.rest/snow-owl-api-docs/src/App.js
+++ core/com.b2international.snowowl.core.rest/snow-owl-api-docs/src/App.js
@@ -20,12 +20,12 @@ class App extends React.Component {
 
   onMenuSelect = (e) => {
     this.setState({
-      selectedKey: e.key
+      selectedKey: e.target.value
     })
   }
 
   render() {
-    const { apis, serverUrl } = this.state
+    const { apis, serverUrl, selectedKey } = this.state
     return (
       <>
         <BackTop />
@@ -33,19 +33,40 @@ class App extends React.Component {
           theme = "light"
           spec-url = {`${serverUrl}/api-docs/${this.state.selectedKey}`}
           server-url = {`${serverUrl}`}
-          show-header="false"
           render-style = "focused"
           default-schema-tab = "example"
           layout = "row"
           schema-expand-level = "3"
           style = {{ height: "100vh", width: "100%" }}
+          show-header="false"
           allow-spec-url-load="false"
           allow-spec-file-load="false"
           allow-server-selection="false"
         >
-          <div slot="nav-logo" style={{ display: "flex", alignItems: "center", justifyContent: "center", fontSize: "24px" }}> 
-            <img src = "./assets/favicon.svg" style={{ width: "40px", marginRight: "8px" }} alt="logo" />
-            <span style={{ color: "#fff" }}> Snow Owl API </span>
+          <div slot="nav-logo">
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", fontSize: "24px" }}> 
+              <img src = "./assets/favicon.svg" style={{ width: "40px", marginRight: "8px" }} alt="logo" />
+              <span style={{ color: "#fff" }}> Snow Owl API Docs </span>
+            </div>
+            <div style={{
+              display: "flex", 
+              flexDirection: "row",
+              alignItems: "center", 
+              justifyContent: "center", 
+              paddingLeft: "8px"
+            }}>
+              <select style={{
+                width: "100%",
+                marginTop: "16px",
+                color: "var(--nav-hover-text-color)",
+                borderColor: "var(--nav-accent-color)",
+                backgroundColor: "var(--nav-hover-bg-color)"
+              }} name="apis" onChange={(e) => this.onMenuSelect(e)} value={selectedKey}>
+                {apis.map(api => 
+                  <option key={api.id} value={`${api.id}`}>{api.title}</option>
+                )}
+              </select>
+            </div>
           </div>
         </rapi-doc>
       </>
