c2821b547df1bfe6bcb93d30b68cf92f75578b51
Henrik Élő <helo@b2international.com>
2021-07-13 18:08:17 +0200

    refactor(CodeSystem): remove unused import

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/codesystem/CodeSystem.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/codesystem/CodeSystem.java
@@ -20,7 +20,6 @@ import java.util.List;
 import com.b2international.commons.http.ExtendedLocale;
 import com.b2international.snowowl.core.ResourceURI;
 import com.b2international.snowowl.core.TerminologyResource;
-import com.b2international.snowowl.core.branch.Branch;
 import com.b2international.snowowl.core.internal.ResourceDocument;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
