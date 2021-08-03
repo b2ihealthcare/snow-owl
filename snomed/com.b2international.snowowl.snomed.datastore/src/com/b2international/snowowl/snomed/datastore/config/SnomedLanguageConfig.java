5f28fac642f7cca67395900705554d400c5dcf15
Laura Molnar <lmolnar@b2international.com>
2021-08-03 14:41:29 +0200

    fix(languages): rename arguments

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/config/SnomedLanguageConfig.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/config/SnomedLanguageConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2019-2021 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,8 +30,8 @@ public class SnomedLanguageConfig {
 	private String languageTag;
 	private List<String> languageRefSetIds;
 	
-	public SnomedLanguageConfig(String code, String...refSetIds) {
-		this(code, ImmutableList.copyOf(refSetIds));
+	public SnomedLanguageConfig(String languageTag, String...languageRefSetIds) {
+		this(languageTag, ImmutableList.copyOf(languageRefSetIds));
 	}
 	
 	@JsonCreator
