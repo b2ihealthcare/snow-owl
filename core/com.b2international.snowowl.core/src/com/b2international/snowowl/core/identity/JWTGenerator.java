881317990f998db9c8f9d6bd93c864cfc50f213c
Mark Czotter <mczotter@b2international.com>
2021-09-07 14:27:05 +0200

    fix(core): return HTTP 400 if external JWT does not have `sub` claim

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/identity/JWTGenerator.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/identity/JWTGenerator.java
@@ -25,6 +25,8 @@ import com.auth0.jwt.JWTCreator.Builder;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.b2international.commons.exceptions.BadRequestException;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 
@@ -90,6 +92,9 @@ public final class JWTGenerator {
 	 */
 	public static User toUser(DecodedJWT jwt) {
 		final String subject = jwt.getSubject();
+		if (Strings.isNullOrEmpty(subject)) {
+			throw new BadRequestException("'subject' JWT token field is required, but it was missing");
+		}
 		final List<Role> roles = jwt.getClaims().entrySet().stream()
 				.filter(entry -> entry.getKey().startsWith(ROLE_PREFIX))
 				.map(entry -> {
