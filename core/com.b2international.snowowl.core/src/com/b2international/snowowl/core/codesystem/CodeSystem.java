dafa64dec9b334a262b8ba251909ec747c94a195
aalaswadi <aalaswadi@b2international.com>
2021-10-07 11:04:48 +0200

    SO-4899: Remove constants already defined in terminology component...
    ... constants

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/codesystem/CodeSystem.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/codesystem/CodeSystem.java
@@ -35,10 +35,6 @@ public final class CodeSystem extends TerminologyResource {
 	
 	public static final String RESOURCE_TYPE = "codesystems";
 	
-	//CodeSystem settings
-	public static final String DEFAULT_REFSET_MODULE_ID = "defaultRefsetModuleId";
-	public static final String DEFAULT_QUERY_TYPE_REFSET_ID = "defaultQueryTypeRefsetId";
-	
 	/**
 	 * @since 8.0
 	 */
