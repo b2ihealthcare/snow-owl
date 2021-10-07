856414ab1bc0f3eb9c0704e5406ac8cc71d06f21
Mark Czotter <mczotter@b2international.com>
2021-10-07 11:01:26 +0200

    feat(cli): pimp revision-cli tool with styling, indentation and extra...
    ...data

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/cli/SnomedCommand.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/cli/SnomedCommand.java
@@ -15,15 +15,20 @@
  */
 package com.b2international.snowowl.snomed.core.cli;
 
+import java.io.PrintStream;
 import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.ZoneOffset;
+import java.util.Iterator;
 
 import com.b2international.commons.exceptions.NotFoundException;
+import com.b2international.index.Hits;
 import com.b2international.index.query.Query;
+import com.b2international.index.revision.Commit;
 import com.b2international.index.revision.RevisionBranch;
 import com.b2international.index.revision.RevisionBranchPoint;
 import com.b2international.index.revision.RevisionIndex;
+import com.b2international.index.util.JsonDiff;
 import com.b2international.snowowl.core.RepositoryManager;
 import com.b2international.snowowl.core.attachments.Attachment;
 import com.b2international.snowowl.core.console.Command;
@@ -39,6 +44,7 @@ import com.b2international.snowowl.snomed.datastore.request.SnomedRequests;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.primitives.Longs;
 
+import picocli.CommandLine;
 import picocli.CommandLine.HelpCommand;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
@@ -61,7 +67,7 @@ public final class SnomedCommand extends Command {
 
 	@Override
 	public void run(CommandLineStream out) {
-//		CommandLine.usage(this, (PrintStream) out);
+		CommandLine.usage(this, (PrintStream) out);
 	}
 
 	@picocli.CommandLine.Command(
@@ -119,61 +125,89 @@ public final class SnomedCommand extends Command {
 	}
 	
 	@picocli.CommandLine.Command(
-		name = "revision",
-		header = "Prints revision information about a SNOMED CT concept",
-		description = "Prints revision information about a SNOMED CT concept"
-	)
-	private static final class RevisionCheckCommand extends Command {
-		
-		@Parameters(index = "0", paramLabel = "BRANCH", description = "The branch to filter the revisions")
-		String branch;
-		
-		@Parameters(index = "1", paramLabel = "CONCEPT_ID", description = "The concept id to show revisions for")
-		String conceptId;
-		
-		@Override
-		public void run(CommandLineStream out) {
-			RevisionIndex index = getContext().service(RepositoryManager.class).get("snomedStore").service(RevisionIndex.class);
-			index.read(branch, searcher -> {
-				System.err.println("Fetching revisions...");
-				searcher.search(Query.select(JsonNode.class)
-						.from(SnomedConceptDocument.class)
-						.where(RevisionDocument.Expressions.id(conceptId))
-						.limit(Integer.MAX_VALUE)
-						.build())
-						.forEach(System.err::println);
-				
-//				searcher.search(Query.select(JsonNode.class)
-//						.from(Commit.class)
-//						.where(Commit.Expressions.id("d7bb66bf-4d5f-463e-a5e2-007bb2af36d3"))
-//						.build())
-//						.forEach(System.err::println);
-				
-				searcher.searcher().search(Query.select(JsonNode.class)
-						.from(SnomedConceptDocument.class)
-						.where(RevisionDocument.Expressions.id(conceptId))
-						.limit(Integer.MAX_VALUE)
-						.build())
-						.stream()
-						.sorted((j1, j2) -> {
-							var j1Created = RevisionBranchPoint.valueOf(j1.get("created").asText());
-							var j2Created = RevisionBranchPoint.valueOf(j2.get("created").asText());
-							return Longs.compare(j1Created.getTimestamp(), j2Created.getTimestamp());
-						})
-						.forEach(c -> {
-							var branchPoint = RevisionBranchPoint.valueOf(c.get("created").asText());
-							try {
-								RevisionBranch branch = index.branching().getBranch(branchPoint.getBranchId());
-								System.err.println(Instant.ofEpochMilli(branchPoint.getTimestamp()).atOffset(ZoneOffset.UTC).toLocalDateTime() + " - " + branch.getPath() + " - " + c);
-							} catch (NotFoundException e) {
-								// unknown branch, probably something that was a test or had to be removed, ignore revision
-							}
-						});
-				
-				System.err.println("Done");
-				return null;
-			});			
+			name = "revision",
+			header = "Prints revision information about a SNOMED CT concept",
+			description = "Prints revision information about a SNOMED CT concept"
+		)
+		private static final class RevisionCheckCommand extends Command {
+			
+			@Parameters(index = "0", paramLabel = "BRANCH", description = "The branch to filter the revisions")
+			String branch;
+			
+			@Parameters(index = "1", paramLabel = "CONCEPT_ID", description = "The concept id to show revisions for")
+			String conceptId;
+			
+			@Option(names = {"-p", "--pretty"}, description = "To make the output pretty print all JSON objects")
+			boolean pretty = false;
+			
+			@Override
+			public void run(CommandLineStream out) {
+				RevisionIndex index = getContext().service(RepositoryManager.class).get("snomedStore").service(RevisionIndex.class);
+				index.read(branch, searcher -> {
+					out.println("Fetching revisions visible from branch...");
+
+					Hits<JsonNode> hits = searcher.search(Query.select(JsonNode.class)
+							.from(SnomedConceptDocument.class)
+							.where(RevisionDocument.Expressions.id(conceptId))
+							.limit(Integer.MAX_VALUE)
+							.build());
+					
+					hits.forEach(rev -> {
+						out.println(pretty ? rev.toPrettyString() : rev.toString());
+					});
+					
+					if (hits.getTotal() > 1) {
+						out.println("Found more than revision for the diagnosed document, fetching all revisions in chronological order:");
+						
+						if (hits.getTotal() == 2) {
+							out.println("Performing a diff between the two revisions:");
+							Iterator<JsonNode> it = hits.iterator();
+							JsonDiff.diff(it.next(), it.next()).forEach(change -> {
+								out.println("\t%s", change);
+							});
+						} else {
+							out.println("More than two revisions found for the same ID, skipping diff generation.");
+						}
+						
+						searcher.searcher().search(Query.select(JsonNode.class)
+									.from(SnomedConceptDocument.class)
+									.where(RevisionDocument.Expressions.id(conceptId))
+									.limit(Integer.MAX_VALUE)
+									.build())
+							.stream()
+							.sorted((j1, j2) -> {
+								var j1Created = RevisionBranchPoint.valueOf(j1.get("created").asText());
+								var j2Created = RevisionBranchPoint.valueOf(j2.get("created").asText());
+								return Longs.compare(j1Created.getTimestamp(), j2Created.getTimestamp());
+							})
+							.forEach(c -> {
+								var branchPoint = RevisionBranchPoint.valueOf(c.get("created").asText());
+								try {
+									RevisionBranch branch = index.branching().getBranch(branchPoint.getBranchId());
+									out.println("%s - %s - %s", Instant.ofEpochMilli(branchPoint.getTimestamp()).atOffset(ZoneOffset.UTC).toLocalDateTime(), branch.getPath(), c);
+									// fetch commit detail for each revision to see what's registered in the commit object for compare and merge processes
+									searcher.search(Query.select(Commit.class)
+											.where(Commit.Expressions.timestamp(branchPoint.getTimestamp()))
+											.limit(1)
+											.build())
+											.stream()
+											.findFirst()
+											.ifPresent(commit -> {
+												out.println("\t-> %s pushed commit '%s' with details '%s'", commit.getAuthor(), commit.getComment(), commit.getDetailsByObject(conceptId));
+											});
+								} catch (Exception e) {
+									if (e instanceof NotFoundException) {
+										// unknown branch, probably something that was a test or had to be removed, ignore revision
+									} else {
+										// print error otherwise
+										out.print(e);
+									}
+								}
+							});
+					}
+					
+					return null;
+				});			
+			}
 		}
-		
-	}
 }
