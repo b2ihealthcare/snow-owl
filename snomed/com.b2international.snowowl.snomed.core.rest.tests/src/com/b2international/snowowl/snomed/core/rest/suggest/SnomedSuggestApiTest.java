d560000927ef3b4ff78cb002a22dcd7bc8872a7b
Tamás Kovács <tkovacs@b2international.com>
2021-10-18 17:12:21 +0200

    SO-4383: verify concept suggestion functionalities

--- snomed/com.b2international.snowowl.snomed.core.rest.tests/src/com/b2international/snowowl/snomed/core/rest/suggest/SnomedSuggestApiTest.java
+++ snomed/com.b2international.snowowl.snomed.core.rest.tests/src/com/b2international/snowowl/snomed/core/rest/suggest/SnomedSuggestApiTest.java
@@ -18,7 +18,7 @@ package com.b2international.snowowl.snomed.core.rest.suggest;
 import static com.b2international.snowowl.test.commons.ApiTestConstants.SUGGEST_API;
 import static com.b2international.snowowl.test.commons.rest.RestExtensions.JSON_UTF8;
 import static com.b2international.snowowl.test.commons.rest.RestExtensions.givenAuthenticatedRequest;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
 
 import java.util.List;
@@ -26,6 +26,7 @@ import java.util.List;
 import org.junit.Test;
 
 import com.b2international.commons.json.Json;
+import com.b2international.snowowl.snomed.common.SnomedConstants.Concepts;
 
 import io.restassured.response.ValidatableResponse;
 
@@ -38,11 +39,17 @@ public class SnomedSuggestApiTest {
 	
 	private static final String BODY_STRUCTURE_ID = "123037004";
 	private static final String CLINICAL_FINDING_ID = "404684003";
+	private static final String SPECIAL_CONCEPT_ID = "370115009";
+	private static final String ATTRIBUTE_ID = Concepts.ATTRIBUTE;
+	private static final String CONCEPT_HISTROY_ATTRIBUTE_ID = "410663007";
+	private static final String CONCEPT_MODEL_ATTRIBUTE_ID = "410662002";
+	private static final String CONCEPT_ATTRIBUTE_ID = "734866006";
 	
 	@Test
 	public void getSuggest() {
 		getSuggest(prepareJson().with("term", "body structure"))
 			.statusCode(200)
+			.assertThat()
 			//Use default limit
 			.body("limit", equalTo(1))
 			.body("total", greaterThanOrEqualTo(1))
@@ -53,6 +60,7 @@ public class SnomedSuggestApiTest {
 	public void postSuggest() {
 		postSuggest(prepareJson().with("term", "finding clinical"))
 			.statusCode(200)
+			.assertThat()
 			//Use default limit
 			.body("limit", equalTo(1))
 			.body("total", greaterThanOrEqualTo(1))
@@ -60,7 +68,56 @@ public class SnomedSuggestApiTest {
 	}
 	
 	@Test
-	public void postBulkSuggest() {
+	public void emptyResultSuggest() {
+		getSuggest(prepareJson().with("term", "empty result term"))
+			.statusCode(200)
+			.assertThat()
+			.body("limit", equalTo(1))
+			.body("total", equalTo(0))
+			.body("items.size()", equalTo(0));
+	}
+	
+	@Test
+	public void filterByTermMinOccurrence() {
+		getSuggest(prepareJson()
+				.with("term", "special concept")
+				.with("minOccurrenceCount", 1)
+				.with("limit", Integer.MAX_VALUE))
+		.statusCode(200)
+		.assertThat()
+		.body("total", greaterThanOrEqualTo(2))
+		.body("items.id", allOf(
+				hasItem(SPECIAL_CONCEPT_ID),
+				hasItem(Concepts.ROOT_CONCEPT)));
+	}
+	
+	@Test
+	public void filterByQueryWithMinOccurrence() {
+		getSuggest(prepareJson()
+				.with("query", String.join(" OR ", SPECIAL_CONCEPT_ID, ATTRIBUTE_ID))
+				.with("minOccurrenceCount", 2)
+				.with("limit", Integer.MAX_VALUE))
+		.statusCode(200)
+		.assertThat()
+		.body("items.id", hasItems(CONCEPT_HISTROY_ATTRIBUTE_ID, CONCEPT_MODEL_ATTRIBUTE_ID, CONCEPT_ATTRIBUTE_ID))
+		.body("items.id", not(hasItems(SPECIAL_CONCEPT_ID, ATTRIBUTE_ID)));
+	}
+	
+	@Test
+	public void filterByMustNotQueryWithMinOccurrence() {
+		getSuggest(prepareJson()
+				.with("query", String.join(" OR ", SPECIAL_CONCEPT_ID, ATTRIBUTE_ID))
+				.with("mustNotQuery", String.join("<<", Concepts.FOUNDATION_METADATA_CONCEPTS))
+				.with("minOccurrenceCount", 2)
+				.with("limit", Integer.MAX_VALUE))
+		.statusCode(200)
+		.assertThat()
+		.body("items.id", hasItems(CONCEPT_HISTROY_ATTRIBUTE_ID, CONCEPT_MODEL_ATTRIBUTE_ID))
+		.body("items.id", not(hasItems(SPECIAL_CONCEPT_ID, ATTRIBUTE_ID, CONCEPT_ATTRIBUTE_ID)));
+	}
+	
+	@Test
+	public void bulkSuggest() {
 		
 		final List<Json> body = List.of(
 			prepareJson()
@@ -78,11 +135,13 @@ public class SnomedSuggestApiTest {
 			.body("size()", equalTo(2))
 			
 			//Assert response of STRUCTURE BODY
+			.assertThat()
 			.body("[0].limit", equalTo(12))
 			.body("[0].total", greaterThanOrEqualTo(1))
 			.body("[0].items[0].term", equalTo(BODY_STRUCTURE_ID))
 			
 			//Assert response of clinical finding
+			.assertThat()
 			.body("[1].limit", equalTo(2))
 			.body("[1].total", greaterThanOrEqualTo(1))
 			.body("[1].items[0].term", equalTo("Clinical finding (finding)"));
