0a111b67c7f9b3fb5345e85e4ee3fc62f9ac2847
aalaswadi <aalaswadi@b2international.com>
2021-11-23 12:17:16 +0100

    SO-4965: Clear local id cache after commit

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/request/rf2/importer/Rf2TransactionContext.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/request/rf2/importer/Rf2TransactionContext.java
@@ -96,6 +96,9 @@ public final class Rf2TransactionContext extends DelegatingTransactionContext {
 			cis.register(idsToRegister);
 			cis.publish(idsToPublish);
 		}
+		// clear local id cache after commit
+		idsToPublish.clear();
+		idsToRegister.clear();
 		return commit;
 	}
 	
