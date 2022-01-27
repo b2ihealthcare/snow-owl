4d760780e92128928f58e5728cbc7ec9d5fb0e4a
Mark Czotter <mczotter@b2international.com>
2022-01-27 12:35:34 +0100

    fix(auth): prevent access to resources when permissions claim is empty..
    ...for the current user or in the given JWT accessToken

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/BaseResourceSearchRequest.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/BaseResourceSearchRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2021-2022 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -153,6 +153,8 @@ public abstract class BaseResourceSearchRequest<R> extends SearchIndexResourceRe
 			}
 			
 			queryBuilder.filter(bool.build());
+		} else {
+			throw new NoResultException();
 		}
 	}
 
