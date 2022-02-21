6b33999aea38f7c1a5a2c217ac2aa1113873d12a
András Péteri <apeteri@b2international.com>
2022-02-21 18:46:32 +0100

    feat(core): Introduce generic taxonomy change processor for concept docs

---
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/taxonomy/SimpleTaxonomyChangeProcessor.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2022 B2i Healthcare Pte Ltd, http://b2i.sg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.core.taxonomy;
+
+import static com.google.common.collect.Maps.newHashMap;
+import static com.google.common.collect.Sets.newHashSet;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+import com.b2international.index.revision.RevisionSearcher;
+import com.b2international.index.revision.StagingArea;
+import com.b2international.snowowl.core.domain.IComponent;
+import com.b2international.snowowl.core.repository.ChangeSetProcessorBase;
+import com.b2international.snowowl.core.repository.RevisionDocument;
+
+/**
+ * @since 8.0.1
+ */
+public abstract class SimpleTaxonomyChangeProcessor<D extends RevisionDocument> extends ChangeSetProcessorBase {
+	
+	private final Class<D> documentClass;
+	private final String parentIdsProperty;
+
+	public SimpleTaxonomyChangeProcessor(final String description, final Class<D> documentClass, final String parentIdsProperty) {
+		super(description);
+		this.documentClass = documentClass;
+		this.parentIdsProperty = parentIdsProperty;
+	}
+	
+	@Override
+	public void process(final StagingArea staging, final RevisionSearcher searcher) throws IOException {
+		final Set<String> conceptsAndAncestorsToLoad = newHashSet();
+		final Set<String> conceptsAndDescendantsToLoad = newHashSet();
+				
+		// We need to update all existing ancestors of new concepts
+		final Map<String, D> newConceptsById = newHashMap();
+		staging.getNewObjects(documentClass)
+			.forEachOrdered(newConcept -> {
+				newConceptsById.put(newConcept.getId(), newConcept);
+				conceptsAndAncestorsToLoad.addAll(getParentIds(newConcept));	
+			});
+
+		// We need to update all concepts in the ancestor and descendant hierarchy of changed concepts
+		final Map<String, D> changedConceptsById = newHashMap();
+		staging.getChangedRevisions(documentClass, Set.of(parentIdsProperty))
+			.forEachOrdered(change -> {
+				// We could use the ID from either revision here
+				final D oldRevision = documentClass.cast(change.oldRevision);
+				final String conceptId = oldRevision.getId();
+
+				// Add concept and ancestors from old revision (ancestor IDs set is reliable)
+				conceptsAndAncestorsToLoad.add(conceptId);
+				conceptsAndAncestorsToLoad.addAll(getParentIds(oldRevision));
+				conceptsAndAncestorsToLoad.addAll(getIndirectAncestorIds(oldRevision));
+				
+				// Add parents from new revision (we can not rely on the ancestor IDs set here)
+				final D newRevision = documentClass.cast(change.newRevision);
+				final Set<String> newParentIds = getParentIds(newRevision);
+				conceptsAndAncestorsToLoad.addAll(newParentIds);
+				
+				// Load descendants for concept as well
+				conceptsAndDescendantsToLoad.add(conceptId);
+				
+				// Store the new revision in the map
+				changedConceptsById.put(conceptId, newRevision);
+			});
+		
+		// Same for detached concepts
+		final Set<String> deletedConceptIds = newHashSet();
+		staging.getRemovedObjects(documentClass)
+			.forEachOrdered(removedConcept -> {
+				final String conceptId = removedConcept.getId();
+				deletedConceptIds.add(conceptId);
+				conceptsAndAncestorsToLoad.add(conceptId);
+				conceptsAndDescendantsToLoad.add(conceptId);
+			});
+
+		final Map<String, D> conceptsById = newHashMap();
+		
+		// First, fetch all ancestors that we could collect from the change set alone
+		searcher.get(documentClass, conceptsAndAncestorsToLoad)
+			.forEach(existingConcept -> {
+				final String conceptId = existingConcept.getId();
+				conceptsById.put(conceptId, existingConcept);
+				
+				// Extend the set with each concept's ancestors (parent and ancestor IDs set is reliable) 
+				conceptsAndAncestorsToLoad.remove(conceptId);
+				conceptsAndAncestorsToLoad.addAll(getParentIds(existingConcept));
+				conceptsAndAncestorsToLoad.addAll(getIndirectAncestorIds(existingConcept));
+			});
+
+		// Load remaining ancestors
+		conceptsAndAncestorsToLoad.removeAll(conceptsById.keySet());
+		searcher.get(documentClass, conceptsAndAncestorsToLoad)
+			.forEach(existingConcept -> conceptsById.put(existingConcept.getId(), existingConcept));
+		
+		conceptsAndAncestorsToLoad.clear();
+		
+		// Load concepts and descendants from the index (these are also the documents which need to be updated)
+		final Map<String, D> conceptAndDescendants = getConceptAndDescendants(conceptsAndDescendantsToLoad, searcher);
+		conceptsById.putAll(conceptAndDescendants);
+		
+		// Assume 1:1 ratio between nodes and edges
+		final SimpleTaxonomyGraph taxonomy = new SimpleTaxonomyGraph(conceptsById.size(), conceptsById.size());
+		taxonomy.addNode(IComponent.ROOT_ID);
+		
+		// Existing state
+		conceptsById.keySet()
+			.forEach(taxonomy::addNode);
+		conceptsById.values()
+			.forEach(concept -> taxonomy.addEdge(concept.getId(), getParentIds(concept)));
+
+		// Add new concepts
+		newConceptsById.keySet()
+			.forEach(taxonomy::addNode);
+		newConceptsById.values()
+			.forEach(concept -> taxonomy.addEdge(concept.getId(), getParentIds(concept)));
+
+		// Update existing edges (re-register edge with the new set of parent IDs)
+		changedConceptsById.values()
+			.forEach(concept -> taxonomy.addEdge(concept.getId(), getParentIds(concept)));
+		
+		// Delete concepts and edges
+		deletedConceptIds.forEach(conceptId -> {
+			taxonomy.removeEdge(conceptId);
+			taxonomy.removeNode(conceptId);
+		});
+		
+		if (taxonomy.build()) {
+			throw new IllegalStateException("Failed to build updated taxonomy.");
+		}
+		
+		// Update new concepts, changed concepts (that were not deleted) and their descendants
+		conceptAndDescendants.values()
+			.forEach(concept -> updateTaxonomy(staging, taxonomy, concept));
+		newConceptsById.values()
+			.forEach(concept -> updateTaxonomy(staging, taxonomy, concept));
+	}
+
+	// direct
+	protected abstract Set<String> getParentIds(final D conceptDocument);
+	
+	// indirect
+	protected abstract Set<String> getIndirectAncestorIds(final D conceptDocument);
+	
+	// direct and indirect
+	protected abstract Map<String, D> getConceptAndDescendants(final Set<String> ancestorIds, final RevisionSearcher searcher) throws IOException;
+
+	private void updateTaxonomy(final StagingArea staging, final SimpleTaxonomyGraph taxonomy, final D concept) {
+		if (staging.isRemoved(concept)) {
+			return;
+		}
+		
+		Set<String> parentIds = taxonomy.getParentIds(concept.getId());
+		if (parentIds.isEmpty()) {
+			parentIds = Set.of(IComponent.ROOT_ID);
+		}
+		
+		Set<String> ancestorIds = taxonomy.getIndirectAncestorIds(concept.getId());
+		if (ancestorIds.isEmpty()) {
+			ancestorIds = Set.of(IComponent.ROOT_ID);
+		}
+		
+		// stageChange should handle new revisions seamlessly as well
+		staging.stageChange(concept, updateTaxonomy(concept, parentIds, ancestorIds));
+	}
+
+	protected abstract D updateTaxonomy(final D concept, final Set<String> parentIds, final Set<String> ancestorIds);
+}
