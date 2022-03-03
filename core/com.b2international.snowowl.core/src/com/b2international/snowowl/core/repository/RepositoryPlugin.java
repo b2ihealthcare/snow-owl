0a5fef47ce11661e5b325b8296761ba1924513e2
András Péteri <apeteri@b2international.com>
2022-03-03 20:31:44 +0100

    feat(core): Initialize Netty server channel in RepositoryPlugin
    Conflicts:
    	core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/repository/RepositoryPlugin.java

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/repository/RepositoryPlugin.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/repository/RepositoryPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2020 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2022 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,12 +17,6 @@ package com.b2international.snowowl.core.repository;
 
 import java.util.Map;
 
-import org.eclipse.net4j.Net4jUtil;
-import org.eclipse.net4j.jvm.IJVMConnector;
-import org.eclipse.net4j.jvm.JVMUtil;
-import org.eclipse.net4j.tcp.TCPUtil;
-import org.eclipse.net4j.util.container.IManagedContainer;
-import org.eclipse.net4j.util.lifecycle.LifecycleUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,11 +26,9 @@ import com.b2international.index.IndexClientFactory;
 import com.b2international.index.Indexes;
 import com.b2international.index.mapping.Mappings;
 import com.b2international.index.revision.TimestampProvider;
+import com.b2international.snowowl.core.IDisposableService;
 import com.b2international.snowowl.core.RepositoryManager;
-import com.b2international.snowowl.core.api.SnowowlRuntimeException;
-import com.b2international.snowowl.core.api.SnowowlServiceException;
 import com.b2international.snowowl.core.branch.review.ReviewConfiguration;
-import com.b2international.snowowl.core.client.TransportClient;
 import com.b2international.snowowl.core.client.TransportConfiguration;
 import com.b2international.snowowl.core.config.*;
 import com.b2international.snowowl.core.domain.RepositoryContextProvider;
@@ -48,11 +40,9 @@ import com.b2international.snowowl.core.plugin.Component;
 import com.b2international.snowowl.core.setup.ConfigurationRegistry;
 import com.b2international.snowowl.core.setup.Environment;
 import com.b2international.snowowl.core.setup.Plugin;
+import com.b2international.snowowl.eventbus.EventBusUtil;
 import com.b2international.snowowl.eventbus.IEventBus;
-import com.b2international.snowowl.eventbus.net4j.EventBusNet4jUtil;
-import com.b2international.snowowl.rpc.RpcConfiguration;
-import com.b2international.snowowl.rpc.RpcProtocol;
-import com.b2international.snowowl.rpc.RpcUtil;
+import com.b2international.snowowl.eventbus.netty.EventBusNettyUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.net.HostAndPort;
@@ -60,6 +50,12 @@ import com.google.common.net.HostAndPort;
 import io.micrometer.core.instrument.FunctionCounter;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 /**
  * @since 3.3
@@ -73,22 +69,17 @@ public final class RepositoryPlugin extends Plugin {
 	public void addConfigurations(ConfigurationRegistry registry) {
 		registry.add("reviewManager", ReviewConfiguration.class);
 		registry.add("repository", RepositoryConfiguration.class);
-		registry.add("rpc", RpcConfiguration.class);
 		registry.add("transport", TransportConfiguration.class);
 		registry.add("jobs", JobConfiguration.class);
 	}
 	
 	@Override
 	public void init(SnowOwlConfiguration configuration, Environment env) throws Exception {
-		final IManagedContainer container = env.container();
-		final boolean gzip = configuration.isGzip();
-		final RpcConfiguration rpcConfig = configuration.getModuleConfig(RpcConfiguration.class);
-		LOG.debug("Preparing RPC communication (config={},gzip={})", rpcConfig, gzip);
-		RpcUtil.prepareContainer(container, rpcConfig, gzip);
-		LOG.debug("Preparing EventBus communication (gzip={})", gzip);
-		int maxThreads = configuration.getModuleConfig(RepositoryConfiguration.class).getMaxThreads();
-		EventBusNet4jUtil.prepareContainer(container, gzip, maxThreads);
-		env.services().registerService(IEventBus.class, EventBusNet4jUtil.getBus(container, maxThreads));
+		RepositoryConfiguration repositoryConfiguration = configuration.getModuleConfig(RepositoryConfiguration.class);
+		env.services().registerService(RepositoryConfiguration.class, repositoryConfiguration);
+		int maxThreads = repositoryConfiguration.getMaxThreads();
+		LOG.debug("Preparing EventBus communication (maxThreads={})", maxThreads);
+		env.services().registerService(IEventBus.class, EventBusUtil.getBus("server", maxThreads));
 		LOG.debug("Preparing JSON support");
 		final ObjectMapper mapper = JsonSupport.getDefaultObjectMapper();
 		mapper.registerModule(new PrimitiveCollectionModule());
@@ -117,6 +108,25 @@ public final class RepositoryPlugin extends Plugin {
 		return builder.build();
 	}
 
+	private static final class ServerChannelService implements IDisposableService {
+
+		private final Channel serverChannel;
+		
+		public ServerChannelService(final Channel serverChannel) {
+			this.serverChannel = serverChannel;
+		}
+
+		@Override
+		public void dispose() {
+			serverChannel.close().awaitUninterruptibly();
+		}
+
+		@Override
+		public boolean isDisposed() {
+			return !serverChannel.isActive();
+		}
+	}
+	
 	@Override
 	public void preRun(SnowOwlConfiguration configuration, Environment env) {
 		if (env.isServer()) {
@@ -126,24 +136,29 @@ public final class RepositoryPlugin extends Plugin {
 			// Add event bus based request metrics
 			registerRequestMetrics(registry, eventBus);
 			
-			final IManagedContainer container = env.container();
-			
-			RpcUtil.getInitialServerSession(container).registerServiceLookup(env::service);
-			
-			Net4jUtil.prepareContainer(container);
-			JVMUtil.prepareContainer(container);
-			TCPUtil.prepareContainer(container);
-			
-			LifecycleUtil.activate(container);
-			
 			final HostAndPort hostAndPort = configuration.getModuleConfig(RepositoryConfiguration.class).getHostAndPort();
 			// open port in server environments
 			if (hostAndPort.getPort() > 0) {
-				TCPUtil.getAcceptor(container, hostAndPort.toString()); // Starts the TCP transport 
+				final NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+				final NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+				final boolean gzip = configuration.isGzip();
+				final ClassLoader compositeClassLoader = env.plugins().getCompositeClassLoader();
+				
+				final Channel serverChannel = new ServerBootstrap()
+					.group(bossGroup, workerGroup)
+					.handler(new LoggingHandler(LogLevel.INFO))
+					.channel(NioServerSocketChannel.class)
+					.childHandler(EventBusNettyUtil.createChannelHandler(gzip, true, eventBus, compositeClassLoader))
+					.bind(hostAndPort.getHost(), hostAndPort.getPortOrDefault(2036))
+					.syncUninterruptibly()
+					.channel();
+
+				// Register channel as a service so it will be shut down when services are disposed
+				env.services().registerService(ServerChannelService.class, new ServerChannelService(serverChannel));
+				
 				LOG.info("Listening on {} for connections", hostAndPort);
 			}
-
-			JVMUtil.getAcceptor(container,	TransportClient.NET_4_J_CONNECTOR_NAME); // Starts the JVM transport
 			
 			final RepositoryManager repositoryManager = new DefaultRepositoryManager();
 			env.services().registerService(RepositoryManager.class, repositoryManager);
@@ -155,14 +170,6 @@ public final class RepositoryPlugin extends Plugin {
 			LOG.debug("Initialized repository plugin.");
 		} else {
 			LOG.debug("Snow Owl application is running in remote mode.");
-		}
-		
-		if (env.isServer()) {
-			try {
-				connectSystemUser(env.container());
-			} catch (SnowowlServiceException e) {
-				throw new SnowowlRuntimeException(e);
-			}
 		}
 	}
 	
@@ -218,12 +225,5 @@ public final class RepositoryPlugin extends Plugin {
 		for (int i = 0; i < numberOfWorkers; i++) {
 			events.registerHandler(Request.ADDRESS, new ApiRequestHandler(env, classLoader));
 		}
-	}
-
-	private void connectSystemUser(IManagedContainer container) throws SnowowlServiceException {
-		final IJVMConnector connector = JVMUtil.getConnector(container, TransportClient.NET_4_J_CONNECTOR_NAME);
-		JVMUtil.getAcceptor(container, TransportClient.NET_4_J_CONNECTOR_NAME);
-		final RpcProtocol clientProtocol = RpcUtil.getRpcClientProtocol(container);
-		clientProtocol.open(connector);
 	}
 }
