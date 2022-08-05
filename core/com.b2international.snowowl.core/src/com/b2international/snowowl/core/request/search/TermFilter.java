14e4a05e9da5d6d4ba126352ee6fff94bb3d0159
Mark Czotter <mczotter@b2international.com>
2022-08-05 12:25:14 +0200

    chore(javadoc): update javadoc in TermFilter

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/search/TermFilter.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/search/TermFilter.java
@@ -19,6 +19,7 @@ import java.io.Serializable;
 import java.util.Set;
 
 import com.b2international.index.query.Expression;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.collect.Iterables;
 
 /**
@@ -30,18 +31,51 @@ public abstract class TermFilter implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * The <b>default</b> term matching contains the following elastic search expressions:
+	 * <p>
+	 * <li>Exact term match on a case insensitive, ASCII folded keyword type field (usually the <b>term.exact</b> field)
+	 * <li>All terms present match on a case insensitive, ASCII folded, possessive removed, split text type field (usually the <b>term</b> field)
+	 * <li>All term prefixes present match on a case insensitive, ASCII folded, possessive removed and split text type field where the field has 2-12 additional prefix terms generated via the edge_ngram token filter (usually the <b>term.prefix</b> field)
+	 * 
+	 * The <b>minShouldMatch</b> term filter contains the following elastic search expressions:
+	 * <p>
+	 * <li>All terms present match on a case insensitive, ASCII folded, possessive removed, split text type field (usually the <b>term</b> field)
+	 * <li>All term prefixes present match on a case insensitive, ASCII folded, possessive removed and split text type field where the field has 2-12 additional prefix terms generated via the edge_ngram token filter (usually the <b>term.prefix</b> field)
+	 * 
+	 * The <b>fuzziness</b> configuration alters the query to allow certain amount of fuzziness in the given terms:
+	 * 
+	 * <li>All terms present match on a case insensitive, ASCII folded, possessive removed, split text type field (usually the <b>term</b> field) with fuzziness enabled with hardcoded 10 expansions of 1 character difference (Levenshtein distance).
+	 * 
+	 * Additionally stopwords can be ignored and case sensitivity can be enabled/disabled.
+	 * 
+	 * @return {@link MatchTermFilter.Builder}
+	 */
+	@JsonIgnore
 	public static MatchTermFilter.Builder match() {
 		return new MatchTermFilter.Builder();
 	}
 	
+	/**
+	 * The <b>exactTermMatch</b> term filter contains the following elastic search expression:
+	 * 
+	 * <li>Documents that contain an <b>exact</b> term in a provided field (usually the <b>term</b> field)
+	 * 
+	 * Additionally case sensitivity can be enabled/disabled.
+	 * 
+	 * @return {@link ExactTermFilter.Builder}
+	 */
+	@JsonIgnore
 	public static ExactTermFilter.Builder exact() {
 		return new ExactTermFilter.Builder();
 	}
 	
+	@JsonIgnore
 	public static ParsedTermFilter.Builder parsed() {
 		return new ParsedTermFilter.Builder();
 	}
 	
+	@JsonIgnore
 	public static MoreLikeThisTermFilter.Builder mlt() {
 		return new MoreLikeThisTermFilter.Builder();
 	}
@@ -53,108 +87,23 @@ public abstract class TermFilter implements Serializable {
 	 */
 	public abstract Set<String> getTerms();
 	
+	@JsonIgnore
 	public final String getSingleTermOrNull() {
 		final Set<String> terms = getTerms();
 		return terms.size() == 1 ? Iterables.getOnlyElement(terms) : null;
 	}
 	
+	@JsonIgnore
 	public Expression toExpression(String field) {
 		return toExpression(field, "text", "exact", "prefix");
 	}
 	
+	@JsonIgnore
 	public abstract Expression toExpression(String field, String textFieldSuffix, String exactFieldSuffix, String prefixFieldSuffix);
 	
+	@JsonIgnore
 	protected final String fieldAlias(String field, String fieldSuffix) {
 		return String.join(".", field, fieldSuffix);
 	}
 	
-//	/**
-//	 * The <b>default</b> term filter contains the following elastic search expressions:
-//	 * <p>
-//	 * <li>Exact term match on a case insensitive, ASCII folded keyword type field (usually the <b>term.exact</b> field)
-//	 * <li>All terms present match on a case insensitive, ASCII folded, possessive removed, split text type field (usually the <b>term</b> field)
-//	 * <li>All term prefixes present match on a case insensitive, ASCII folded, possessive removed and split text type field where the field has 2-12 additional prefix terms generated via the edge_ngram token filter (usually the <b>term.prefix</b> field)
-//	 * 
-//	 * @param term
-//	 * 			- the term to apply the expressions on
-//	 * @return {@link TermFilter}
-//	 */
-//	@JsonIgnore
-//	public static final TermFilter defaultTermMatch(final String term) {
-//		return builder().term(term).build();
-//	}
-//	
-//	/**
-//	 * The <b>minTermMatch</b> term filter contains the following elastic search expressions:
-//	 * <p>
-//	 * <li>All terms present match on a case insensitive, ASCII folded, possessive removed, split text type field (usually the <b>term</b> field)
-//	 * <li>All term prefixes present match on a case insensitive, ASCII folded, possessive removed and split text type field where the field has 2-12 additional prefix terms generated via the edge_ngram token filter (usually the <b>term.prefix</b> field)
-//	 * 
-//	 * @param term
-//	 * 			- the term to apply the expressions on 
-//	 * @param minShouldMatch
-//	 * 			- the number of tokens to match
-//	 * @return {@link TermFilter}
-//	 */
-//	@JsonIgnore
-//	public static final TermFilter minTermMatch(final String term, final Integer minShouldMatch) {
-//		if (minShouldMatch >= 1) {
-//			return builder().term(term).minShouldMatch(minShouldMatch).build();
-//		} else {
-//			throw new BadRequestException("minShouldMatch parameter must be greater than or equal to 1. It was '%s'.", minShouldMatch);
-//		}
-//	}
-//	
-//	/**
-//	 * The <b>fuzzyMatch</b> term filter contains the following elastic search expression:
-//	 * 
-//	 * <li>All terms present match on a case insensitive, ASCII folded, possessive removed, split text type field (usually the <b>term</b> field) with fuzziness enabled with hardcoded 10 expansions of 1 character difference (Levenshtein distance). 
-//	 * 
-//	 * @param term
-//	 * 			- the term to apply the expressions on
-//	 * @return {@link TermFilter}
-//	 */
-//	@JsonIgnore
-//	public static final TermFilter fuzzyMatch(final String term) {
-//		return builder().term(term).fuzzy(true).build();
-//	}
-//	
-//	/**
-//	 * The <b>exactTermMatch</b> term filter contains the following elastic search expression:
-//	 * 
-//	 * <li>Documents that contain an <b>exact</b> term in a provided field (usually the <b>term</b> field)
-//	 * 
-//	 * @param term
-//	 * 			- the term to apply the expressions on
-//	 * @return {@link TermFilter}
-//	 */
-//	@JsonIgnore
-//	public static final TermFilter exactTermMatch(final String term) {
-//		return builder().term(term).exact(true).caseSensitive(true).build();
-//	}
-//
-//	/**
-//	 * The <b>exactTermMatch</b> term filter contains the following elastic search expression:
-//	 * 
-//	 * <li>Exact term match on a case insensitive, ASCII folded keyword type field (usually the <b>term.exact</b> field)
-//	 * 
-//	 * @param term
-//	 * 			- the term to apply the expressions on
-//	 * @return {@link TermFilter}
-//	 */
-//	@JsonIgnore
-//	public static final TermFilter exactIgnoreCaseTermMatch(final String term) {
-//		return builder().term(term).exact(true).build();
-//	}
-//	
-//	/**
-//	 * @param term
-//	 * 			- the term to apply the expressions on
-//	 * @return {@link TermFilter}
-//	 */
-//	@JsonIgnore
-//	public static final TermFilter parsedTermMatch(final String term) {
-//		return builder().term(term).parsed(true).build();
-//	}
-
 }
