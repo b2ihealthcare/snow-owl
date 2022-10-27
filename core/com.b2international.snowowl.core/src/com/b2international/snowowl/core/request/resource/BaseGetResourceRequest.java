bdeb10d52a03f0ad64a478765013d051dc372fe3
Mark Czotter <mczotter@b2international.com>
2022-10-27 15:04:09 +0200

    fix(api): support accessing the version denoted by the LATEST keyword

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/resource/BaseGetResourceRequest.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/resource/BaseGetResourceRequest.java
@@ -15,14 +15,14 @@
  */
 package com.b2international.snowowl.core.request.resource;
 
+import com.b2international.commons.exceptions.BadRequestException;
 import com.b2international.snowowl.core.ResourceURI;
 import com.b2international.snowowl.core.ServiceProvider;
 import com.b2international.snowowl.core.domain.RepositoryContext;
-import com.b2international.snowowl.core.request.GetResourceRequest;
-import com.b2international.snowowl.core.request.ResourceRequests;
-import com.b2international.snowowl.core.request.RevisionIndexReadRequestTimestampProvider;
-import com.b2international.snowowl.core.request.SearchResourceRequestBuilder;
+import com.b2international.snowowl.core.request.*;
+import com.b2international.snowowl.core.request.version.VersionSearchRequestBuilder;
 import com.b2international.snowowl.core.version.Version;
+import com.b2international.snowowl.core.version.VersionDocument;
 import com.google.common.base.Strings;
 
 /**
@@ -50,15 +50,31 @@ public abstract class BaseGetResourceRequest<SB extends SearchResourceRequestBui
 		if (!Strings.isNullOrEmpty(resourceUri.getTimestampPart())) {
 			return Long.parseLong(resourceUri.getTimestampPart().substring(1));
 		} else if (!resourceUri.isHead() && !resourceUri.isNext()) {
-			return ResourceRequests.prepareSearchVersion()
-					.one()
-					.filterByResource(resourceUri.withoutPath())
-					.filterByVersionId(resourceUri.getPath())
-					.buildAsync()
-					.execute(context)
-					.first()
-					.map(Version::getCreatedAt)
-					.orElse(null);
+			VersionSearchRequestBuilder versionSearch = ResourceRequests.prepareSearchVersion()
+				.one()
+				.filterByResource(resourceUri.withoutPath());
+			
+			if (resourceUri.isLatest()) {
+				// fetch the latest resource version if LATEST is specified in the URI
+				versionSearch.sortBy(SearchResourceRequest.Sort.fieldDesc(VersionDocument.Fields.EFFECTIVE_TIME));
+			} else {
+				// try to fetch the path as exact version if not the special LATEST is specified in the URI
+				versionSearch.filterByVersionId(resourceUri.getPath());
+			}
+			
+			// determine the final branch path, if based on the version search we find a version, then use that, otherwise use the defined path as relative branch of the code system working branch
+			return versionSearch.buildAsync()
+				.execute(context)
+				.first()
+				.map(Version::getCreatedAt)
+				.orElseGet(() -> {
+					// ignore if accessing the LATEST versioned state, but there is no version present
+					if (resourceUri.isLatest()) {
+						return null;
+					} else {
+						throw new BadRequestException("Only version paths and timestamp fragments are supported when accessing a resource via an URI. Got: %s", resourceUri.getPath());
+					}
+				});
 		}
 		return null;
 	}
