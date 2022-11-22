760b69f69aff032c8ec1fc77b6763ee98e6efcb5
Gabor Nagy <gnagy@b2international.com>
2022-11-22 17:25:50 +0100

    fix(api): move resource setting delimiter to constant, introduce ...
    ... Resource.Settings nested class to collect common metadata properties

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/BaseResourceSearchRequest.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/BaseResourceSearchRequest.java
@@ -114,11 +114,11 @@ public abstract class BaseResourceSearchRequest<R> extends SearchIndexResourceRe
 		if (containsKey(OptionKey.SETTINGS)) {
 			final Collection<String> properties = getCollection(OptionKey.SETTINGS, String.class);
 			properties.forEach( property -> {
-				if (property.contains("#")) {
-					final String propertyName = property.split("\\#")[0];
+				if (property.contains(Resource.SETTINGS_DELIMITER)) {
+					final String propertyName = property.split(Resource.SETTINGS_DELIMITER)[0];
 					final String propertyValue = property.substring(propertyName.length() + 1, property.length());
 					if (Strings.isNullOrEmpty(propertyValue)) {
-						throw new BadRequestException("Settings argument %s is not allowed. Expected format is propertyName#propertyValue.", property);
+						throw new BadRequestException("Settings argument %s is not allowed. Expected format is propertyName" + Resource.SETTINGS_DELIMITER + "propertyValue.", property);
 					}
 					//Check if property has specified value
 					if (propertyValue.endsWith("*")) {
