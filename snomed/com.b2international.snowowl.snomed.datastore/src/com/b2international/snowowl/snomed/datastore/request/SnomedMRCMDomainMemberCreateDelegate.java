d04352291610030c28310bbd3178cbf8e3678e38
AAAlinaaa <aalaswadi@b2international.com>
2023-02-07 15:17:51 +0100

    SO-5535: Add non-null requirements to MRCM properties that may be empty

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/request/SnomedMRCMDomainMemberCreateDelegate.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/datastore/request/SnomedMRCMDomainMemberCreateDelegate.java
@@ -37,6 +37,12 @@ final class SnomedMRCMDomainMemberCreateDelegate extends SnomedRefSetMemberCreat
 		checkReferencedComponent(refSet);
 		checkNonEmptyProperty(SnomedRf2Headers.FIELD_MRCM_DOMAIN_CONSTRAINT);
 		
+		checkNonNullProperty(SnomedRf2Headers.FIELD_MRCM_PARENT_DOMAIN);
+		checkNonNullProperty(SnomedRf2Headers.FIELD_MRCM_PROXIMAL_PRIMITIVE_CONSTRAINT);
+		checkNonNullProperty(SnomedRf2Headers.FIELD_MRCM_PROXIMAL_PRIMITIVE_REFINEMENT);
+		checkNonNullProperty(SnomedRf2Headers.FIELD_MRCM_DOMAIN_TEMPLATE_FOR_PRECOORDINATION);
+		checkNonNullProperty(SnomedRf2Headers.FIELD_MRCM_DOMAIN_TEMPLATE_FOR_POSTCOORDINATION);
+		
 		checkComponentExists(refSet, context, SnomedRf2Headers.FIELD_MODULE_ID, getModuleId());
 		checkComponentExists(refSet, context, SnomedRf2Headers.FIELD_REFERENCED_COMPONENT_ID, getReferencedComponentId());
 
