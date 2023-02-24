42aeb920f993d2b5e1a3786d02088ac605857611
Mark Czotter <mczotter@b2international.com>
2023-02-24 14:06:45 +0100

    fix(auth): ensure that usage of expired tokens are properly logged

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/identity/AuthorizationHeaderVerifier.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/identity/AuthorizationHeaderVerifier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2022 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2021-2023 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package com.b2international.snowowl.core.identity;
 
 import java.util.Base64;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.b2international.commons.exceptions.UnauthorizedException;
 import com.google.common.base.Charsets;
 
@@ -79,6 +80,9 @@ public final class AuthorizationHeaderVerifier {
 	public User authJWT(final String token) {
 		try {
 			return jwtSupport.authJWT(token);
+		} catch (TokenExpiredException e) {
+			IdentityProvider.LOG.warn("Failed login attempt with an expired authorization token");
+			throw new UnauthorizedException("Incorrect authorization token");
 		} catch (Exception e) {
 			// try to authorize JWT using the global provider first, then fall back to the dedicated identity provider specific JWT authentication
 		}
