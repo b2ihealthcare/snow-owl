bc76b98b05572958eccd19aa8ffed751d165d95d
Mark Czotter <mczotter@b2international.com>
2023-03-08 09:31:54 +0100

    chore(index): add missing FunctionalInterface annotation...
    ...to RevisionIndexRead

--- commons/com.b2international.index/src/com/b2international/index/revision/RevisionIndexRead.java
+++ commons/com.b2international.index/src/com/b2international/index/revision/RevisionIndexRead.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2023 B2i Healthcare Pte Ltd, http://b2i.sg
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import java.io.IOException;
  *            - the return type of the read op
  * @see RevisionIndex#read(RevisionIndexRead)
  */
+@FunctionalInterface
 public interface RevisionIndexRead<T> {
 
 	/**
