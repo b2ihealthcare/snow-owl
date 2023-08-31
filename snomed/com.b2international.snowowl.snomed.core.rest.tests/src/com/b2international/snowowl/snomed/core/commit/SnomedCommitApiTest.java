f5ee2a62aad5a5524723acdb246812eab448df91
András Péteri <apeteri@b2international.com>
2023-08-31 10:44:30 +0200

    feat(core): Support ResourceURI input for lock/unlock requests

--- snomed/com.b2international.snowowl.snomed.core.rest.tests/src/com/b2international/snowowl/snomed/core/commit/SnomedCommitApiTest.java
+++ snomed/com.b2international.snowowl.snomed.core.rest.tests/src/com/b2international/snowowl/snomed/core/commit/SnomedCommitApiTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.b2international.snowowl.snomed.core.io.commit;
+package com.b2international.snowowl.snomed.core.commit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
