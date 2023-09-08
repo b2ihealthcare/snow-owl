d1335f77d7d80d123886a11c9735f971b40c935b
AAAlinaaa <aalaswadi@b2international.com>
2023-09-08 09:58:58 +0200

    fix(resources): Remove scope specification from dependency filtering

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/version/VersionCreateRequest.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/version/VersionCreateRequest.java
@@ -200,7 +200,7 @@ public final class VersionCreateRequest implements Request<RepositoryContext, Bo
 			//Find resources that depend on this resource
 			List<String> dependentResourceIds = ResourceRequests.prepareSearch()
 				.setLimit(10_000)
-				.filterByDependency(String.format("(uri:%s OR uri:%s/* OR uri:%s\\?*) AND scope:domain) ", resource, resource, resource))
+				.filterByDependency(String.format("uri:%s OR uri:%s/* OR uri:%s\\?*", resource, resource, resource))
 				.setFields(ResourceDocument.Fields.ID)
 				.stream(context)
 				.flatMap(Resources::stream)
