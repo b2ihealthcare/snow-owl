ff973b723e80e311d081b54a4f6097ab534d38ea
Mark Czotter <mczotter@b2international.com>
2023-10-06 08:10:45 +0200

    fix(core): deserialization of anyOf/allOf permissions

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/identity/Permission.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/identity/Permission.java
@@ -131,9 +131,13 @@ public interface Permission extends Serializable {
 		final String operation = parts[0];
 		final String resourceReference = parts[1];
 		if (RequireAnyPermission.isRequireAnyResource(resourceReference)) {
-			return requireAny(operation, List.of(resourceReference));
+			String[] resourceIds = resourceReference.substring("anyOf(".length(), resourceReference.length() - 1).split(",");
+			return requireAny(operation, resourceIds);
+		} else if (RequireAllPermission.isRequireAllResource(resourceReference)) {
+			String[] resourceIds = resourceReference.substring("allOf(".length(), resourceReference.length() - 1).split(",");
+			return requireAll(operation, resourceIds);
 		} else {
-			return requireAll(operation, List.of(resourceReference));
+			return requireAll(operation, resourceReference);
 		}
 	}
 
