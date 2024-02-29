2022a51fd7c985776df5312606ebec96d37192f9
Mark Czotter <mczotter@b2international.com>
2024-02-29 10:36:00 +0100

    test: bump ES image to 8.12.2

--- commons/com.b2international.index.test.tools/src/com/b2international/index/IndexResource.java
+++ commons/com.b2international.index.test.tools/src/com/b2international/index/IndexResource.java
@@ -57,7 +57,7 @@ public final class IndexResource extends ExternalResource {
 	 */
 	public static final String ES_USE_TEST_CONTAINER_VARIABLE = "so.index.es.useDocker";
 	
-	public static final String DEFAULT_ES_DOCKER_IMAGE = "docker.elastic.co/elasticsearch/elasticsearch:8.12.0";
+	public static final String DEFAULT_ES_DOCKER_IMAGE = "docker.elastic.co/elasticsearch/elasticsearch:8.12.2";
 
 	private static final AtomicBoolean INIT = new AtomicBoolean(false);
 	
