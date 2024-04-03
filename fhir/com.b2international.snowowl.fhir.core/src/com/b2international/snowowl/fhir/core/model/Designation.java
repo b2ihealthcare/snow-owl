3bac3bf8c28c6a395abd9b894866ce919315be3d
Mark Czotter <mczotter@b2international.com>
2024-04-03 14:27:55 +0200

    chore(fhir): convert new Description model to Designation by...
    ...specifying the language part as well

--- fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/model/Designation.java
+++ fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/model/Designation.java
@@ -15,8 +15,11 @@
  */
 package com.b2international.snowowl.fhir.core.model;
 
-import jakarta.validation.constraints.NotEmpty;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.b2international.snowowl.core.domain.Description;
 import com.b2international.snowowl.fhir.core.model.dt.Code;
 import com.b2international.snowowl.fhir.core.model.dt.Coding;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -24,6 +27,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import jakarta.validation.constraints.NotEmpty;
 
 /**
  * This class represents a FHIR designation.
@@ -125,6 +130,19 @@ public final class Designation {
 		protected Designation doBuild() {
 			return new Designation(language, use, value);
 		}
+	}
+
+	/**
+	 * Maps the given collection of {@link Description} objects into FHIR {@link Designation} objects by mapping the term and language fields to the matching properties.
+	 * @param descriptions
+	 * @return a {@link List} of {@link Designation} objects, never <code>null</code>.
+	 */
+	@JsonIgnore
+	public static List<Designation> fromDescriptions(Collection<Description> descriptions) {
+		return descriptions
+				.stream()
+				.map(description -> Designation.builder().value(description.getTerm()).language(description.getLanguage()).build())
+				.collect(Collectors.toList());
 	}
 	
 }
