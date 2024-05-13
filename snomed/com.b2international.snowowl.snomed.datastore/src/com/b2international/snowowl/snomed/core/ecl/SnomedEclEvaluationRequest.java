030304a94cf5aabe14420f965a170deadec791e1
Mark Czotter <mczotter@b2international.com>
2024-05-13 15:09:16 +0200

    fix(ecl): use wildcard care instead of handcrafted regex query...
    ...for wild lexical search type

--- snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/ecl/SnomedEclEvaluationRequest.java
+++ snomed/com.b2international.snowowl.snomed.datastore/src/com/b2international/snowowl/snomed/core/ecl/SnomedEclEvaluationRequest.java
@@ -23,8 +23,6 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import jakarta.validation.constraints.NotNull;
-
 import org.eclipse.emf.ecore.EObject;
 
 import com.b2international.commons.collections.Collections3;
@@ -62,6 +60,8 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Multimap;
+
+import jakarta.validation.constraints.NotNull;
 
 /**
  * Evaluates the given ECL expression {@link String} or parsed {@link ExpressionConstraint} to an executable {@link Expression query expression}.
@@ -630,6 +630,11 @@ final class SnomedEclEvaluationRequest extends EclEvaluationRequest<BranchContex
 	@Override
 	protected Expression termRegexExpression(String regex, boolean caseInsensitive) {
 		return Expressions.regexp(SnomedDescriptionIndexEntry.Fields.TERM, regex, caseInsensitive);
+	}
+	
+	@Override
+	protected Expression termWildExpression(String wild, boolean caseInsensitive) {
+		return Expressions.wildcard(SnomedDescriptionIndexEntry.Fields.TERM, wild, caseInsensitive);
 	}
 	
 	@Override
