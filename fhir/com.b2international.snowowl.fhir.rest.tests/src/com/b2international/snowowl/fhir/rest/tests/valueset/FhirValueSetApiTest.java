14070fdff6c58c207f6734f7dafe49dd8459adcf
András Péteri <apeteri@b2international.com>
2024-10-22 22:20:53 +0200

    chore(fhir.rest.tests): Update copyright notice and "since" header of...
    ...new test class FhirValueSetApiTest

--- fhir/com.b2international.snowowl.fhir.rest.tests/src/com/b2international/snowowl/fhir/rest/tests/valueset/FhirValueSetApiTest.java
+++ fhir/com.b2international.snowowl.fhir.rest.tests/src/com/b2international/snowowl/fhir/rest/tests/valueset/FhirValueSetApiTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2024 B2i Healthcare, https://b2ihealthcare.com
+ * Copyright 2024 B2i Healthcare, https://b2ihealthcare.com
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import com.b2international.snowowl.test.commons.rest.RestExtensions;
 /**
  * FHIR /ValueSet Resource API Tests
  * 
- * @since 6.6
+ * @since 9.4
  */
 public class FhirValueSetApiTest extends FhirRestTest {
 	
